09204c232ed5dff829ed492bb353277d3ce56dc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:56 +0000

    Add package ibrahimhahaforpypipackage/1.0/ibrahimhahaforpypipackage-1.0.tar.gz

---
+++ packages/ibrahimhahaforpypipackage/ibrahimhahaforpypipackage-1.0.tar.gz/ibrahimhahaforpypipackage-1.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+import setuptools
+
+setup(
+    name='ibrahimhahaforpypipackage',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='license.txt',
+    author='DELL',
+    author_email='zaheeribrahim346@gmail.com',
+    description='this is my file',
+
+
+)
