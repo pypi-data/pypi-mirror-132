2e8bd3f913582ef74b8971c7bc2bca963d7a0876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:18 +0000

    Add package pytest-blockage/0.2.4/pytest-blockage-0.2.4.tar.gz

---
+++ packages/pytest-blockage/pytest-blockage-0.2.4.tar.gz/pytest-blockage-0.2.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name='pytest-blockage',
+    version='0.2.4',
+    description='Disable network requests during a test run.',
+    long_description=(open('README.rst').read() +
+                      open('CHANGELOG.rst').read()),
+    long_description_content_type='text/x-rst',
+    url="https://github.com/rob-b/pytest-blockage",
+    license='BSD',
+    install_requires=['pytest'],
+    py_modules=['pytest_blockage'],
+    entry_points={'pytest11': ['blockage = pytest_blockage']},
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+    ]
+)
