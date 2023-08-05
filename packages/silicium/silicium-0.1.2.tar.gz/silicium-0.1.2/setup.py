fd610e840921293db2e7ecbe3c3292a808add5a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:25 +0000

    Add package silicium/0.1.2/silicium-0.1.2.tar.gz

---
+++ packages/silicium/silicium-0.1.2.tar.gz/silicium-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+NAME = "silicium"
+VERSION = "0.1.2"
+URL = "https://github.com/SamimiesGames/silicium"
+
+AUTHOR = "Samimies"
+DESCRIPTION = "Silicium is an ahead-of-time interpreted all-language compiler in Python."
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    url=URL,
+    author=AUTHOR,
+    license="MIT",
+    description=DESCRIPTION,
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"], where="src"),
+    package_dir={"": "src"},
+    install_requires=["htmlmin"]
+)
+
---
+++ packages/silicium/silicium-0.1.2.tar.gz/silicium-0.1.2/src/silicium/component.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+import uuid
+
+
+@dataclass
+class AbstractComponent(ABC):
+    build_target: str
+    parent: any
+    sub_components: list
+
+    @property
+    @abstractmethod
+    def code(self) -> str:
+        ...
+
+    @abstractmethod
+    def grand_render(self):
+        ...
+
+
+class Component(AbstractComponent):
+    def __init__(self, parent, add: bool = True, **kwargs):
+        self.parent = parent
+        self.sub_components = []
+
+        self.id = uuid.uuid4()
+
+        for name, value in kwargs.items():
+            setattr(self, name, value)
+
+        if not add:
+            return
+
+        if isinstance(self.parent, AbstractComponent):
+            self.parent.sub_components.append(self)
+        else:
+            self.parent.builder.add_component(self)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}"
+
+    @property
+    @abstractmethod
+    def code(self) -> str:
+        ...
