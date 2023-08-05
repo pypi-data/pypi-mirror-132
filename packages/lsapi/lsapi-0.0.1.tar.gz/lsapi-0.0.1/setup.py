288b56262b8c3153376d82082ae645e64a3d0ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:11 +0000

    Add package lsapi/0.0.1/lsapi-0.0.1.tar.gz

---
+++ packages/lsapi/lsapi-0.0.1.tar.gz/lsapi-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+     name='lsapi',  
+     version='0.0.1',
+     scripts=['lsapi'] ,
+     author="fwang",
+     author_email="wangfeng@lsasset.com.cn",
+     description="LSAsset Trading Platform client API",
+     long_description="LSAsset Trading Platform client API",
+     url="https://www.lsasset.com.cn",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Development Status :: 1 - Planning",
+     ],
+ )
