186f34fd9fe234036c7d3c165c9707b21396b226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:58 +0000

    Add package django-tagulous/1.3.1/django_tagulous-1.3.1-py3-none-any.whl

---
+++ packages/django-tagulous/django_tagulous-1.3.1-py3-none-any.whl/tagulous/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Django Tagulous - Fabulous Tags
+"""
+__version__ = "1.3.1"
+__license__ = "BSD"
+__author__ = "Richard Terry"
+
+default_app_config = "tagulous.apps.TagulousConfig"
