94c85023e914dd3a79928e5f61cc9b7d72e710cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:57 +0000

    Add package AlexDistAlx/0.0/AlexDistAlx-0.0.tar.gz

---
+++ packages/AlexDistAlx/AlexDistAlx-0.0.tar.gz/AlexDistAlx-0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='AlexDistAlx',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['AlexDistAlx'],
+      license='MIT',
+      author_email='s.aleks-02@mail.ru',
+      zip_safe=False)
