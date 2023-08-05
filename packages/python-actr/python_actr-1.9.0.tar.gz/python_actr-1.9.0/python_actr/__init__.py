72b579a911a9c24e58252ac274912ad394f75ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:36 +0000

    Add package python-actr/1.9.0/python_actr-1.9.0.tar.gz

---
+++ packages/python-actr/python_actr-1.9.0.tar.gz/python_actr-1.9.0/python_actr/__init__.py
@@ -0,0 +1,7 @@
+from .model import Model, log_everything
+from .production import ProductionSystem
+from .logger import log, finished
+from .runner import run, run_with
+from .actr import *
+from .display import display
+from . import version
