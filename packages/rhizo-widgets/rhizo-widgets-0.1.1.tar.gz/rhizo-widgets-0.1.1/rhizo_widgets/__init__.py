cb207f815a9991826384fafe4be03107e2939f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:25 +0000

    Add package rhizo-widgets/0.1.1/rhizo-widgets-0.1.1.tar.gz

---
+++ packages/rhizo-widgets/rhizo-widgets-0.1.1.tar.gz/rhizo-widgets-0.1.1/rhizo_widgets/__init__.py
@@ -0,0 +1,16 @@
+__version__ = '0.1.0'
+
+import ipywidgets
+
+
+class S3Bucket:
+  def __init__(self, session):
+    regions = session.get_available_regions('s3')
+    self.session = session
+    self.region_dropdown = ipywidgets.Dropdown(options=[''] + regions,
+                                          description='AWS Region:')
+  
+  def form(self):
+      return ipywidgets.VBox([
+          self.region,
+      ])
