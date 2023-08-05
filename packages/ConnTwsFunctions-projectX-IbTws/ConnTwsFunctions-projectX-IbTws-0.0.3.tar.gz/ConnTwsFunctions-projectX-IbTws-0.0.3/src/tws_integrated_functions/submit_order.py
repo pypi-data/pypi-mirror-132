658013392bdbfefdd23659011046728f4513609f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:38 +0000

    Add package ConnTwsFunctions-projectX-IbTws/0.0.3/ConnTwsFunctions-projectX-IbTws-0.0.3.tar.gz

---
+++ packages/ConnTwsFunctions-projectX-IbTws/ConnTwsFunctions-projectX-IbTws-0.0.3.tar.gz/ConnTwsFunctions-projectX-IbTws-0.0.3/src/tws_integrated_functions/submit_order.py
@@ -0,0 +1,42 @@
+from src.tws_integrated_functions.appConnectionTws import ConnectionTwsApi
+from ibapi.utils import iswrapper
+
+
+class SubmitOrder(ConnectionTwsApi):
+
+    def __init__(self, addr, port, client_id):
+        super().__init__(addr=addr, port=port, client_id=client_id)
+
+    @iswrapper
+    def nextValidId(self, order_id):
+        ''' Provides the next order ID '''
+        self.order_id = order_id
+        print('Order ID: '.format(order_id))
+
+    @iswrapper
+    def openOrder(self,order_id, contract, order, state):
+        ''' Called in response to the submitted order '''
+        print('Order status: '.format(state.status))
+        print('Commission charged: '.format(state.commission))
+
+    @iswrapper
+    def orderStatus(self,order_id, status, filled, remaining, avgFillPrice, \
+        permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice):
+        ''' Check the status of the subnitted order '''
+        print('Number of filled positions: {}'.format(filled))
+        print('Average fill price: {}'.format(avgFillPrice))
+
+    @iswrapper
+    def position(self,account, contract, pos, avgCost):
+        ''' Read information about the account's open positions '''
+        print('Position in {}: {}'.format(contract.symbol, pos))
+
+    @iswrapper
+    def accountSummary(self, req_id, account, tag, value, currency):
+        ''' Read information about the account '''
+        print('Account {}: {} = {}'.format(account, tag, value))
+
+    @iswrapper
+    def error(self, req_id, code, msg):
+        print('Error {}: {}'.format(code, msg))
+
