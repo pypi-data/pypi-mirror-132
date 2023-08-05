ebceeb8fb5320880e24d9d56eea5367eb5f2de23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:38 +0000

    Add package plone.restapi/8.17.0/plone.restapi-8.17.0.tar.gz

---
+++ packages/plone.restapi/plone.restapi-8.17.0.tar.gz/plone.restapi-8.17.0/src/plone/restapi/serializer/expansion.py
@@ -0,0 +1,25 @@
+from plone.restapi.interfaces import IExpandableElement
+from zope.component import getAdapters
+
+
+def expandable_elements(context, request):
+    """Returns a dict containing all expandable elements."""
+    expands = request.form.get("expand", "").split(",")
+    elements = getAdapters((context, request), IExpandableElement)
+    res = {}
+    for element in elements:
+        if element[0] in expands:
+            update_dict_recursively(res, element[1](expand=True))
+        else:
+            update_dict_recursively(res, element[1](expand=False))
+    return {"@components": res}
+
+
+def update_dict_recursively(d, u):
+    for key, value in u.items():
+        if isinstance(value, dict):
+            r = update_dict_recursively(d.get(key, {}), value)
+            d[key] = r
+        else:
+            d[key] = u[key]
+    return d
