22f92f8ea0b1353e5ca56b5999159d3c9b777f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:05 +0000

    Add package yadism/0.10.1/yadism-0.10.1.tar.gz

---
+++ packages/yadism/yadism-0.10.1.tar.gz/yadism-0.10.1/src/yadism/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+.. todo::
+    docs
+"""
+__version__ = "0.10.1"
+
+from .runner import Runner
+
+
+def run_yadism(theory: dict, observables: dict):
+    """
+    .. todo::
+        - decide the purpose
+        - implement
+        - docs
+    """
+    runner = Runner(theory, observables)
+    return runner.get_result()
