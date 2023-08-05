48e6a8402a2db253d8a3128a62e7c85cbf38b4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:05 +0000

    Add package pyorange/2.1.0/pyorange-2.1.0.tar.gz

---
+++ packages/pyorange/pyorange-2.1.0.tar.gz/pyorange-2.1.0/pyorange/anomaly.py
@@ -0,0 +1,7 @@
+# Module: Anomaly
+# Author: satish kumar <satishsriram369@gmail.com>
+# License: MIT
+# Release: PyOrange 2.1
+# Last modified : 25/12/2021
+
+from pycaret.anomaly import *
---
+++ packages/pyorange/pyorange-2.1.0.tar.gz/pyorange-2.1.0/pyorange/arules.py
@@ -0,0 +1,7 @@
+# Module: Arules
+# Author: satish kumar <satishsriram369@gmail.com>
+# License: MIT
+# Release: PyOrange 2.1
+# Last modified : 25/12/2020
+
+from pycaret.arules import *
