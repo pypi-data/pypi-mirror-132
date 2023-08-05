9f96438e67dc0903610c2002b78bbbf4235bf45b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:50 +0000

    Add package warsaw-data-api/0.2.0/warsaw_data_api-0.2.0-py2.py3-none-any.whl

---
+++ packages/warsaw-data-api/warsaw_data_api-0.2.0-py2.py3-none-any.whl/warsaw_data_api/__init__.py
@@ -0,0 +1,23 @@
+from .session_factory import SessionFactory
+
+
+__author__ = "Radoslaw Wielonski"
+__version__ = "0.2.0"
+
+DEFAULT_SESSION = None
+
+
+def setup_default_session(**kwargs):
+    global DEFAULT_SESSION
+    DEFAULT_SESSION = SessionFactory(**kwargs)
+
+
+def _get_default_session():
+    if DEFAULT_SESSION is None:
+        setup_default_session()
+
+    return DEFAULT_SESSION
+
+
+def client(*args, **kwargs):
+    return _get_default_session().client(*args, **kwargs)
