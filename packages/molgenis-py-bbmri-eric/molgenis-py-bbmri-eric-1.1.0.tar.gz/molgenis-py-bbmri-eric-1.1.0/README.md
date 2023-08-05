680b3c55b15000f8bf57708b3a4935af17231605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:52 +0000

    Add package molgenis-py-bbmri-eric/1.1.0/molgenis-py-bbmri-eric-1.1.0.tar.gz

---
+++ packages/molgenis-py-bbmri-eric/molgenis-py-bbmri-eric-1.1.0.tar.gz/molgenis-py-bbmri-eric-1.1.0/README.md
@@ -0,0 +1,100 @@
+# molgenis-py-bbmri-eric
+
+MOLGENIS Python tooling for BBMRI-ERIC.
+
+## Description
+This library contains tools for the MOLGENIS BBMRI-ERIC Directory that help with
+staging and publishing the data of the national nodes. **Staging** is the process of copying
+data from a national node's external server (for example [BBMRI-NL](https://catalogue.bbmri.nl/menu/main/home)) to
+the staging area on the ERIC directory. Not all national nodes have external servers; these
+do not need to be staged. **Publishing** is the process of copying and combining the data from the staging areas
+to the public combined tables of the directory.
+
+## Usage
+
+These tools can be used as a library in a script. Start by installing the library with
+`pip install molgenis-py-bbmri-eric`.
+
+```python
+from molgenis.bbmri_eric.bbmri_client import EricSession
+from molgenis.bbmri_eric.eric import Eric
+
+# First, initialise an EricSession (an extension of the molgenis-py-client Session)
+session = EricSession(url="<DIRECTORY_URL>")
+session.login("<USERNAME>", "<PASSWORD>")
+
+# Get the nodes you want to work with
+nodes_to_stage = session.get_external_nodes(["NL", "BE"])
+nodes_to_publish = session.get_nodes() # all nodes
+
+# Create an Eric instance and use that to perform the desired actions
+eric = Eric(session)
+staging_report = eric.stage_external_nodes(nodes_to_stage)
+publishing_report = eric.publish_nodes(nodes_to_publish)
+
+if publishing_report.has_errors():
+    raise ValueError("Some nodes did not publish correctly")
+```
+
+If you want to use the data of a node for another purpose, you can use the `EricSession`
+and `ExternalServerSession` to retrieve these.
+
+```python
+from molgenis.bbmri_eric.bbmri_client import EricSession, ExternalServerSession
+from molgenis.bbmri_eric.model import NodeData
+
+# Get the staging and published data of NL from the directory
+session = EricSession(url="<DIRECTORY_URL")
+nl = session.get_nodes(["NL"])
+nl_staging_data: NodeData = session.get_staging_node_data(nl)
+nl_published_data: NodeData = session.get_published_node_data(nl)
+
+# Get the data from the external server of NL
+external_session = ExternalServerSession(node=nl)
+nl_external_data: NodeData = external_session.get_node_data()
+
+# Now you can use the NodeData objects as you wish
+for person in nl_external_data.persons.rows:
+    print(person)
+```
+
+
+## For developers
+This project uses [pre-commit](https://pre-commit.com/) and [pipenv](https://pypi.org/project/pipenv/) for the development workflow.
+
+Install pre-commit and pipenv if you haven't already:
+```
+pip install pre-commit
+pip install pipenv
+```
+
+Install the git commit hooks:
+```
+pre-commit install
+```
+
+Create an environment and install the package including all (dev) dependencies:
+```
+pipenv install --dev
+```
+
+Enter the environment:
+```
+pipenv shell
+```
+
+Build and run the tests:
+```
+tox
+```
+
+The package's command-line entry point is already installed
+```
+eric -h
+```
+
+
+## Note
+
+This project has been set up using PyScaffold 4.0.2. For details and usage
+information on PyScaffold see https://pyscaffold.org/.
---
+++ packages/molgenis-py-bbmri-eric/molgenis-py-bbmri-eric-1.1.0.tar.gz/molgenis-py-bbmri-eric-1.1.0/tests/test_enricher.py
@@ -0,0 +1,81 @@
+from unittest.mock import MagicMock
+
+from molgenis.bbmri_eric.enricher import Enricher
+from molgenis.bbmri_eric.model import QualityInfo
+from molgenis.bbmri_eric.printer import Printer
+
+
+def test_enricher_node_codes(node_data):
+    for table in node_data.import_order:
+        assert "national_node" not in table.rows[0]
+
+    Enricher(node_data, MagicMock(), Printer(), MagicMock())._set_national_node_code()
+
+    for table in node_data.import_order:
+        assert table.rows[0]["national_node"] == "NO"
+
+
+def test_enricher_commercial_use():
+    node_data = MagicMock()
+    node_data.collections.rows = [
+        {"biobank": "biobank1", "collaboration_commercial": True},
+        {"biobank": "biobank1", "collaboration_commercial": False},
+        {"biobank": "biobank1"},
+        {"biobank": "biobank2", "collaboration_commercial": True},
+        {"biobank": "biobank2", "collaboration_commercial": False},
+        {"biobank": "biobank2"},
+        {"biobank": "biobank3", "collaboration_commercial": True},
+        {"biobank": "biobank3", "collaboration_commercial": False},
+        {"biobank": "biobank3"},
+    ]
+    node_data.biobanks.rows_by_id = {
+        "biobank1": dict(),
+        "biobank2": {"collaboration_commercial": True},
+        "biobank3": {"collaboration_commercial": False},
+    }
+
+    Enricher(node_data, MagicMock(), Printer(), MagicMock())._set_commercial_use_bool()
+
+    assert node_data.collections.rows[0]["commercial_use"] is True
+    assert node_data.collections.rows[1]["commercial_use"] is False
+    assert node_data.collections.rows[2]["commercial_use"] is True
+    assert node_data.collections.rows[3]["commercial_use"] is True
+    assert node_data.collections.rows[4]["commercial_use"] is False
+    assert node_data.collections.rows[5]["commercial_use"] is True
+    assert node_data.collections.rows[6]["commercial_use"] is False
+    assert node_data.collections.rows[7]["commercial_use"] is False
+    assert node_data.collections.rows[8]["commercial_use"] is False
+
+
+def test_enricher_quality(node_data):
+    q_info = QualityInfo(
+        biobanks={
+            "bbmri-eric:ID:NO_BIOBANK1": ["quality1", "quality2"],
+            "bbmri-eric:ID:NO_CoronaTrondelag": ["quality3"],
+        },
+        collections={
+            "bbmri-eric:ID:NO_bbmri-eric:ID:NO_CancerBiobankOUH:collection"
+            ":all_samples_samples": ["quality1"]
+        },
+    )
+
+    Enricher(node_data, q_info, Printer(), MagicMock())._set_quality_info()
+
+    assert node_data.biobanks.rows_by_id["bbmri-eric:ID:NO_BIOBANK1"]["quality"] == [
+        "quality1",
+        "quality2",
+    ]
+    assert "quality" not in node_data.biobanks.rows_by_id["bbmri-eric:ID:NO_Janus"]
+    assert node_data.biobanks.rows_by_id["bbmri-eric:ID:NO_CoronaTrondelag"][
+        "quality"
+    ] == ["quality3"]
+    assert node_data.collections.rows_by_id[
+        "bbmri-eric:ID:NO_bbmri-eric:ID:NO_CancerBiobankOUH:collection"
+        ":all_samples_samples"
+    ]["quality"] == ["quality1"]
+    assert (
+        "quality"
+        not in node_data.collections.rows_by_id[
+            "bbmri-eric:ID:NO_moba:collection:all_samples"
+        ]
+    )
