d752bc9a0edc878f42c010f35cc5e5126c3e029a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:17 +0000

    Add package pypipet/0.0.14a0/pypipet-0.0.14a0-py3-none-any.whl

---
+++ packages/pypipet/pypipet-0.0.14a0-py3-none-any.whl/pypipet/cli/__init__.py
@@ -0,0 +1,14 @@
+import click, logging
+from .cli import cli
+from pypipet.core.logging import setup_logging
+
+from . import ( 
+    init,
+    catalog,
+    product,
+    template,
+    order,
+    inventory,
+    fulfillment,
+    static
+)
