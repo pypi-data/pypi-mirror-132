51fceb511ab5482eb16b7599401a6f67d2281827
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:15 +0000

    Add package pybpjs/0.0.4/pybpjs-0.0.4.tar.gz

---
+++ packages/pybpjs/pybpjs-0.0.4.tar.gz/pybpjs-0.0.4/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+	name="pybpjs",
+	version="0.0.4",
+	author="Moriz",
+	author_email="morizbebenk@gmail.com",
+	description="Aplikasi Python yang digunakan untuk menangani proses dekripsi respon data dari bridging BPJS VClaim v2.0 (Encrypted Version). Support VClaim v1 / v1.1 dan Antrean RS.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/morizbebenk/pybpjs",
+    project_urls={
+        "Bug Tracker": "https://github.com/morizbebenk/pybpjs/issues",
+    },
+    classifiers=[
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    license="MIT",
+    keywords="bpjs vclaim vclaim-rest vclaim-encrypted",
+    install_requires=["lzstring", "requests", "pycryptodome"]
+)
