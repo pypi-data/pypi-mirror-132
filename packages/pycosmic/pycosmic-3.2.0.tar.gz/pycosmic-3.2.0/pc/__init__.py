9951645e9d9152583f9db86a381ffca41be9e1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:29 +0000

    Add package pycosmic/3.2.0/pycosmic-3.2.0.tar.gz

---
+++ packages/pycosmic/pycosmic-3.2.0.tar.gz/pycosmic-3.2.0/pc/__init__.py
@@ -0,0 +1,6 @@
+from pc.file_crud import *
+from pc.code import * 
+from pc.utils import *
+from pc.qr import * 
+from pc.models import *
+from pc.detect import * 
---
+++ packages/pycosmic/pycosmic-3.2.0.tar.gz/pycosmic-3.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup ,find_packages
+
+
+
+setup(
+
+name = "pycosmic",
+version="3.2.0",
+description="Packages That Shortens The Code  ",
+author="Rishabh",
+author_email="rishivisionworld@gmail.com",
+install_requires=['qrcode','opencv-python'],
+packages=find_packages(),
+
+
+)
