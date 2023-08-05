99c1f9fd6d9b710b3f1686dda7b50120a6410571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:38 +0000

    Add package ConnTwsFunctions-projectX-IbTws/0.0.3/ConnTwsFunctions_projectX_IbTws-0.0.3-py3-none-any.whl

---
+++ packages/ConnTwsFunctions-projectX-IbTws/ConnTwsFunctions_projectX_IbTws-0.0.3-py3-none-any.whl/tws_integrated_functions/account_summary.py
@@ -0,0 +1,21 @@
+from src.tws_integrated_functions.appConnectionTws import ConnectionTwsApi
+from ibapi.utils import iswrapper
+
+
+class AccountSummaryWrapper(ConnectionTwsApi):
+
+    def __init__(self, addr, port, client_id):
+        super().__init__(addr=addr, port=port, client_id=client_id)
+        self.funds = 0.0
+
+    @iswrapper
+    def accountSummary(self, req_id, acct, tag, val, currency):
+        """ Called in response to reqAccountSummary """
+        if tag == 'AvailableFunds':
+            print('Account {}: available funds = {}: Currency = {}'.format(acct, val, currency))
+            self.funds = float(val)
+
+    @iswrapper
+    def error(self, reqId, code, msg):
+        print('Error {}: {}'.format(code, msg))
+
