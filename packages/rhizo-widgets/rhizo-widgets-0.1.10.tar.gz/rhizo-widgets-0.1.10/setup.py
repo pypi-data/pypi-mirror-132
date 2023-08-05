7d3005872a9d21ebe3eedca76607f9ca7e42c7f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:22 +0000

    Add package rhizo-widgets/0.1.10/rhizo-widgets-0.1.10.tar.gz

---
+++ packages/rhizo-widgets/rhizo-widgets-0.1.10.tar.gz/rhizo-widgets-0.1.10/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rhizo_widgets']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ipywidgets>=7.6.5,<8.0.0']
+
+setup_kwargs = {
+    'name': 'rhizo-widgets',
+    'version': '0.1.10',
+    'description': '',
+    'long_description': None,
+    'author': 'Andy Jefferson',
+    'author_email': 'andy@rhizo.co',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
