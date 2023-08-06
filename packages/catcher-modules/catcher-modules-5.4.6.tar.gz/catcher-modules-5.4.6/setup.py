c8cb347f9bc4bca7c5683c75d82c91412e215a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:08:53 +0000

    Add package catcher-modules/5.4.6/catcher-modules-5.4.6.tar.gz

---
+++ packages/catcher-modules/catcher-modules-5.4.6.tar.gz/catcher-modules-5.4.6/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+
+import catcher_modules
+
+
+def get_requirements() -> list:
+    with open('requirements.txt', 'r') as f:
+        return f.readlines()
+
+
+def extras() -> dict:
+    modules = {
+        'kafka': ["pykafka==2.8.*"],
+        'couchbase': ["couchbase==3.2.*"],
+        'postgres': ["sqlalchemy==1.3.*", "psycopg2==2.8.*"],
+        'mssql': ["pyodbc==4.0.*", "sqlalchemy==1.3.*"],
+        'mysql': ["cython==0.29.*", "pymysql==0.9.*", "sqlalchemy==1.3.*"],
+        'oracle': ["sqlalchemy==1.3.*", "cx_oracle==7.1.*"],
+        'sqlite': ["sqlalchemy==1.3.*"],
+        'redis': ["redis==3.2.*"],
+        'mongodb': ["pymongo==3.8.*"],
+        'docker': ["docker==3.7.*"],
+        'elastic': ["elasticsearch==7.13.*"],
+        's3': ["boto3==1.9.*"],
+        'rabbit': ["pika==0.13.1"],
+        'email': ["imbox==0.9.*"],
+        'marketo': ["marketorestpython==0.5.*"],
+        'airflow': ["cryptography==2.8.*"],
+        'selenium': ["selenium==3.141.*"],
+        'salesforce': ["simple-salesforce==1.11.*"]
+    }
+    modules['all'] = list(set([item for sublist in modules.values() for item in sublist]))
+    # don't try to install couchbase in travis
+    modules['travis'] = [m for m in modules['all'] if not m.startswith('couchbase')]
+    return modules
+
+
+setup(name=catcher_modules.APPNAME,
+      version=catcher_modules.APPVSN,
+      description='Additional modules for catcher.',
+      author=catcher_modules.APPAUTHOR,
+      author_email='valerii.tikhonov@gmail.com',
+      url='https://github.com/comtihon/catcher_modules',
+      packages=find_packages(),
+      install_requires=get_requirements(),
+      include_package_data=True,
+      package_data={'catcher_modules': ['resources/*']},
+      classifiers=[
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Topic :: Software Development :: Testing'
+      ],
+      extras_require=extras(),
+      tests_require=['mock', 'pytest', 'requests']
+      )
