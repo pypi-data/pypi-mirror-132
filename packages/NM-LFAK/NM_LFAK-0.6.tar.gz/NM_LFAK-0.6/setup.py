98c27b1a2e014eb5ee16b8d2f2f07bbedb7ff1af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:55 +0000

    Add package NM-LFAK/0.6/NM_LFAK-0.6.tar.gz

---
+++ packages/NM-LFAK/NM_LFAK-0.6.tar.gz/NM_LFAK-0.6/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='NM_LFAK',
+      version='0.6',
+      description='Numerical methods',
+      packages=find_packages(),
+      author_email='vovalagutov1111@yandex.ru',
+      zip_safe=False,
+      )
