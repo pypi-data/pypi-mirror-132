96dfbbe2c0d2c91ba47e3f56a11429d98d9fe163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:18 +0000

    Add package sotl/0.1.dev0/sotl-0.1.dev0.tar.gz

---
+++ packages/sotl/sotl-0.1.dev0.tar.gz/sotl-0.1.dev0/setup.py
@@ -0,0 +1,9 @@
+from distutils. core import setup
+setup(
+
+name= 'sotl',
+version='0.1dev',
+packages=[ 'sotl' , ],
+license='MIT',
+long_description=open( r'C:\Users\PD55-USB3L\Desktop\Python\sotl_lib\README.txt ' ) . read(),
+)
