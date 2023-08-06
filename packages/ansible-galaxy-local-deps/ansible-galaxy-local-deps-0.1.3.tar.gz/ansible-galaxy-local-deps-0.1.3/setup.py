856833c7fe9c98ac22f05847ee9da4f9525d225b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:00 +0000

    Add package ansible-galaxy-local-deps/0.1.3/ansible-galaxy-local-deps-0.1.3.tar.gz

---
+++ packages/ansible-galaxy-local-deps/ansible-galaxy-local-deps-0.1.3.tar.gz/ansible-galaxy-local-deps-0.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(name='ansible-galaxy-local-deps',
+      version='0.1.3',
+      description='CLI for interacting with Ansible roles and their CIs',
+      url='http://github.com/andrewrothstein/ansible-galaxy-local-deps',
+      author='Andrew Rothstein',
+      author_email='andrew.rothstein@gmail.com',
+      license='MIT',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      python_requires='>=3.5',
+      install_requires=['PyYAML'],
+      extras_require={
+          "test": ['nose2']
+      },
+      zip_safe=False,
+      entry_points={
+        'console_scripts': [
+            'ansible-galaxy-local-deps-install=ansiblegalaxylocaldeps.installdeps:main',
+            'ansible-galaxy-local-deps-write=ansiblegalaxylocaldeps.writedeps:main',
+            'gendottravis=ansiblegalaxylocaldeps.gendottravis:main',
+            'gengithubactions=ansiblegalaxylocaldeps.gengithubactions:main',
+            'ansible-galaxy-local-deps-change-dep=ansiblegalaxylocaldeps.changedep:main',
+            'ansible-galaxy-local-deps-add-os=ansiblegalaxylocaldeps.addos:main',
+            'ansible-galaxy-local-deps-drop-os=ansiblegalaxylocaldeps.dropos:main'
+        ]
+      }
+      )
