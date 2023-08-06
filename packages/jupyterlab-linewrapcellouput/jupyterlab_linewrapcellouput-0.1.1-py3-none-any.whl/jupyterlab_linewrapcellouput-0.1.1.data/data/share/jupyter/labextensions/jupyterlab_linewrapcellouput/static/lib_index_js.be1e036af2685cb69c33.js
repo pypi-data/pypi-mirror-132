28d9565ffbcba2df272a7dfbaa5323cfd6a0c580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:41 +0000

    Add package jupyterlab-linewrapcellouput/0.1.1/jupyterlab_linewrapcellouput-0.1.1-py3-none-any.whl

---
+++ packages/jupyterlab-linewrapcellouput/jupyterlab_linewrapcellouput-0.1.1-py3-none-any.whl/jupyterlab_linewrapcellouput-0.1.1.data/data/share/jupyter/labextensions/jupyterlab_linewrapcellouput/static/lib_index_js.be1e036af2685cb69c33.js
@@ -0,0 +1,86 @@
+"use strict";
+(self["webpackChunkjupyterlab_linewrapcellouput"] = self["webpackChunkjupyterlab_linewrapcellouput"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "ButtonLineWrapCellOutput": () => (/* binding */ ButtonLineWrapCellOutput),
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _lumino_disposable__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @lumino/disposable */ "webpack/sharing/consume/default/@lumino/disposable");
+/* harmony import */ var _lumino_disposable__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_lumino_disposable__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @jupyterlab/apputils */ "webpack/sharing/consume/default/@jupyterlab/apputils");
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_1__);
+
+
+function JupyterLabLineWrapCellOuputOn() {
+    var divs = document.querySelectorAll(".jp-OutputArea-output pre");
+    for (let i = 0; i < divs.length; i++) {
+        var div = divs[i];
+        if (div.style['whiteSpace'] != "pre")
+            div.style["whiteSpace"] = "pre";
+    }
+}
+function JupyterLabLineWrapCellOuputOff() {
+    var divs = document.querySelectorAll(".jp-OutputArea-output pre");
+    for (let i = 0; i < divs.length; i++) {
+        var div = divs[i];
+        if (div.style['whiteSpace'] != "pre-wrap")
+            div.style["whiteSpace"] = "pre-wrap";
+    }
+}
+/**
+ * Initialization data for the jupyterlab_linewrapcelloutput extension.
+ */
+const plugin = {
+    id: 'jupyterlab_linewrapcelloutput:plugin',
+    autoStart: true,
+    activate: (app) => {
+        console.log('JupyterLab extension jupyterlab_linewrapcelloutput is activated!');
+        app.docRegistry.addWidgetExtension('Notebook', new ButtonLineWrapCellOutput());
+    }
+};
+class ButtonLineWrapCellOutput {
+    createNew(panel, context) {
+        const triggerLineWrapCellOutput = () => {
+            if (SET) {
+                SET = false;
+                console.log('Extension jupyterlab_linewrapcelloutput disabled');
+                clearInterval(t);
+                JupyterLabLineWrapCellOuputOff();
+                if (button.hasClass('selected'))
+                    button.removeClass('selected');
+            }
+            else {
+                SET = true;
+                console.log('Extension jupyterlab_linewrapcelloutput enabled');
+                t = setInterval(JupyterLabLineWrapCellOuputOn, 10);
+                button.addClass('selected');
+            }
+        };
+        const button = new _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_1__.ToolbarButton({
+            className: 'buttonLineWrapCellOuput',
+            iconClass: 'wll-WrapIcon',
+            label: 'wrap',
+            onClick: triggerLineWrapCellOutput,
+            tooltip: 'Line Wrap Cell Ouput'
+        });
+        panel.toolbar.insertItem(10, 'LineWrapCellOutput', button);
+        var SET = true;
+        var t = setInterval(JupyterLabLineWrapCellOuputOn, 10);
+        button.addClass('selected');
+        return new _lumino_disposable__WEBPACK_IMPORTED_MODULE_0__.DisposableDelegate(() => { button.dispose(); });
+    }
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (plugin);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.be1e036af2685cb69c33.js.map
---
+++ packages/jupyterlab-linewrapcellouput/jupyterlab_linewrapcellouput-0.1.1-py3-none-any.whl/jupyterlab_linewrapcellouput/labextension/static/style_index_js.3890cbb301d609685760.js
@@ -0,0 +1,93 @@
+"use strict";
+(self["webpackChunkjupyterlab_linewrapcellouput"] = self["webpackChunkjupyterlab_linewrapcellouput"] || []).push([["style_index_js"],{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./style/base.css":
+/*!**************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./style/base.css ***!
+  \**************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/cssWithMappingToString.js */ "./node_modules/css-loader/dist/runtime/cssWithMappingToString.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/getUrl.js */ "./node_modules/css-loader/dist/runtime/getUrl.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _left_align_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./left-align.png */ "./style/left-align.png");
+// Imports
+
+
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
+var ___CSS_LOADER_URL_REPLACEMENT_0___ = _node_modules_css_loader_dist_runtime_getUrl_js__WEBPACK_IMPORTED_MODULE_2___default()(_left_align_png__WEBPACK_IMPORTED_MODULE_3__["default"]);
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, "/** Toggled button background */\r\n.selected .buttonLineWrapCellOuput {\r\n    background-color: var(--jp-layout-color2);\r\n}\r\n\r\n/* Toolbar icon */\r\n.wll-WrapIcon {\r\n    background-color: var(--jp-inverse-layout-color3);\r\n    -webkit-mask-image: url(" + ___CSS_LOADER_URL_REPLACEMENT_0___ + ");\r\n    mask-image: url(" + ___CSS_LOADER_URL_REPLACEMENT_0___ + ");\r\n    -webkit-mask-size: contain;\r\n    mask-size: contain;\r\n}", "",{"version":3,"sources":["webpack://./style/base.css"],"names":[],"mappings":"AAAA,+BAA+B;AAC/B;IACI,yCAAyC;AAC7C;;AAEA,iBAAiB;AACjB;IACI,iDAAiD;IACjD,2DAAyC;IACzC,mDAAiC;IACjC,0BAA0B;IAC1B,kBAAkB;AACtB","sourcesContent":["/** Toggled button background */\r\n.selected .buttonLineWrapCellOuput {\r\n    background-color: var(--jp-layout-color2);\r\n}\r\n\r\n/* Toolbar icon */\r\n.wll-WrapIcon {\r\n    background-color: var(--jp-inverse-layout-color3);\r\n    -webkit-mask-image: url(./left-align.png);\r\n    mask-image: url(./left-align.png);\r\n    -webkit-mask-size: contain;\r\n    mask-size: contain;\r\n}"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./style/left-align.png":
+/*!******************************!*\
+  !*** ./style/left-align.png ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "c96d3b4564cb09356f70b9d6cd250a68cd4850ab2ddcff33951ff6d26fd7e5c7.png");
+
+/***/ }),
+
+/***/ "./style/base.css":
+/*!************************!*\
+  !*** ./style/base.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../node_modules/css-loader/dist/cjs.js!./base.css */ "./node_modules/css-loader/dist/cjs.js!./style/base.css");
+
+            
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"], options);
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"].locals || {});
+
+/***/ }),
+
+/***/ "./style/index.js":
+/*!************************!*\
+  !*** ./style/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _base_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base.css */ "./style/base.css");
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=style_index_js.3890cbb301d609685760.js.map
