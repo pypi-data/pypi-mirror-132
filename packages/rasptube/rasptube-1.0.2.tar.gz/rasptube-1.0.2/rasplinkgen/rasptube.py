c306dd371392927a4df5ea106c1a1592650b3848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:13 +0000

    Add package rasptube/1.0.2/rasptube-1.0.2.tar.gz

---
+++ packages/rasptube/rasptube-1.0.2.tar.gz/rasptube-1.0.2/rasplinkgen/rasptube.py
@@ -0,0 +1,11 @@
+import os
+
+def conTup_1(tup):
+    com_1 = ''
+    for item in tup:
+        com_1 = com_1 + item
+    return com_1
+com_1_tup = 'sudo youtube-dl -g -f 22 "', urlTup, '"'
+com_1 = conTup_1(com_1_tup)
+os.system(com_1)
+
