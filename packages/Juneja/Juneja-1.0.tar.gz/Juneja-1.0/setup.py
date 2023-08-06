a3fa94cec649ff9be032c369645db8e6d11ab080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:20 +0000

    Add package Juneja/1.0/Juneja-1.0.tar.gz

---
+++ packages/Juneja/Juneja-1.0.tar.gz/Juneja-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="Juneja",
+version="1.0",
+description="This is a Python Module made by Ansh Juneja",
+long_description="All in One basic module which performs basic Mathematic Calculations Along with some functions which is present in math module and it will also Generate Random Numbers on your Demand, Avoid Multiple Modules and Import this with a Single statement... If You Have Any Queries Regarding this Module then You can Email Me at- ansh.63@live.com ",
+author="Ansh Juneja",
+author_email="ansh.63@live.com",
+packages=["Juneja"],
+install_requires=[])
