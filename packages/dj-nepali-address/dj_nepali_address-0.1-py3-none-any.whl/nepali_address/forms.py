4d92970765505b69e5d5ff33473ec98f58bc3139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:50 +0000

    Add package dj-nepali-address/0.1/dj_nepali_address-0.1-py3-none-any.whl

---
+++ packages/dj-nepali-address/dj_nepali_address-0.1-py3-none-any.whl/nepali_address/forms.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.forms import ModelForm
+from .models import Province, District, Municipality
+
+class ProvinceForm(ModelForm):
+    class Meta:
+        model = Province
+        fields = ['name']
+
+class DistrictForm(ModelForm):
+    class Meta:
+        model = District
+        fields = ['name', 'province']
+
+class MunicipalityForm(ModelForm):
+    class Meta:
+        model = Municipality
+        fields = ['name', 'district']
