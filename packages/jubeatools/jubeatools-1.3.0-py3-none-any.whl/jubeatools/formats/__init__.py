9056fc0a74cf61a0c55a358ec6efe4646944852e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:57 +0000

    Add package jubeatools/1.3.0/jubeatools-1.3.0-py3-none-any.whl

---
+++ packages/jubeatools/jubeatools-1.3.0-py3-none-any.whl/jubeatools/formats/__init__.py
@@ -0,0 +1,5 @@
+"""
+Module containing all the load/dump code for all file formats
+"""
+from .enum import Format
+from .formats import DUMPERS, LOADERS
---
+++ packages/jubeatools/jubeatools-1.3.0-py3-none-any.whl/jubeatools/formats/memon/__init__.py
@@ -0,0 +1,20 @@
+"""
+memon (memo + json)
+□□□■——◁
+
+memon is a json-based jubeat chart set format designed to be easier to
+parse than existing "memo-like" formats (memo, youbeat, etc ...).
+
+https://github.com/Stepland/memon
+"""
+
+from .v0 import (
+    dump_memon_0_1_0,
+    dump_memon_0_2_0,
+    dump_memon_0_3_0,
+    dump_memon_legacy,
+    load_memon_0_1_0,
+    load_memon_0_2_0,
+    load_memon_0_3_0,
+    load_memon_legacy,
+)
