66d69257b55654bf0b4d227a14ab25b45350949d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:32 +0000

    Add package proxyauthenticator/0.0.7/proxyauthenticator-0.0.7.tar.gz

---
+++ packages/proxyauthenticator/proxyauthenticator-0.0.7.tar.gz/proxyauthenticator-0.0.7/README.md
@@ -0,0 +1,7 @@
+# **What is it?**
+
+**proxyauthenticator** is a Python package that support
+anyone to add proxy to script easily when working in any local or cloud
+environment which uses proxy for in and out connections.
+
+
---
+++ packages/proxyauthenticator/proxyauthenticator-0.0.7.tar.gz/proxyauthenticator-0.0.7/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+from setuptools import setup
+from setuptools import find_namespace_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="proxyauthenticator",
+    version="0.0.7",
+    author="Nuwan Chaminda",
+    author_email="lokupodda123@gmail.com",
+    description="Proxy Authenticator for python projects",
+    long_description="file: README.md",
+    long_description_content_type="text/markdown",
+    url="https://github.com/NuwanChaminda/proxyauthenticator",
+
+    # package_dir={"": "src"},
+    # Specify folder content.
+    packages=["authenticator"],
+    include_package_data=True,
+    install_requires=["setuptools", "os", "wheel"],
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        # Programming Languages Used..
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ]
+
+)
