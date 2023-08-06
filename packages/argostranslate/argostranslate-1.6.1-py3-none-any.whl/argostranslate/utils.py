91ab9557cc712ea7cbc5afa10d13c42ecd63b507
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:39 +0000

    Add package argostranslate/1.6.1/argostranslate-1.6.1-py3-none-any.whl

---
+++ packages/argostranslate/argostranslate-1.6.1-py3-none-any.whl/argostranslate/utils.py
@@ -0,0 +1,15 @@
+import logging
+
+from argostranslate import settings
+
+logging.basicConfig(level=logging.DEBUG if settings.debug else logging.INFO)
+
+
+def info(*argv):
+    logging.debug(str(argv))
+
+
+def error(*argv):
+    logging.error(str(argv))
+
+
