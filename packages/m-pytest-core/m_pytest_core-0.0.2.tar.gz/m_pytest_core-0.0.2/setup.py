d7c10812fd09da882318bd9e623b4ed5cef90851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:34 +0000

    Add package m-pytest-core/0.0.2/m_pytest_core-0.0.2.tar.gz

---
+++ packages/m-pytest-core/m_pytest_core-0.0.2.tar.gz/m_pytest_core-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='m_pytest_core',
+    version='0.0.2',
+    description='The framework core from pytest.',
+    install_requires=['allure-pytest', 'allure-python-commons', 'testrail-api', 'SQLAlchemy'],
+    packages=['m_pytest_core'],
+    url='https://github.com/AlekseiMiasnikov/pytest_core.git',
+)
