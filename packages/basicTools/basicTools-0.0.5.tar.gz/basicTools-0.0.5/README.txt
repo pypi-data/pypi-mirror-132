6ab55a6605552a97b127554d74d675920a7f998e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:09 +0000

    Add package basicTools/0.0.5/basicTools-0.0.5.tar.gz

---
+++ packages/basicTools/basicTools-0.0.5.tar.gz/basicTools-0.0.5/README.txt
@@ -0,0 +1,8 @@
+A module to do basic problems
+
+1.EmailValidation\n
+2.Roots Of Quadratic Equation\n
+3.half pyramid using character and Numbers\n
+4.full pyramid using character and Numbers\n
+5.pascal triangle\n
+6.floyd triangle\n
---
+++ packages/basicTools/basicTools-0.0.5.tar.gz/basicTools-0.0.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 1 - Planning',
+  'Intended Audience :: Developers',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='basicTools',
+  version='0.0.5',
+  description='A very basic tool',
+  long_description_content_type ='text/markdown',
+  long_description=open('README.txt').read() + "\n",
+  url='',  
+  author='Jenifar',
+  author_email='jenifar200227@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords=['email','basicTools'],
+  packages=find_packages(),
+  install_requires=['regex','math'] 
+)
