fe0aa5eca345ed84c660c2ec8c4575b561f1db26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:42:31 +0000

    Add package DDetector/0.0.2/DDetector-0.0.2.tar.gz

---
+++ packages/DDetector/DDetector-0.0.2.tar.gz/DDetector-0.0.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+    name="DDetector",
+    version="0.0.2",
+    author="Jones Lin",
+    author_email="jonneslin@gmail.com",
+    description="Data distribution detector",
+    url="https://github.com/JonnesLin/distribution_detection",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'scikit-learn'
+    ]
+)
