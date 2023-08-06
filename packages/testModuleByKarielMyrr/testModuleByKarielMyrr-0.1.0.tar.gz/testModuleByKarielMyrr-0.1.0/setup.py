67e405ab957cdf26bd5cb026e9044e209389c026
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:39 +0000

    Add package testModuleByKarielMyrr/0.1.0/testModuleByKarielMyrr-0.1.0.tar.gz

---
+++ packages/testModuleByKarielMyrr/testModuleByKarielMyrr-0.1.0.tar.gz/testModuleByKarielMyrr-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='testModuleByKarielMyrr',
+    version='0.1.0',    
+    description='Test module for Data Analysis.',
+    author='Karile_Myrr',
+    author_email='kirillant2013@gmail.com',
+    url='https://github.com/Kariel-Myrr/kariel_andan_test_module.git',
+    packages=['testModuleByKarielMyrr'],
+    install_requires=['numpy>=1.9.0'],
+
+    classifiers=[ 
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
---
+++ packages/testModuleByKarielMyrr/testModuleByKarielMyrr-0.1.0.tar.gz/testModuleByKarielMyrr-0.1.0/testModuleByKarielMyrr/__init__.py
@@ -0,0 +1,9 @@
+"""
+module_kariel1
+
+Test module for Data Analysis.
+"""
+
+__version__ = "1.0"
+__author__ = "Antonov Kirill"
+print("you have imported module for statistics.")
