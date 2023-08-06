7b5158411e6f3e1f5b44ec587212edfb9ecd8a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:15 +0000

    Add package feapder/1.7.1/feapder-1.7.1.tar.gz

---
+++ packages/feapder/feapder-1.7.1.tar.gz/feapder-1.7.1/tests/test.py
@@ -0,0 +1,16 @@
+# from feapder import Request
+# from feapder.buffer.request_buffer import RequestBuffer
+#
+# request_buffer = RequestBuffer(redis_key="xxxx")
+#
+# # 构造任务
+# request = Request("url")
+# # 添加任务
+# request_buffer.put_request(request)
+# # 推送
+# request_buffer.flush()
+#
+
+from feapder.network.user_pool.b import B
+
+B().a()
