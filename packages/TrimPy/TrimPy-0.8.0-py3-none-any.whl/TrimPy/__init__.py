187c1a822671c50024047354864efe599c29a93f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:08 +0000

    Add package TrimPy/0.8.0/TrimPy-0.8.0-py3-none-any.whl

---
+++ packages/TrimPy/TrimPy-0.8.0-py3-none-any.whl/TrimPy/__init__.py
@@ -0,0 +1,27 @@
+# TrimPy: A basic API implementation for Trimlight Select systems,
+# Copyright (C) 2021 Ethan Dye
+#
+# TrimPy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# TrimPy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with TrimPy. If not, see <https://www.gnu.org/licenses/>.
+
+from .enum.pattern import Pattern
+from .enum.trim import Trim
+from .enum.mode import Mode
+from .enum.animation import Animation
+from .messages.message import formatConnMsg, formatModeMsg, formatDispMsg, formatNameMsg, formatQueryPatternMsg
+from .messages.message import formatUpdatePatternMsg, formatPreviewPatternMsg, formatDeletePatternMsg, formatDotMsg
+from .messages.parser import parseOptions, parseQueryData
+
+__all__ = ['Pattern', 'Trim', 'Mode', 'Animation', 'formatConnMsg', 'formatModeMsg', 'formatDispMsg', 'formatNameMsg',
+           'formatQueryPatternMsg', 'formatUpdatePatternMsg', 'formatPreviewPatternMsg', 'formatDeletePatternMsg',
+           'formatDotMsg', 'parseOptions', 'parseQueryData', '__version__']
+__version__ = '0.8.0'
