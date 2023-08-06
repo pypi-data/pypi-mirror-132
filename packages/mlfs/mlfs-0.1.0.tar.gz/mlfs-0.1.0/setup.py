d7844ee2b9fe8d9fecd5c44bca674ebdd0765536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:19 +0000

    Add package mlfs/0.1.0/mlfs-0.1.0.tar.gz

---
+++ packages/mlfs/mlfs-0.1.0.tar.gz/mlfs-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='mlfs',
+    version='0.1.0',    
+    description='A package for maximum likelihood density fitting and simulation',
+    url='https://github.com/romanov360/legendary-chainsaw',
+    author='Tristan Romanov',
+    author_email='romanov360@gmail.com',
+    license='BSD 2-clause',
+    packages=['mlfs'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: Microsoft :: Windows',        
+        'Programming Language :: Python',
+    ],
+)
