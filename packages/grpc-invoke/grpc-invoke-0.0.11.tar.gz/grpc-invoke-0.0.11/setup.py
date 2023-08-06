41379edf50a84613fea3b66891327d79d9aab8cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:33 +0000

    Add package grpc-invoke/0.0.11/grpc-invoke-0.0.11.tar.gz

---
+++ packages/grpc-invoke/grpc-invoke-0.0.11.tar.gz/grpc-invoke-0.0.11/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="grpc-invoke",
+  version="0.0.11",
+  author="woody(吴冉旭)",
+  author_email="619434176@qq.com",
+  description="a grpc request tool like requests",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/wuranxu/grpc-invoke",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
