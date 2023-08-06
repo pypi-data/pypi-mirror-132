62b75863e284d02c9f1650b8878aed02efee5dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:14 +0000

    Add package optilib/1.1/optilib-1.1.tar.gz

---
+++ packages/optilib/optilib-1.1.tar.gz/optilib-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+long_description= '''Python module for optimization tasks'''
+setup(name='optilib',
+      version='1.1',
+      url='https://github.com/deaflurryth/optimlib',
+      license='MIT',
+      author='deaflurryth',
+      author_email='deaflurryth@icloud.com',
+      description='Add static script_dir() method to Path',
+      zip_safe=False)
