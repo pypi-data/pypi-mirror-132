0cf3cb6a86f0ab86d9c039c91c09f396c0e4daa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:10 +0000

    Add package ClouderaVariable/0.0.1/ClouderaVariable-0.0.1.tar.gz

---
+++ packages/ClouderaVariable/ClouderaVariable-0.0.1.tar.gz/ClouderaVariable-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(name='ClouderaVariable',
+      version='0.0.1',
+      description='Configuration path information of Cloudera related components',
+      author='zhuhs',
+      author_email='zhuhs087@163.com',
+      requires= ['numpy'],
+      packages=find_packages(),
+      license="apache 3.0"
+      )
