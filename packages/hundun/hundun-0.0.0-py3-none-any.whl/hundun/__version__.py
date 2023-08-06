c2fe92b1216439b3b185f7ecf54edbfb56cddc30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:36 +0000

    Add package hundun/0.0.0/hundun-0.0.0-py3-none-any.whl

---
+++ packages/hundun/hundun-0.0.0-py3-none-any.whl/hundun/__version__.py
@@ -0,0 +1,5 @@
+MAJOR = 0
+MINOR = 0
+MICRO = 0
+
+__version__ = f'{MAJOR}.{MINOR}.{MICRO}'
---
+++ packages/hundun/hundun-0.0.0-py3-none-any.whl/hundun/equations/lorenz.py
@@ -0,0 +1,19 @@
+from ..systems._differential import Differential as _Differential
+
+
+class Lorenz(_Differential):
+
+    def parameter(self, s=10, r=28, b=8/3):
+        self.s, self.r, self.b = s, r, b
+        self.dim = 3
+
+    def equation(self, t, u):
+        s, r, b = self.s, self.r, self.b
+
+        x, y, z = u
+
+        x_dot = s*(y - x)
+        y_dot = r*x - y - x*z
+        z_dot = x*y - b*z
+
+        return x_dot, y_dot, z_dot
