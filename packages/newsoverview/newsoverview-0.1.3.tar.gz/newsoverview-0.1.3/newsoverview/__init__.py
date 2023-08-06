d6bba3e9be14d2b0416374b387e8e89c8641ae0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:42 +0000

    Add package newsoverview/0.1.3/newsoverview-0.1.3.tar.gz

---
+++ packages/newsoverview/newsoverview-0.1.3.tar.gz/newsoverview-0.1.3/newsoverview/__init__.py
@@ -0,0 +1,10 @@
+# to init
+from .covid_news import CoronavirusNews
+from .en_world_news import EnglishWorldNews
+from .protests import Protests
+from .travel import TravelWarnings
+from .terror_monitor import TerrorNews
+from .tr_world_news import TranslatedWorldNews
+from .simple_search import SimpleSearch
+from .hospital_news import HospitalNews
+from .keywords_en import KWSearch
---
+++ packages/newsoverview/newsoverview-0.1.3.tar.gz/newsoverview-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='newsoverview',
+    version='0.1.3',
+    packages=['newsoverview'],
+    url='https://github.com/bethcha/newsoverview',
+    license='MIT',
+    author='Bethan',
+    author_email='89695634+bethcha@users.noreply.github.com',
+    description='News at a glance',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=['pygooglenews', 'deep-translator']
+)
