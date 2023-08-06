1a8ded06aa787074088cd537c039ec306b7ad004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:46 +0000

    Add package catswalk/0.0.5/catswalk-0.0.5.tar.gz

---
+++ packages/catswalk/catswalk-0.0.5.tar.gz/catswalk-0.0.5/catswalk/scraping/webdriver.py
@@ -0,0 +1,140 @@
+import logging
+from bs4 import BeautifulSoup
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from catswalk.scraping.request import CWRequest
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+class CWWebDriver:
+    def __init__(self, binary_location: str = None, executable_path: str = None, execution_env: str = "local", proxy: str = None):
+        """[summary]
+
+        Args:
+            binary_location (str): [description]
+            executable_path (str): [description]
+            execution_env (str, optional): [local, local_headless, aws]. Defaults to "local".
+            proxy (str, optional): [description]. Defaults to None.
+        """
+        options = Options()
+        if execution_env == "local_headless":
+            options.binary_location = binary_location
+            options.add_argument('--headless') # https://www.ytyng.com/blog/ubuntu-chromedriver/
+            options.add_argument("--disable-dev-shm-usage")  # overcome limited resource problems
+            options.add_argument("start-maximized")  # open Browser in maximized mode
+            options.add_argument("disable-infobars")  # disabling infobars
+            options.add_argument("--disable-extensions")  # disabling extensions
+            options.add_argument("--disable-gpu")  # applicable to windows os only
+            options.add_argument("--no-sandbox")  # Bypass OS security model
+        elif execution_env == "aws_lambda":
+            executable_path = "/opt/browser/chromedriver"
+            options.binary_location = "/opt/browser/headless-chromium"
+            options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--single-process")
+            options.add_argument("--disable-gpu")
+            options.add_argument("--window-size=1280x1696")
+            options.add_argument("--disable-application-cache")
+            options.add_argument("--disable-infobars")
+            options.add_argument("--hide-scrollbars")
+            options.add_argument("--enable-logging")
+            options.add_argument("--log-level=0")
+            options.add_argument("--ignore-certificate-errors")
+            options.add_argument("--homedir=/tmp")
+            options.add_argument('--disable-dev-shm-usage')
+        else:
+            options.binary_location = binary_location
+        if proxy:
+            logging.info("WebDriverSession proxy on")
+            options.add_argument(f"proxy-server={proxy}")
+
+        caps = DesiredCapabilities.CHROME
+        caps['loggingPrefs'] = {'performance': 'INFO'}
+        #logging.info(f"WebDriverSession.__init__ : {binary_location}, {executable_path}, {proxy}, {execution_env}")
+        self.driver = webdriver.Chrome(options=options, executable_path=executable_path, desired_capabilities=caps)
+        self.driver.implicitly_wait(5.0)
+
+    def close(self):
+        """[Close WebDriverSession, if chromewebdriver dosen't kill, plsease execute "killall chromedriver"]
+        
+        """
+        self.driver.quit()
+
+    def reload(self):
+        self.driver.refresh()
+
+    @property
+    def cookies(self):
+        return self.driver.get_cookies()
+
+    def to_request_session(self) -> CWRequest:
+        """[summary]
+        
+        Returns:
+            Request -- [description]
+        """
+        session = CWRequest()
+        for cookie in self.driver.get_cookies():
+            self.driver.cookies.set(cookie["name"], cookie["value"])
+        return session
+
+    def wait_rendering_by_id(self, id, timeout=20):
+        """[summary]
+        
+        Arguments:
+            id {[type]} -- [description]
+        
+        Keyword Arguments:
+            timeout {int} -- [description] (default: {20})
+        """
+        WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((By.ID, id)))
+
+    def wait_rendering_by_class(self, _class, timeout=20):
+        """[summary]
+        
+        Arguments:
+            _class {[type]} -- [description]
+        
+        Keyword Arguments:
+            timeout {int} -- [description] (default: {20})
+        """
+        WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, _class)))
+
+    def move(self, url):
+        self.driver.get(url)
+
+    def print_screen(self, w, h, path, filename):
+        """[summary]
+
+        Args:
+            w ([type]): [description]
+            h ([type]): [description]
+            path ([type]): [description]
+        """
+        # set window size
+        self.driver.set_window_size(w, h)
+
+        # Get Screen Shot
+        fullpath = f"{path}/{filename}.png"
+        self.driver.save_screenshot(fullpath)
+    
+    def print_fullscreen(self, path, filename):
+        # get width and height of the page
+        w = self.driver.execute_script("return document.body.scrollWidth;")
+        h = self.driver.execute_script("return document.body.scrollHeight;")
+        self.print_screen(w, h, path, filename)
+
+    @property
+    def html(self):
+        html = self.driver.page_source.encode('utf-8')
+        return BeautifulSoup(html, "lxml")
+
+    @property
+    def log(self):
+        result = []
+        for entry in self.driver.get_log('performance'):
+            result.append(entry['message'])
+        return result
