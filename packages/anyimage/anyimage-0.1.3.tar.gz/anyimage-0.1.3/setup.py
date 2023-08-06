f8a16d3e83b96b487bbae616dafd6af78250e68e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:10 +0000

    Add package anyimage/0.1.3/anyimage-0.1.3.tar.gz

---
+++ packages/anyimage/anyimage-0.1.3.tar.gz/anyimage-0.1.3/setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['anyim',
+ 'anyim.utils',
+ 'clip',
+ 'glide_text2im',
+ 'glide_text2im.clip',
+ 'glide_text2im.tokenizer',
+ 'tokenizer']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Pillow>=8.4.0,<9.0.0',
+ 'attrs>=21.2.0,<22.0.0',
+ 'filelock>=3.4.2,<4.0.0',
+ 'fire>=0.4.0,<0.5.0',
+ 'ftfy>=6.0.3,<7.0.0',
+ 'regex>=2021.11.10,<2022.0.0',
+ 'requests>=2.23.0,<3.0.0',
+ 'torch>=1.10.0,<2.0.0',
+ 'tqdm>=4.62.3,<5.0.0']
+
+entry_points = \
+{'console_scripts': ['anyim = anyim.main:main',
+                     'anyimage = anyim.main:main',
+                     'getim = anyim.main:main']}
+
+setup_kwargs = {
+    'name': 'anyimage',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Richard Brooker',
+    'author_email': 'richard@anghami.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
