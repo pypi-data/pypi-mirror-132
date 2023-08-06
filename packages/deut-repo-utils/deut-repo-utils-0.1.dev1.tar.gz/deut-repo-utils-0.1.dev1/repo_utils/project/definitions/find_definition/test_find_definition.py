249c43493ac4285eb9eddea0e8c11d05168fa747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:48 +0000

    Add package deut-repo-utils/0.1.dev1/deut-repo-utils-0.1.dev1.tar.gz

---
+++ packages/deut-repo-utils/deut-repo-utils-0.1.dev1.tar.gz/deut-repo-utils-0.1.dev1/repo_utils/project/definitions/find_definition/test_find_definition.py
@@ -0,0 +1,12 @@
+from find_definition import find_definition
+
+
+def test_find_definition():
+    path = find_definition("define")
+    assert "repo_utils" in path
+
+    path = find_definition("TestObj")
+    assert "repo_utils" in path
+
+    path = find_definition("TestObj", namespace="functions")
+    assert path is None
