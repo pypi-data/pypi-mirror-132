46e9c0dc7c4352577f5c112d9c8980b90e4c7407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:06 +0000

    Add package ml-framework/0.0.14/ml_framework-0.0.14.tar.gz

---
+++ packages/ml-framework/ml_framework-0.0.14.tar.gz/ml_framework-0.0.14/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+# Reads the content of your README.md into a variable to be used in the setup below
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ml_framework",  # should match the package folder
+    packages=["ml_framework"],  # should match the package folder
+    version="0.0.14",  # important for updates
+    license="MIT",  # should match your chosen license
+    description="Machine Learning Framework",
+    long_description=long_description,  # loads your README.md
+    long_description_content_type="text/markdown",  # README.md is of type 'markdown'
+    author="feketedavid1012&ansticky",
+    author_email="antsticky@gmail.com",
+    url="https://github.com/antsticky/AILib/",
+    project_urls={  # Optional
+        "Bug Tracker": "https://github.com/antsticky/AILib//issues"
+    },
+    install_requires=["requests"],  # list all packages that your package uses
+    keywords=["framework", "ML", "AI"],  # descriptive meta-data
+    classifiers=[  # https://pypi.org/classifiers
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Documentation",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    download_url="https://github.com/antsticky/AILib/archive/refs/tags/0.0.3.tar.gz",
+)
