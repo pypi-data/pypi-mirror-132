abb058cd0d03267deb45c52cb7943df4b6e06304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:39 +0000

    Add package metadata-guardian/0.1.0/metadata_guardian-0.1.0-cp37-abi3-macosx_10_7_x86_64.whl

---
+++ packages/metadata-guardian/metadata_guardian-0.1.0-cp37-abi3-macosx_10_7_x86_64.whl/metadata_guardian/source/external/external_metadata_source.py
@@ -0,0 +1,45 @@
+from abc import abstractmethod
+from typing import Any, List
+
+from ...exceptions import MetadataGuardianException
+from ..metadata_source import MetadataSource
+
+
+class ExternalMetadataSource(MetadataSource):
+    """ExternalMetadataSource Source."""
+
+    @abstractmethod
+    def get_column_names(
+        self, database_name: str, table_name: str, include_comment: bool = False
+    ) -> List[str]:
+        """
+        Get the column names from the schema.
+        :param database_name: the database name
+        :param table_name: the table name
+        :param include_comment: include the comment
+        :return: the list of the column names
+        """
+        pass
+
+    @abstractmethod
+    def get_table_names_list(self, database_name: str) -> List[str]:
+        """
+        Get the table names list from the database.
+        :param database_name: the database name
+        :return: the list of the table names of the database
+        """
+        pass
+
+    @abstractmethod
+    def get_connection(self) -> Any:
+        """
+        Get the connection of the source.
+        :return: the source connection
+        """
+        pass
+
+
+class ExternalMetadataSourceException(MetadataGuardianException):
+    """Raised where there is an exception to describe a external metadata source exception."""
+
+    pass
