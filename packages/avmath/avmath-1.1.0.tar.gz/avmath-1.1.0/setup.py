3287f0e78494daf6886edb9ae044101b0015edde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:49 +0000

    Add package avmath/1.1.0/avmath-1.1.0.tar.gz

---
+++ packages/avmath/avmath-1.1.0.tar.gz/avmath-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="avmath",
+    packages=["avmath"],
+    version="1.1.0",
+    license="MIT",
+    description="Math module with advanced features",
+    author="Camillo Ballandt",
+    url="https://www.github.com/ballandt/avmath",
+    download_url="https://github.com/ballandt/avmath/",
+)
