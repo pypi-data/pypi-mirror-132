929555c2e8a4cc9a0399ce5c210400e26fa708a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:51 +0000

    Add package vessl/0.1.26.dev3/vessl-0.1.26.dev3.tar.gz

---
+++ packages/vessl/vessl-0.1.26.dev3.tar.gz/vessl-0.1.26.dev3/vessl/internal/__init__.py
@@ -0,0 +1,10 @@
+from vessl.internal.vessl_run import VesslRun
+
+_run = VesslRun()
+
+init = _run.init
+api = _run.api
+finish = _run.finish
+log = _run.log
+upload = _run.upload
+progress = _run.progress
