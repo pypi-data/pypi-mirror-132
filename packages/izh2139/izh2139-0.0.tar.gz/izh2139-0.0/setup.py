5ea4c0877527753d198242a0dae07a474c59aaca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:27 +0000

    Add package izh2139/0.0/izh2139-0.0.tar.gz

---
+++ packages/izh2139/izh2139-0.0.tar.gz/izh2139-0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='izh2139',
+      version='0.0',
+      description='Numerical methods',
+      packages=find_packages(),
+      author_email='ustinovkirill23@gmail.com',
+      zip_safe=False,
+      )
