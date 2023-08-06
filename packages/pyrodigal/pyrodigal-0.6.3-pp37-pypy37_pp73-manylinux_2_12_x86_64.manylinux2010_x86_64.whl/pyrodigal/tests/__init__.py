3a9febfbeb12664067241f450cada34e34920f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:13 +0000

    Add package pyrodigal/0.6.3/pyrodigal-0.6.3-pp37-pypy37_pp73-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/pyrodigal/pyrodigal-0.6.3-pp37-pypy37_pp73-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/pyrodigal/tests/__init__.py
@@ -0,0 +1,17 @@
+from . import (
+    test_connection_scorer,
+    test_gene,
+    test_genes,
+    test_nodes,
+    test_orf_finder,
+    test_sequence,
+)
+
+def load_tests(loader, suite, pattern):
+    suite.addTests(loader.loadTestsFromModule(test_connection_scorer))
+    suite.addTests(loader.loadTestsFromModule(test_gene))
+    suite.addTests(loader.loadTestsFromModule(test_genes))
+    suite.addTests(loader.loadTestsFromModule(test_nodes))
+    suite.addTests(loader.loadTestsFromModule(test_orf_finder))
+    suite.addTests(loader.loadTestsFromModule(test_sequence))
+    return suite
