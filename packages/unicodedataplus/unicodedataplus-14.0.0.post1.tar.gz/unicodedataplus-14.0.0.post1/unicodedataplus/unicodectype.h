7dc0326fd3ac90b23ad2c225440c554f1996aec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:12 +0000

    Add package unicodedataplus/14.0.0.post1/unicodedataplus-14.0.0.post1.tar.gz

---
+++ packages/unicodedataplus/unicodedataplus-14.0.0.post1.tar.gz/unicodedataplus-14.0.0.post1/unicodedataplus/unicodectype.h
@@ -0,0 +1,8 @@
+#ifndef __UNICODECTYPE_H__
+#define __UNICODECTYPE_H__
+
+int _PyUnicodePlus_ToDecimalDigit(Py_UCS4 ch);
+int _PyUnicodePlus_ToDigit(Py_UCS4 ch);
+double _PyUnicodePlus_ToNumeric(Py_UCS4 ch);
+
+#endif
