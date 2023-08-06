39b35de36376293080dae945453b64a68c786560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:50 +0000

    Add package pgeng/1.7.1/pgeng-1.7.1-py3-none-any.whl

---
+++ packages/pgeng/pgeng-1.7.1-py3-none-any.whl/pgeng/vfx/__init__.py
@@ -0,0 +1,27 @@
+'Visual effects classes and functions for pgeng'
+from .. import _failed_imports
+
+try:
+    from .vfx_core import circle_lighting
+except ImportError as e:
+    _failed_imports.append(e.name)
+
+try:
+    from .flame import *
+except ImportError as e:
+    _failed_imports.append(e.name)
+
+try:
+    from .particle import Particle
+except ImportError as e:
+    _failed_imports.append(e.name)
+
+try:
+    from .shockwave import ShockWave
+except ImportError as e:
+    _failed_imports.append(e.name)
+
+try:
+    from .spark import *
+except ImportError as e:
+    _failed_imports.append(e.name)
