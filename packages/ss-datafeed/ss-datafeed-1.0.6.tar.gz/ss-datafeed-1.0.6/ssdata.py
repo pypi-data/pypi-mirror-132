aeffcd8e0e50341b19b8476f9581efd158e35d6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:06 +0000

    Add package ss-datafeed/1.0.6/ss-datafeed-1.0.6.tar.gz

---
+++ packages/ss-datafeed/ss-datafeed-1.0.6.tar.gz/ss-datafeed-1.0.6/ssdata.py
@@ -0,0 +1,11 @@
+from ssdata_util import query
+from ssdata_datasource import SS_DATASOURCE
+
+class ssdata:
+    def __init__(self, appId, appSec):
+        self.appId = appId
+        self.appSec = appSec
+    def dataset(self,sql,*params,datasource=SS_DATASOURCE.DS_EFORM):
+        result = query(self.appId,self.appSec,sql,*params,
+                                   datasource=datasource)
+        return result
