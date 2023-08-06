ec099554d9cbf361e93b1ebf2a92681813e9a4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:38 +0000

    Add package basicTools/0.0.6/basicTools-0.0.6.tar.gz

---
+++ packages/basicTools/basicTools-0.0.6.tar.gz/basicTools-0.0.6/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 1 - Planning',
+  'Intended Audience :: Developers',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3',
+  'Operating System :: OS Independent'
+]
+ 
+setup(
+  name='basicTools',
+  version='0.0.6',
+  description='A very basic tool',
+  long_description_content_type ='text/markdown',
+  long_description=open('README.txt').read() + "\n",
+  url='',  
+  author='Jenifar',
+  author_email='jenifar200227@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords=['email','basicTools'],
+  packages=find_packages(),
+  install_requires=['regex','math'] 
+)
