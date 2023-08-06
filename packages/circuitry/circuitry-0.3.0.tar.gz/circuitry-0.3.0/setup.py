e4396769d35e40c5da71f70268135a6b57737c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:16 +0000

    Add package circuitry/0.3.0/circuitry-0.3.0.tar.gz

---
+++ packages/circuitry/circuitry-0.3.0.tar.gz/circuitry-0.3.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read().replace(".. include:: toc.rst\n\n", "")
+
+# The lines below can be parsed by `docs/conf.py`.
+name = "circuitry"
+version = "0.3.0"
+
+setup(
+    name=name,
+    version=version,
+    packages=[name,],
+    install_requires=["parts~=1.2","circuit~=0.4",],
+    license="MIT",
+    url="https://github.com/nthparty/circuitry",
+    author="Andrei Lapets",
+    author_email="a@lapets.io",
+    description="Embedded domain-specific combinator library for "+\
+                "assembling abstract definitions of logic circuits.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
