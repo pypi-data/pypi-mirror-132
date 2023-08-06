24be1e1b7f87a7b6473e479095d88d9380391168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:52 +0000

    Add package bankreport/0.1.2/bankreport-0.1.2.tar.gz

---
+++ packages/bankreport/bankreport-0.1.2.tar.gz/bankreport-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['bankreport']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=7.1.2,<8.0.0', 'pandas>=1.1.4,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['bankreport = bankreport:main']}
+
+setup_kwargs = {
+    'name': 'bankreport',
+    'version': '0.1.2',
+    'description': 'Parse and analyze csv files from online banking.',
+    'long_description': None,
+    'author': 'Heinrich Hartmann',
+    'author_email': 'heinrich@heinrichhartmann.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/heinrichhartmann/bankreport',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
