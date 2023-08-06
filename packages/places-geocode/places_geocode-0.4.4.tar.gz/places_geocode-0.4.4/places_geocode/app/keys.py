cd75ac26a5dc4164eb6bf6bd8353ea5c74cab2d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:29 +0000

    Add package places-geocode/0.4.4/places_geocode-0.4.4.tar.gz

---
+++ packages/places-geocode/places_geocode-0.4.4.tar.gz/places_geocode-0.4.4/places_geocode/app/keys.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+rapid_api_token = "bc5b270c17msh058aaac04af67a4p16d4ccjsn75cd807f68f5"
+google_api_token = "AIzaSyB94FUFckhQUa-jRpy8Us8HF7AeRQ5KPqU"
+estated_token = 'J9aEmitNylIyFD5bQiVLdsZs07qw1t'
+mongo_pass = 'Barca2004!'
