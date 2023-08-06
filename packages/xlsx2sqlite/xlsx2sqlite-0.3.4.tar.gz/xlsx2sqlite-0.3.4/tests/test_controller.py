d9b0e3748c6f4576ada4a7aa1ff88e7638e00fa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:27 +0000

    Add package xlsx2sqlite/0.3.4/xlsx2sqlite-0.3.4.tar.gz

---
+++ packages/xlsx2sqlite/xlsx2sqlite-0.3.4.tar.gz/xlsx2sqlite-0.3.4/tests/test_controller.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+import xlsx2sqlite.controller as controller
+from tests.test_ini_parsing import ini_path
+
+
+def test_new_controller(ini_path):
+    """support for in-memory database"""
+    ctrler = controller.new_controller(ini_path)
+    assert ctrler._config is not None
+    assert ctrler._ini.get("db_file") is None
+
+
+def test_ls_entities(ini_path):
+    """unit testint ls_entities method"""
+    ctrler = controller.new_controller(ini_path)
+    #TODO: difficult to test without writing a database file.
