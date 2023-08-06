623b279e483fc17e4acf87844244d8afd52ae3a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:47 +0000

    Add package ml-wrappers/0.0.2/ml_wrappers-0.0.2.tar.gz

---
+++ packages/ml-wrappers/ml_wrappers-0.0.2.tar.gz/ml_wrappers-0.0.2/ml_wrappers/dataset/__init__.py
@@ -0,0 +1,9 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+"""Defines a common dataset wrapper and common functions for data manipulation."""
+
+from .dataset_wrapper import DatasetWrapper
+
+__all__ = ['DatasetWrapper']
