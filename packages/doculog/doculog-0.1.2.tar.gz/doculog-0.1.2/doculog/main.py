d9670da7ed580589089875cf467bc3faa6d21492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:23 +0000

    Add package doculog/0.1.2/doculog-0.1.2.tar.gz

---
+++ packages/doculog/doculog-0.1.2.tar.gz/doculog-0.1.2/doculog/main.py
@@ -0,0 +1,19 @@
+"""
+CLI entrypoints to the code.
+"""
+from pathlib import Path
+
+from doculog.changelog import ChangelogDoc
+from doculog.config import configure
+
+
+def generate_changelog():
+    root = Path.cwd()
+
+    config = configure(root)
+    log_path = root / config["changelog_name"]
+
+    doc = ChangelogDoc(log_path)
+    doc.generate()
+    doc.save()
+    print(f"Saved changelog to {log_path}")
