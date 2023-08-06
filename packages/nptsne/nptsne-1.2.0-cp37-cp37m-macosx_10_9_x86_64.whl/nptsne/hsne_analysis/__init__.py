10fdd3824a29f27cb65307559e178ea65142026d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:43 +0000

    Add package nptsne/1.2.0/nptsne-1.2.0-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/nptsne/nptsne-1.2.0-cp37-cp37m-macosx_10_9_x86_64.whl/nptsne/hsne_analysis/__init__.py
@@ -0,0 +1,10 @@
+# The hsne_analysis module is inspired by the original
+# high_dimensional_inspector analysis library from Nicola Pezzotti
+#
+# The difference is that this module contains no visualization code
+# it simply presents a generic model for navigating an hsne analysis
+
+from ..libs._nptsne._hsne_analysis import Analysis, EmbedderType, SparseTsne
+from .analysis_model import AnalysisContainer, AnalysisModel
+
+__all__ = ("AnalysisContainer", "AnalysisModel", "Analysis", "EmbedderType", "SparseTsne")
