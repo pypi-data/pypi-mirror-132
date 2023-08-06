fc78637b56f6cf94a9acbb98daa29715282323d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:24 +0000

    Add package ampel-interface/0.8.2a6/ampel-interface-0.8.2a6.tar.gz

---
+++ packages/ampel-interface/ampel-interface-0.8.2a6.tar.gz/ampel-interface-0.8.2a6/ampel/struct/AmpelBuffer.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : Ampel-interface/ampel/struct/AmpelBuffer.py
+# License           : BSD-3-Clause
+# Author            : vb <vbrinnel@physik.hu-berlin.de>
+# Date              : 31.05.2018
+# Last Modified Date: 01.12.2021
+# Last Modified By  : vb <vbrinnel@physik.hu-berlin.de>
+
+from typing import Optional, Dict, List, Any, TypedDict, Literal, Union
+from ampel.types import StockId
+from ampel.content.StockDocument import StockDocument
+from ampel.content.DataPoint import DataPoint
+from ampel.content.T1Document import T1Document
+from ampel.content.T2Document import T2Document
+from ampel.content.LogDocument import LogDocument
+
+# Please update BufferKey on AmpelBuffer udpates
+# There is currently unfortunately no way of extracting a Literal out of a TypedDict
+BufferKey = Literal['id', 'stock', 'origin', 't0', 't1', 't2', 'logs', 'extra']
+
+class AmpelBuffer(TypedDict, total=False):
+	"""
+	Content bundle used to build :class:`~ampel.view.SnapView.SnapView`.
+	
+	This is a dict containing 1 or more of the following items:
+	"""
+	# Could stock be of type List[StockDocument] to enable hybrid/dual transients ?
+	id: StockId
+	stock: Optional[StockDocument]
+	origin: Optional[Union[int, List[int]]]
+	t0: Optional[List[DataPoint]]
+	t1: Optional[List[T1Document]]
+	t2: Optional[List[T2Document]]
+	logs: Optional[List[LogDocument]]
+	extra: Optional[Dict[str, Any]]
