2ab771569284b566c758cac79fa275ab3d865baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:31 +0000

    Add package pyrodigal/0.6.4/pyrodigal-0.6.4-cp310-cp310-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/pyrodigal/pyrodigal-0.6.4-cp310-cp310-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/pyrodigal/__init__.py
@@ -0,0 +1,59 @@
+from collections.abc import (
+    Sequence as _Sequence,
+    Sized as _Sized,
+)
+
+from . import _pyrodigal
+from ._pyrodigal import (
+    Gene,
+    Genes,
+    Mask,
+    Masks,
+    Node,
+    Nodes,
+    OrfFinder,
+    Prediction,
+    Predictions,
+    Pyrodigal,
+    Sequence,
+    TrainingInfo,
+    MetagenomicBin,
+)
+
+__doc__ = _pyrodigal.__doc__
+__all__ = [
+    "Gene",
+    "Genes",
+    "Mask",
+    "Masks",
+    "Node",
+    "Nodes",
+    "OrfFinder",
+    "Prediction",
+    "Predictions",
+    "Pyrodigal",
+    "Sequence",
+    "TrainingInfo",
+    "MetagenomicBin"
+]
+
+__author__ = "Martin Larralde <martin.larralde@embl.de>"
+__license__ = "GPLv3"
+__version__ = "0.6.4"
+
+_Sized.register(Sequence)
+_Sequence.register(Genes)
+_Sequence.register(Masks)
+_Sequence.register(Nodes)
+_Sequence.register(Predictions)
+
+# Small addition to the docstring: we want to show a link redirecting to the
+# rendered version of the documentation, but this can only work when Python
+# is running with docstrings enabled
+if __doc__ is not None:
+    __doc__ += """See Also:
+    An online rendered version of the documentation for this version
+    of the library on
+    `Read The Docs <https://pyrodigal.readthedocs.io/en/v{}/>`_.
+
+    """.format(__version__)
