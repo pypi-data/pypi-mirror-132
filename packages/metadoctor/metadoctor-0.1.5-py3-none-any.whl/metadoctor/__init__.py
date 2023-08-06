6cb966fac3e80cbe38739efee9342117d8c3ce25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:24:08 +0000

    Add package metadoctor/0.1.5/metadoctor-0.1.5-py3-none-any.whl

---
+++ packages/metadoctor/metadoctor-0.1.5-py3-none-any.whl/metadoctor/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '0.1.5'
+
+from metadoctor import settings
+from metadoctor import database
+from metadoctor import enumeration
+from metadoctor import base 
+from metadoctor import utils 
+from metadoctor import element
