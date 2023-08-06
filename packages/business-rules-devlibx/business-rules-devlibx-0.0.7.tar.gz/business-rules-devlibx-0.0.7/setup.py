71d28f99fef4da4af3facaef8194915c7821f029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:08:58 +0000

    Add package business-rules-devlibx/0.0.7/business-rules-devlibx-0.0.7.tar.gz

---
+++ packages/business-rules-devlibx/business-rules-devlibx-0.0.7.tar.gz/business-rules-devlibx-0.0.7/setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+import setuptools
+
+with open('HISTORY.rst') as f:
+    history = f.read()
+
+description = 'Python DSL for setting up business intelligence rules that can be configured without code'
+
+setuptools.setup(
+    name='business-rules-devlibx',
+    version="0.0.7",
+    description='{0}\n\n{1}'.format(description, history),
+    author='devlibx',
+    author_email='devlibxgithub@gmail.com',
+    url='https://github.com/devlibx/business-rules',
+    packages=['business_rules'],
+    package_dir={"": "."},
+    license='MIT'
+)
