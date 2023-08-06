aca6e7012c8a1ffe251d515bac2b3fc272aaa2cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:56 +0000

    Add package image-bbox-slicer/0.4/image_bbox_slicer-0.4.tar.gz

---
+++ packages/image-bbox-slicer/image_bbox_slicer-0.4.tar.gz/image_bbox_slicer-0.4/image_bbox_slicer/__init__.py
@@ -0,0 +1,18 @@
+'''
+Image and Bounding Box Slicer-Resizer
+~~~~~~~~~~~~
+Slice up or Resize images and their bounding boxes.
+Basic usage instructions are available at Slicing_Demo.ipynb and Resizing_Demo.ipynb
+
+:copyright: (c) 2019 Akshay L Chandra, Vineeth N Balasubramanian
+:license: MIT, see LICENSE for more details.
+'''
+
+__title__ = 'image_bbox_slicer'
+__author__ = 'Akshay L Chandra'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2019 Akshay L Chandra, Vineeth N Balasubramanian'
+
+from .slicer import *
+from .helpers import *
+from .resizer import *
---
+++ packages/image-bbox-slicer/image_bbox_slicer-0.4.tar.gz/image_bbox_slicer-0.4/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+setup(
+  name='image_bbox_slicer',
+  packages=['image_bbox_slicer'],
+  version='0.4',
+  license='MIT',
+  long_description='This easy-to-use library is a data transformer sometimes useful in Object Detection \
+    and Segmentation tasks. With only a few lines of code, one can slice images and their bounding box annotations \
+      into smaller tiles, both into specific sizes and into any arbitrary number of equal parts. \
+        The tool also supports resizing of images and their bounding box annotations, both by specific sizes \
+          and by a resizing/scaling factor.\n\nRead the docs at https://image-bbox-slicer.readthedocs.io/en/latest/.',
+  author='Akshay L Chandra',
+  author_email='research@akshaychandra.com',
+  url='https://github.com/acl21/image_bbox_slicer/',
+  download_url='https://github.com/acl21/image_bbox_slicer/archive/refs/tags/v0.4.tar.gz',
+  keywords=['Image Slicer', 'Bounding Box Annotations Slicer', 'Slicer', 'PASCAL VOC Slicer', 'Object Detection',
+            'Resize Images', 'Resize Bounding Box Annotations', 'Segmentation'],
+  install_requires=[
+    'Pillow',
+    'numpy',
+    'matplotlib',
+    'pascal-voc-writer'
+    ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
