bd500e860d1b89cb38ddfc52666d3692b40e2d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:16 +0000

    Add package lordkelvin/3.7.3/lordkelvin-3.7.3.tar.gz

---
+++ packages/lordkelvin/lordkelvin-3.7.3.tar.gz/lordkelvin-3.7.3/setup.py
@@ -0,0 +1,22 @@
+import os, lk, setuptools as _
+_.setup(name='lordkelvin', version=lk.__version__,
+        description='super cool toolkit',
+        long_description=open(__file__[:-8]+"README.md").read(),
+        long_description_content_type='text/markdown',
+        url='https://gitlab.com/circleclicklabs/lordkelvin.git',
+        author='Joel Ward', author_email='jmward+python@gmail.com',
+        zip_safe=True, license='MIT',
+        py_modules=['lk'],
+        entry_points=dict(console_scripts=['lk=lk:main',
+                                           'deploy=lk:main2',
+                                           'execute=lk:main2',
+                                           'save=lk:main2',
+                                           'balance=lk:main2',
+                                           ]),
+        scripts=(lambda d:[d+x for x in os.listdir(d)])('scripts/'),
+        install_requires=['docopt','web3==5.24','eth_account'],
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ])
