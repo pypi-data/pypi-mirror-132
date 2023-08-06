f6044bac58d7560d254c11a61d82e4a821fbaee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:16 +0000

    Add package py-btc-price/0.2/py_btc_price-0.2.tar.gz

---
+++ packages/py-btc-price/py_btc_price-0.2.tar.gz/py_btc_price-0.2/py_btc_price/__init__.py
@@ -0,0 +1,2 @@
+from .btc_price import btc_convert
+from .btc_price import get_btc_price
---
+++ packages/py-btc-price/py_btc_price-0.2.tar.gz/py_btc_price-0.2/py_btc_price/btc_price.py
@@ -0,0 +1,21 @@
+import requests
+
+def get_btc_price(currency='USD'):
+    response = requests.get('https://blockchain.info/ticker')
+    
+    if response.status_code == 200:
+        return  response.json()[currency]['last']
+    
+    return None
+    
+
+def btc_convert(amount):
+    try:
+        current_price = get_btc_price('USD')
+        
+        if current_price:
+            return (1.0 / current_price ) * amount
+        
+    except Exception as err:
+        
+        return None
