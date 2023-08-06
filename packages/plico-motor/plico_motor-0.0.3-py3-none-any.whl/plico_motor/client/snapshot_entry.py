bb21ac126f6156b920bbf1e8f7f033aabe4bfe6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:09 +0000

    Add package plico-motor/0.0.3/plico_motor-0.0.3-py3-none-any.whl

---
+++ packages/plico-motor/plico_motor-0.0.3-py3-none-any.whl/plico_motor/client/snapshot_entry.py
@@ -0,0 +1,9 @@
+
+class SnapshotEntry(object):
+    MOTOR_NAME = 'NAME'
+    POSITION = "POS"
+    STEPS_PER_SI_UNITS = 'SI2STEPS'
+    WAS_HOMED = 'WASHOMED'
+    MOTOR_TYPE = 'TYPE'
+    IS_MOVING = 'ISMOVING'
+    LAST_COMMANDED_POSITION = 'LASTCMD'
