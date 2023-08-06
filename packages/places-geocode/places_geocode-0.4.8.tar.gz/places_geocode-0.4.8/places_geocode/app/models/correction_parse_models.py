362018f1e50e6a347956c3688eef8f80b9d93bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:11 +0000

    Add package places-geocode/0.4.8/places_geocode-0.4.8.tar.gz

---
+++ packages/places-geocode/places_geocode-0.4.8.tar.gz/places_geocode-0.4.8/places_geocode/app/models/correction_parse_models.py
@@ -0,0 +1,9 @@
+"""address autocomplete/correction and address parsing models go here"""
+
+from pydantic import BaseModel
+from typing import List, Union, Tuple, Dict
+
+
+class CorrectionModel(BaseModel):
+    """dummy correction model"""
+    pass
