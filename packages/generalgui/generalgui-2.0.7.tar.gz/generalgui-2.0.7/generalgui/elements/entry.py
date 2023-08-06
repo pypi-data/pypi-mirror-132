972e362ccab3da48df0a5d2a4238e09eace34a65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:05 +0000

    Add package generalgui/2.0.7/generalgui-2.0.7.tar.gz

---
+++ packages/generalgui/generalgui-2.0.7.tar.gz/generalgui-2.0.7/generalgui/elements/entry.py
@@ -0,0 +1,27 @@
+
+import tkinter as tk
+
+from generalgui.properties.generic import Generic
+from generalgui.properties.text import Text
+from generalgui.properties.toggle import Toggle
+from generalgui.properties.editable import Editable
+
+
+class Entry(Generic, Text, Toggle, Editable):
+    # Type hinting
+    widget = ...  # type: tk.Entry
+
+    # Generic
+    widget_cls = tk.Entry
+
+    # Editable
+    _editable_tk_var = tk.StringVar
+    def _editable_hook_get(self):
+        return self.text
+    def _editable_hook_set(self):
+        self._text = self._editable_tk_var_inst.get()
+
+    def __init__(self, parent=None, text=None):
+        pass
+
+
