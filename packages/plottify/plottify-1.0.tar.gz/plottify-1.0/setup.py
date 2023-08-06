54fd2c69a0e1fcd8b2e6723a51c0ffd30461186b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:02 +0000

    Add package plottify/1.0/plottify-1.0.tar.gz

---
+++ packages/plottify/plottify-1.0.tar.gz/plottify-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="plottify",
+    version="1.0",
+    description="Plottify",
+    url="https://github.com/andrewcharlesjones/plottify",
+    download_url="https://github.com/andrewcharlesjones/plottify/archive/refs/tags/v01.tar.gz",
+    author="Andy Jones",
+    author_email="ajones788@gmail.com",
+    packages=find_packages(),
+)
