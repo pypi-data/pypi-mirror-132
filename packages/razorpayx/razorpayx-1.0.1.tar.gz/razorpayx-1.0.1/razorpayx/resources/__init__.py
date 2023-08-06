dfc9b48c5e76d13fc986ab5628710934b9acce11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:37 +0000

    Add package razorpayx/1.0.1/razorpayx-1.0.1.tar.gz

---
+++ packages/razorpayx/razorpayx-1.0.1.tar.gz/razorpayx-1.0.1/razorpayx/resources/__init__.py
@@ -0,0 +1,16 @@
+from .contact import Contact
+from .fundaccount import FundAccount
+from .payout import Payout
+from .fundaccountvalidations import FundAccountValidation
+from .payoutlink import PayoutLink
+from .transaction import Transaction
+
+
+__all__ = [
+    'Contact',
+    'FundAccount',
+    'Payout',
+    'FundAccountValidation',
+    'PayoutLink',
+    'Transaction',
+]
