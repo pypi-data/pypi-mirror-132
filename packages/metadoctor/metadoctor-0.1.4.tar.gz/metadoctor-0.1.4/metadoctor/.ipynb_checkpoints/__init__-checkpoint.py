6d74197876b2ac1d57ac74a6e2b4e708c1cb41aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:35 +0000

    Add package metadoctor/0.1.4/metadoctor-0.1.4.tar.gz

---
+++ packages/metadoctor/metadoctor-0.1.4.tar.gz/metadoctor-0.1.4/metadoctor/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,8 @@
+__version__ = '0.1.4'
+
+from metadoctor import settings
+from metadoctor import database
+from metadoctor import enumeration
+from metadoctor import base 
+from metadoctor import utils 
+from metadoctor import element
---
+++ packages/metadoctor/metadoctor-0.1.4.tar.gz/metadoctor-0.1.4/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['metadoctor', 'metadoctor..ipynb_checkpoints']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['MarkupSafe>=2.0.1,<3.0.0',
+ 'deta>=1.0.1,<2.0.0',
+ 'passlib[bcrypt]>=1.7.4,<2.0.0',
+ 'starlette>=0.17.1,<0.18.0']
+
+setup_kwargs = {
+    'name': 'metadoctor',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': None,
+    'author': 'arantesdv',
+    'author_email': 'arantesdv@me.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
