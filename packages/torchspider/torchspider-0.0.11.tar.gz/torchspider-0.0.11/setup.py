ea96efd0bf84cc7a166dd349e1d3465416cf7e64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:45 +0000

    Add package torchspider/0.0.11/torchspider-0.0.11.tar.gz

---
+++ packages/torchspider/torchspider-0.0.11.tar.gz/torchspider-0.0.11/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+
+with open("requirements.txt") as f:
+    requirements = [req.strip() for req in f]
+
+setup(
+    name='torchspider',
+    version='0.0.11',
+    description='A Pytorch-based framework',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Gary Lai',
+    author_email="glai9665@gmail.com",
+    packages=['torchspider'],
+    package_dir={'': 'src'},
+    install_requires=requirements,
+    url="https://github.com/ghlai9665/octopus",
+    python_requires=">=3.6",
+    classifiers=["License :: OSI Approved :: Apache Software License",
+                 "Programming Language :: Python :: 3"],
+    license='Apache v2.0'
+)
