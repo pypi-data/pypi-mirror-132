80669b862d069cc6ca31541ae34889e2bb9029b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:06 +0000

    Add package flow-ctrl/2.0.6/flow-ctrl-2.0.6.tar.gz

---
+++ packages/flow-ctrl/flow-ctrl-2.0.6.tar.gz/flow-ctrl-2.0.6/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import setuptools
+
+setuptools.setup(
+    name='flow-ctrl',
+    version='2.0.6',
+    author='Ignace Konig',
+    description='package for managing pipelines with simple commands',
+    license='MIT',
+    install_requires=['pynput'])
+
