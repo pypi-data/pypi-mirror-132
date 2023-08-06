eb35b364c37e8072dc741a8dc4bddfbcae0eb772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:49 +0000

    Add package basicTools/0.0.7/basicTools-0.0.7.tar.gz

---
+++ packages/basicTools/basicTools-0.0.7.tar.gz/basicTools-0.0.7/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import pathlib
+ 
+classifiers = [
+  'Development Status :: 1 - Planning',
+  'Intended Audience :: Developers',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3',
+  'Operating System :: OS Independent'
+]
+ 
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+
+setup(
+  name='basicTools',
+  version='0.0.7',
+  description='A very basic tool',
+  long_description_content_type ='text/markdown',
+  long_description=README,
+  url='',  
+  author='Jenifar',
+  author_email='jenifar200227@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords=['email','basicTools'],
+  install_requires=['regex','math'] 
+)
