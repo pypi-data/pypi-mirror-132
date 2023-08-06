fb87eefb15131acf9a20c323e267d07c7a1f4fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:23 +0000

    Add package prog-algs/1.2.3/prog_algs-1.2.3-py3-none-any.whl

---
+++ packages/prog-algs/prog_algs-1.2.3-py3-none-any.whl/prog_algs/predictors/monte_carlo.py
@@ -0,0 +1,135 @@
+# Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
+
+from .prediction import UnweightedSamplesPrediction
+from .predictor import Predictor
+from copy import deepcopy
+from functools import partial
+from prog_models.sim_result import SimResult, LazySimResult
+from prog_algs.uncertain_data import UnweightedSamples, UncertainData
+
+def prediction_fcn(x, model, params, events, loading):
+    # This is the main prediction function for the multi-threading
+    events_remaining = deepcopy(events)
+    first_output = model.output(x)
+    time_of_event = {}
+    last_state = {}
+    times = []
+    # inputs will be the same as states unless we explicitly deepcopy
+    inputs = SimResult()
+    states = SimResult()
+    outputs = LazySimResult(fcn = model.output)
+    event_states = LazySimResult(fcn = model.event_state)
+    params = deepcopy(params)
+    params['x'] = x
+    while len(events_remaining) > 0:  # Still events to predict
+        (t, u, xi, z, es) = model.simulate_to_threshold(loading, first_output, **params, threshold_keys=events_remaining, print=False)
+
+        # Add results
+        times.extend(t)
+        inputs.extend(u)
+        states.extend(xi)
+        outputs.extend(z)
+        event_states.extend(es)
+
+        # Get which event occurs
+        t_met = model.threshold_met(states[-1])
+        t_met = {key: t_met[key] for key in events_remaining}  # Only look at remaining keys
+        try:
+            event = list(t_met.keys())[list(t_met.values()).index(True)]
+        except ValueError:
+            # no event has occured
+            for event in events_remaining:
+                time_of_event[event] = None
+                last_state[event] = None
+            break
+
+        # An event has occured
+        time_of_event[event] = times[-1]
+        events_remaining.remove(event)  # No longer an event to predect to
+
+        # Remove last state (event)
+        params['t0'] = times.pop()
+        if 'horizon' in params:
+            # Reset horizon to account for time spent
+            params['horizon'] = params['horizon'] - params['t0']
+        inputs.pop()
+        params['x'] = states.pop()
+        last_state[event] = deepcopy(params['x'])
+        outputs.pop()
+        event_states.pop()
+        
+    return (times, inputs, states, outputs, event_states, time_of_event, last_state)
+
+
+class MonteCarlo(Predictor):
+    """
+    Class for performing a monte-carlo model-based prediction.
+
+    A Predictor using the monte carlo algorithm. The provided initial states are simulated until either a specified time horizon is met, or the threshold for all simulated events is reached for all samples. A provided future loading equation is used to compute the inputs to the system at any given time point. 
+
+    The following configuration parameters are supported (as kwargs in constructor or as parameters in predict method):
+    
+    Configuration Parameters
+    ------------------------------
+    t0 : float
+        Initial time at which prediction begins, e.g., 0
+    dt : float
+        Simulation step size (s), e.g., 0.1
+    events : List[string]
+        Events to predict (subset of model.events) e.g., ['event1', 'event2']
+    horizon : float
+        Prediction horizon (s)
+    n_samples : int
+        Number of samples to use. If not specified, a default value is used. If state is type UnweightedSamples and n_samples is not provided, the provided unweighted samples will be used directly.
+    save_freq : float
+        Frequency at which results are saved (s)
+    save_pts : List[float]
+        Any additional savepoints (s) e.g., [10.1, 22.5]
+    """
+    DEFAULT_N_SAMPLES = 100  # Default number of samples to use, if none specified
+
+    def predict(self, state : UncertainData, future_loading_eqn, **kwargs):
+        if isinstance(state, dict):
+            # Convert to UnweightedSamples
+            from prog_algs.uncertain_data import ScalarData
+            state = ScalarData(state)
+
+        params = deepcopy(self.parameters) # copy parameters
+        params.update(kwargs) # update for specific run
+
+        # Sample from state if n_samples specified or state is not UnweightedSamples
+        if 'n_samples' in params:
+            # If n_samples is specified, sample
+            state = state.sample(params['n_samples'])
+        elif not isinstance(state, UnweightedSamples):
+            # If no n_samples specified, but state is not UnweightedSamples, then sample with default
+            state = state.sample(self.DEFAULT_N_SAMPLES)
+
+        # Perform prediction
+        pred_fcn = partial(
+            prediction_fcn, 
+            model = self.model, 
+            params = params,
+            events = params['events'],
+            loading = future_loading_eqn)
+        
+        result = [pred_fcn(sample) for sample in state]
+        times_all, inputs_all, states_all, outputs_all, event_states_all, time_of_event, last_states = map(list, zip(*result))
+        
+        # Return longest time array
+        times_length = [len(t) for t in times_all]
+        times_max_len = max(times_length)
+        times = times_all[times_length.index(times_max_len)] 
+        
+        inputs_all = UnweightedSamplesPrediction(times, inputs_all)
+        states_all = UnweightedSamplesPrediction(times, states_all)
+        outputs_all = UnweightedSamplesPrediction(times, outputs_all)
+        event_states_all = UnweightedSamplesPrediction(times, event_states_all)
+        time_of_event = UnweightedSamples(time_of_event)
+
+        # Transform final states:
+        last_states = {
+            key: UnweightedSamples([sample[key] for sample in last_states]) for key in time_of_event.keys()
+        }
+        time_of_event.final_state = last_states
+        return (times, inputs_all, states_all, outputs_all, event_states_all, time_of_event)
---
+++ packages/prog-algs/prog_algs-1.2.3-py3-none-any.whl/prog_algs/state_estimators/particle_filter.py
@@ -0,0 +1,149 @@
+# Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
+
+from . import state_estimator
+from numpy import array, empty, random, take, exp, max, take
+from filterpy.monte_carlo import residual_resample
+from numbers import Number
+from scipy.stats import norm
+from ..uncertain_data import UnweightedSamples
+from ..exceptions import ProgAlgTypeError
+from warnings import warn
+
+
+class ParticleFilter(state_estimator.StateEstimator):
+    """
+    Estimates state using a particle filter (PF) algorithm.
+
+    This class defines logic for a PF using a Prognostics Model (see Prognostics Model Package). This filter uses measurement data with noise to estimate the state of the system using a particles. At each step, particles are predicted forward (with noise). Particles are resampled with replacement from the existing particles according to how well the particles match the observed measurements.
+
+    The supported configuration parameters (keyword arguments) for UKF construction are described below:
+
+    Constructor Configuration Parameters:
+        t0 : float
+            Starting time (s)
+        dt : float 
+            time step (s)
+        num_particles : int
+            Number of particles in particle filter
+        resample_fcn : function 
+            Resampling function ([weights]) -> [indexes] e.g., filterpy.monte_carlo.residual_resample
+        x0_uncertainty : float or dict
+            Initial uncertainty in state. Can be 1. scalar (standard deviation applied to all), or 2. dict (stardard deviation for each)\n
+            e.g., 0.5 or {'state1': 0.5, 'state2': 0.2}
+    """
+    default_parameters = {
+            't0': -1e-99,  # practically 0, but allowing for a 0 first estimate
+            'num_particles': 20, 
+            'resample_fcn': residual_resample,
+            'x0_uncertainty': 0.5
+        }
+
+    def __init__(self, model, x0, measurement_eqn = None, **kwargs):
+        super().__init__(model, x0, measurement_eqn = measurement_eqn, **kwargs)
+        
+        self.__measure = measurement_eqn if measurement_eqn else model.output
+
+        # Build array inplace
+        x = array(list(x0.values()))
+
+        if isinstance(self.parameters['x0_uncertainty'], dict):
+            sd = array([self.parameters['x0_uncertainty'][key] for key in x0.keys()])
+        elif isinstance(self.parameters['x0_uncertainty'], Number):
+            sd = array([self.parameters['x0_uncertainty']] * len(x0))
+        else:
+            raise ProgAlgTypeError
+
+        if 'R' in self.parameters:
+            # For backwards compatibility
+            warn("'R' is deprecated. Use 'measurement_noise' instead.", DeprecationWarning)
+            self.parameters['measurement_noise'] = self.parameters['R']
+        elif 'measurement_noise' not in self.parameters:
+            self.parameters['measurement_noise'] = {key: 0.0 for key in x0.keys()}
+
+        samples = [random.normal(
+            x[i], sd[i], self.parameters['num_particles']) for i in range(len(x))]
+        self.particles = dict(zip(x0.keys(), samples))
+    
+    def __str__(self):
+        return "{} State Estimator".format(self.__class__)
+        
+    def estimate(self, t, u, z):
+        assert t > self.t, "New time must be greater than previous"
+        dt = t - self.t
+        self.t = t
+
+        # Optimization
+        particles = self.particles
+        next_state = self.model.next_state
+        apply_process_noise = self.model.apply_process_noise
+        output = self.__measure
+        apply_measurement_noise = self.model.apply_measurement_noise
+        noise_params = self.model.parameters['measurement_noise']
+        num_particles = self.parameters['num_particles']
+        # Check which output keys are present (i.e., output of measurement function)
+        measurement_keys = output({key: particles[key][0] for key in particles.keys()}).keys()
+        zPredicted = {key: empty(num_particles) for key in measurement_keys}
+
+        if self.model.is_vectorized:
+            # Propagate particles state
+            self.particles = apply_process_noise(next_state(particles, u, dt))
+
+            # Get particle measurements
+            zPredicted = apply_measurement_noise(output(self.particles))
+        else:
+            # Propogate and calculate weights
+            for i in range(num_particles):
+                x = {key: particles[key][i] for key in particles.keys()}
+                x = next_state(x, u, dt) 
+                x = apply_process_noise(x)
+                for key in particles.keys():
+                    self.particles[key][i] = x[key]
+                z = output(x)
+                z = apply_measurement_noise(z)
+                for key in measurement_keys:
+                    zPredicted[key][i] = z[key]
+
+        # Calculate pdf values
+        pdfs = array([norm(zPredicted[key], noise_params[key]).logpdf(z[key])
+                      for key in zPredicted.keys()])
+
+        # Calculate log weights
+        log_weights = pdfs.sum(0)
+
+        # Scale
+        # We subtract the max log weights for numerical stability. 
+        # Sometimes log weights can be a large negative value
+        # when you exponentiate that value the computer will round the result to 0 for most of the weights (sometimes all of them) 
+        # this causes problems when trying to sample from the particles. 
+        # We shift them up by the max log weight (essentially making the max log weight 0) to help avoid that problem. 
+        # When we normalize the weights by dividing by the sum of all the weights, that constant cancels out.
+        max_log_weight = max(log_weights)
+        scaled_weights = log_weights - max_log_weight
+
+        # Convert to weights
+        unnorm_weights = exp(scaled_weights)
+
+        # Normalize
+        total_weight = sum(unnorm_weights)
+        self.weights = unnorm_weights / total_weight
+
+        # Resample indices
+        indexes = self.parameters['resample_fcn'](self.weights)
+
+        # Resampled particles
+        samples = [take(self.particles[state], indexes)
+                   for state in self.particles.keys()]
+
+        # Particles as a dictionary
+        self.particles = dict(zip(self.particles.keys(), samples))
+
+    @property
+    def x(self):
+        """
+        Getter for property 'x', the current estimated state. 
+
+        Example
+        -------
+        state = observer.x
+        """
+        return UnweightedSamples(self.particles)
