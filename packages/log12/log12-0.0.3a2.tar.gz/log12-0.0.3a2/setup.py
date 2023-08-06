796cb78f8283670af5d48f23bea95efc636c1ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:13 +0000

    Add package log12/0.0.3a2/log12-0.0.3a2.tar.gz

---
+++ packages/log12/log12-0.0.3a2.tar.gz/log12-0.0.3a2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION="0.0.3a2"
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="log12",
+    version=VERSION,
+    url='https://github.com/nkratzke/log12',
+    license='MIT',
+    author='Nane Kratzke',
+    author_email='nane.kratzke@th-luebeck.de',
+    description='Logging for 12-factor apps.',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    python_requires=">=3.6,<4.0",
+    packages=find_packages()
+)
