8ad164b837723042a375a6747a1495dfe6996175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:21 +0000

    Add package dongtai-sdk/0.0.9/dongtai_sdk-0.0.9.tar.gz

---
+++ packages/dongtai-sdk/dongtai_sdk-0.0.9.tar.gz/dongtai_sdk-0.0.9/dongtai_sdk/DongTai.py
@@ -0,0 +1,99 @@
+'''
+Author: 饕餮
+Date: 2021-12-23 15:10:01
+version: 
+LastEditors: 饕餮
+LastEditTime: 2021-12-24 12:52:57
+Description: Main
+'''
+from .base.DongTaiProject import DongTaiProject,DongTaiProjectVersion
+from .DongTaiApi import DongTaiApi
+from .base.BaseObejct import DongTaiError
+
+class DongTai:
+    def __init__(self,configPath='config.json'):
+        self.dongTaiApi = DongTaiApi(configPath)
+
+    #[Project Function]
+    def GetProjectList(self,page,pageSize,name=None):
+        returnData = []
+        repData = self.dongTaiApi.GetProjectList(page,pageSize,name)
+        if repData["status"] == 201:
+            for tmpData in repData["data"]:
+                tmpObject = DongTaiProject(tmpData)
+                returnData.append(tmpObject)
+            return returnData
+        else:
+            errorMsg = {"status":repData["status"],"msg":repData["msg"]}
+            errorObject = DongTaiError(errorMsg)
+            return errorObject
+
+    def GetProjectVerList(self,projectId):
+        returnData = []
+        repData = self.dongTaiApi.GetProjectVerList(projectId)
+        if repData["status"] == 201:
+            for tmpData in repData["data"]:
+                tmpObject = DongTaiProjectVersion(tmpData)
+                returnData.append(tmpObject)
+            return returnData
+        else:
+            errorMsg = {"status":repData["status"],"msg":repData["msg"]}
+            errorObject = DongTaiError(errorMsg)
+            return errorObject
+
+    def AddProjectVersion(self,projectId,verName,description,isEdit=True):
+        repData = self.dongTaiApi.AddProjectVersion(projectId,verName,description,isEdit)
+        if repData["status"] == 201:
+            tmpObject = DongTaiProjectVersion(repData["data"])
+            return tmpObject
+        else:
+            errorMsg = {"status":repData["status"],"msg":repData["msg"]}
+            errorObject = DongTaiError(errorMsg)
+            return errorObject
+
+    def SearchProject(self,projectId):
+        repData = self.dongTaiApi.SearchProject(projectId)
+        if repData["status"] == 201:
+            tmpObject = DongTaiProject(repData["data"])
+            return tmpObject
+        else:
+            errorMsg = {"status":repData["status"],"msg":repData["msg"]}
+            errorObject = DongTaiError(errorMsg)
+            return errorObject
+
+    #[Agent Function]
+    def DeleteAgent(self,agentId):
+        repData = self.dongTaiApi.DeleteAgent(agentId)
+        if repData["status"] == 201:
+            return True
+        else:
+            errorMsg = {"status":repData["status"],"msg":repData["msg"]}
+            errorObject = DongTaiError(errorMsg)
+            return errorObject
+
+    def StartAgent(self,agentId):
+        repData = self.dongTaiApi.StartAgent(agentId)
+        if repData["status"] == 201:
+            return True
+        else:
+            errorMsg = {"status":repData["status"],"msg":repData["msg"]}
+            errorObject = DongTaiError(errorMsg)
+            return errorObject
+
+    def StopAgent(self,agentId):
+        repData = self.dongTaiApi.StartAgent(agentId)
+        if repData["status"] == 201:
+            return True
+        else:
+            errorMsg = {"status":repData["status"],"msg":repData["msg"]}
+            errorObject = DongTaiError(errorMsg)
+            return errorObject
+
+    def ModifiedAgentAlias(self,agentId,alias):
+        pass
+
+    def GetAgentList(self,page=1,pageSize=50,projectName=None,state=None,token=None):
+        pass
+
+    def GetAgentDetail(self,agentId):
+        pass
