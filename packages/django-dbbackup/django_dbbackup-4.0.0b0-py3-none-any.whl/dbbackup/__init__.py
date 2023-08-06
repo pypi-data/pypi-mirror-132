018e6a73a30fa4797823eb8837c8f1f4e704af2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:26 +0000

    Add package django-dbbackup/4.0.0b0/django_dbbackup-4.0.0b0-py3-none-any.whl

---
+++ packages/django-dbbackup/django_dbbackup-4.0.0b0-py3-none-any.whl/dbbackup/__init__.py
@@ -0,0 +1,6 @@
+"""Management commands to help backup and restore a project database and media"""
+
+import django
+
+if django.VERSION < (3, 2):
+    default_app_config = 'dbbackup.apps.DbbackupConfig'
