8245286edc96c17bd98e6027237ec7008dca5128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:43 +0000

    Add package nummethvera/0.1.3/nummethvera-0.1.3.tar.gz

---
+++ packages/nummethvera/nummethvera-0.1.3.tar.gz/nummethvera-0.1.3/nummethvera/__init__.py
@@ -0,0 +1,9 @@
+from . import approxinterp
+from . import diffequat
+from . import diffintegr
+from . import matrixop
+from . import otjigants
+from . import slau
+from . import veivletfurie
+
+__version__ = '0.1.3'
