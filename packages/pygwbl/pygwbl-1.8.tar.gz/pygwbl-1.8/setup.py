d0a41c5b86299fb3472e6cbea49a24723533c695
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:18 +0000

    Add package pygwbl/1.8/pygwbl-1.8.tar.gz

---
+++ packages/pygwbl/pygwbl-1.8.tar.gz/pygwbl-1.8/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+import os
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='pygwbl',
+    version='1.8',
+    py_modules=['pygwbl'],  # 这个要跟发布的模块名一致
+    author='Dagwbl',
+    author_email='Dagwbl@qq.com',
+    url='https://github.com/Dagwbl/pygwbl',
+    project_urls={
+        "Bug Tracker": "https://github.com/Dagwbl/pygwbl/issues",
+    },
+    description="Dagwbl's personal tool set.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6"
+    )
