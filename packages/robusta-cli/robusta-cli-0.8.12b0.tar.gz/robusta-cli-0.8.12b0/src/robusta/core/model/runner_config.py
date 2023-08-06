f4b332389e9c3bc5f754603268d6836dedbeb63a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:39 +0000

    Add package robusta-cli/0.8.12b0/robusta-cli-0.8.12b0.tar.gz

---
+++ packages/robusta-cli/robusta-cli-0.8.12b0.tar.gz/robusta-cli-0.8.12b0/src/robusta/core/model/runner_config.py
@@ -0,0 +1,32 @@
+from typing import List, Optional, Union, Dict
+from pydantic import BaseModel, SecretStr
+
+from ...model.playbook_definition import PlaybookDefinition
+from ..sinks.datadog.datadog_sink import DataDogSinkConfigWrapper
+from ..sinks.kafka.kafka_sink import KafkaSinkConfigWrapper
+from ..sinks.robusta.robusta_sink import RobustaSinkConfigWrapper
+from ..sinks.slack.slack_sink import SlackSinkConfigWrapper
+
+
+class PlaybookRepo(BaseModel):
+    url: str
+    key: Optional[SecretStr] = SecretStr("")
+    pip_install: bool = (
+        True  # Set to False, if the playbooks package is already in site-packages.
+    )
+
+
+class RunnerConfig(BaseModel):
+    playbook_repos: Dict[str, PlaybookRepo]
+    sinks_config: Optional[
+        List[
+            Union[
+                RobustaSinkConfigWrapper,
+                SlackSinkConfigWrapper,
+                DataDogSinkConfigWrapper,
+                KafkaSinkConfigWrapper,
+            ]
+        ]
+    ]
+    global_config: Optional[dict] = {}
+    active_playbooks: Optional[List[PlaybookDefinition]] = []
