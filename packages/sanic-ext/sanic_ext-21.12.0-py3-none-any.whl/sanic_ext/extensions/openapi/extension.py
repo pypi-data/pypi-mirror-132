3242af580cc2e0023aea0c126f4a2ebe783cfc73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:43 +0000

    Add package sanic-ext/21.12.0/sanic_ext-21.12.0-py3-none-any.whl

---
+++ packages/sanic-ext/sanic_ext-21.12.0-py3-none-any.whl/sanic_ext/extensions/openapi/extension.py
@@ -0,0 +1,29 @@
+from ..base import Extension
+from .blueprint import blueprint_factory
+
+
+class OpenAPIExtension(Extension):
+    name = "openapi"
+
+    def startup(self, _) -> None:
+        if self.app.config.OAS:
+            self.bp = blueprint_factory(self.app.config)
+            self.app.blueprint(self.bp)
+
+    def label(self):
+        if self.app.config.OAS:
+            return self._make_url()
+
+        return ""
+
+    def _make_url(self):
+        name = f"{self.bp.name}.index"
+        _server = (
+            None
+            if "SERVER_NAME" in self.app.config
+            else self.app.serve_location
+        ) or None
+        _external = bool(_server) or "SERVER_NAME" in self.app.config
+        return (
+            f"[{self.app.url_for(name, _external=_external, _server=_server)}]"
+        )
