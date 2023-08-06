cc0caa34ff95e4be9eb0b3e89c25fd32dca402a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:42 +0000

    Add package GS-PRACTICE/1.0.0/GS-PRACTICE-1.0.0.tar.gz

---
+++ packages/GS-PRACTICE/GS-PRACTICE-1.0.0.tar.gz/GS-PRACTICE-1.0.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="GS-PRACTICE",
+    version="1.0.0",
+    author="shirotak",
+    author_email="tshiro@kuhp.kyoto-u.ac.jp",
+    description="Tumor genomic subtyping using mutational signatures",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shirotak/GS-PRACTICE",
+    project_urls={
+        "Bug Tracker": "https://github.com/shirotak/GS-PRACTICE/issues",
+    },
+    packages=['gspractice'],
+    package_dir={"gspractice":"src/gspractice"},
+    package_data={'gspractice':['data/*']},
+    entry_points={
+        'console_scripts': ['gs-practice = gspractice.run_gspractice:runAsStandalone',
+            'gs-makeclfs = gspractice.makeclfs:main'
+            ]
+    },
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    )
