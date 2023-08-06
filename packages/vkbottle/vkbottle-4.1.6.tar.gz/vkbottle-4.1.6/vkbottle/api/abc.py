62af4caf1ba1e5e71e439bd6cc6f2cae666ea26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:47 +0000

    Add package vkbottle/4.1.6/vkbottle-4.1.6.tar.gz

---
+++ packages/vkbottle/vkbottle-4.1.6.tar.gz/vkbottle-4.1.6/vkbottle/api/abc.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    from vkbottle.http import ABCHTTPClient
+
+    from .request_rescheduler import ABCRequestRescheduler
+
+
+class ABCAPI(ABC):
+    """Abstract API class
+    Documentation: https://github.com/vkbottle/vkbottle/blob/master/docs/low-level/api/api.md
+    """
+
+    http_client: "ABCHTTPClient"
+    ignore_errors: bool
+    request_rescheduler: "ABCRequestRescheduler"
+
+    @abstractmethod
+    async def request(self, method: str, data: dict) -> dict:
+        """Makes a single request opening a session"""
+
+    @abstractmethod
+    async def validate_response(self, method: str, data: dict, response: Any) -> Any:
+        pass
+
+    @abstractmethod
+    async def validate_request(self, request: Any) -> Any:
+        pass
