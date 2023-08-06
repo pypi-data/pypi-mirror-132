6382514d898efcc808ecbc5668dfafeb71f57c85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:06 +0000

    Add package diopy/0.5.5/diopy-0.5.5.tar.gz

---
+++ packages/diopy/diopy-0.5.5.tar.gz/diopy-0.5.5/README.md
@@ -0,0 +1,12 @@
+# diopy
+
+The package dior is the part of scDIOR, which is used for single-cell data IO between R and Python (Python version)
+
+## installation
+We recommend using pip for installation
+```shell
+pip install diopy
+```
+
+
+
---
+++ packages/diopy/diopy-0.5.5.tar.gz/diopy-0.5.5/setup.py
@@ -0,0 +1,32 @@
+import glob, os
+from setuptools import setup, find_packages
+
+path = os.path.abspath(os.path.dirname(__file__))
+
+try:
+  with open(os.path.join(path, 'README.md')) as f:
+    long_description = f.read()
+except Exception as e:
+  long_description = "The single cell data IO between R and Python(Python version)"
+  
+
+setup(
+    name = "diopy",
+    version = "0.5.5",
+    keywords = ["scRNA-seq", "hdf5", "data IO", "scanpy"],
+    description = "The single cell data IO between R and Python(Python version)",
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    python_requires=">=3.5.0",
+    license = "MIT License",
+    author = "Huijian Feng",
+    author_email = "fenghuijian@outlook.com",
+    packages = find_packages(),
+    include_package_data = True,
+    # If any package contains *.r files, include them:
+    package_data={'': ['*.R']},
+    requires = ["scipy", "scanpy","pandas", "numpy", "anndata","re","os","h5py","typing", "argparse"],
+    platforms = "any",
+    # packages=['diopy'],
+    scripts = ['bin/scdior']
+)
