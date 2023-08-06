95ecdc29c2cd874ccc64708e14a252d0deb1c1b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:37 +0000

    Add package mongodf/0.0.1/mongodf-0.0.1.tar.gz

---
+++ packages/mongodf/mongodf-0.0.1.tar.gz/mongodf-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+import mongodf
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='mongodf',
+    url="https://github.com/VK/mlflow-pyfunc-server",
+    version=mongodf.__version__,
+    packages=find_packages(),
+    license = "MIT",
+    keywords = "mongoDB pandas dataframe filter",
+    long_description=read('README.md'),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'pymongo',
+        'pandas'
+    ]
+)
