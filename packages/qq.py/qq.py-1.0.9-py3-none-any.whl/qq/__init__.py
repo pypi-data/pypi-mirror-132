8d18f37769a1fee69d5071c7e68854bf95c9b398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:45 +0000

    Add package qq.py/1.0.9/qq.py-1.0.9-py3-none-any.whl

---
+++ packages/qq.py/qq.py-1.0.9-py3-none-any.whl/qq/__init__.py
@@ -0,0 +1,39 @@
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
+__title__ = 'qq'
+__author__ = 'Foxwhite'
+__license__ = 'MIT'
+__version__ = '1.0.9'
+
+import logging
+from typing import NamedTuple, Literal
+from .client import *
+from .user import *
+from .channel import *
+from .guild import *
+from .flags import *
+from .member import *
+from .message import *
+from .asset import *
+from .role import *
+from .file import *
+from .colour import *
+from .object import *
+from . import utils, abc
+from .enum import *
+from .shard import *
+from .error import *
+from .embeds import *
+from .mention import *
+from .partial_emoji import *
+from .raw_models import *
+
+
+class VersionInfo(NamedTuple):
+    major: int
+    minor: int
+    micro: int
+    releaselevel: Literal["alpha", "beta", "candidate", "final"]
+    serial: int
+
+
+version_info: VersionInfo = VersionInfo(major=1, minor=0, micro=9, releaselevel='beta', serial=0)
