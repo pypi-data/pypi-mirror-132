41969d4e29485ef7301bcc890fe1288a30af0f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:01 +0000

    Add package t-mo/0.0.1/t%D0%B0%D0%B0%D0%B0%D0%B0%D0%B0mo-0.0.1.tar.gz

---
+++ packages/t-mo/t%D0%B0%D0%B0%D0%B0%D0%B0%D0%B0mo-0.0.1.tar.gz/tаааааmo-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Test'
+
+# Setting up
+setup(
+    name="tаааааmo",
+    version=VERSION,
+    author="jjl",
+    author_email="<kallerona132@hotmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
