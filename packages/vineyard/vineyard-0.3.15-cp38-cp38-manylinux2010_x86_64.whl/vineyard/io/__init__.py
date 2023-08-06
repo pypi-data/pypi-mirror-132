e0f18e6db763a4796c8bc84cb9cfbacf5c54ef1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:46 +0000

    Add package vineyard/0.3.15/vineyard-0.3.15-cp38-cp38-manylinux2010_x86_64.whl

---
+++ packages/vineyard/vineyard-0.3.15-cp38-cp38-manylinux2010_x86_64.whl/vineyard/io/__init__.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020-2021 Alibaba Group Holding Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from .stream import open, read, write
+from .serialization import serialize, deserialize
+
+from . import byte
+from . import dataframe
+from . import recordbatch
+from . import parallel
+
+from vineyard.core.builder import default_builder_context
+from vineyard.core.resolver import default_resolver_context
+from vineyard.io.byte import register_byte_stream_types
+from vineyard.io.dataframe import register_dataframe_stream_types
+from vineyard.io.recordbatch import register_recordbatch_stream_types
+from vineyard.io.parallel import register_parallel_stream_types
+
+
+def register_builtin_stream_types(builder_ctx, resolver_ctx):
+    register_byte_stream_types(builder_ctx, resolver_ctx)
+    register_dataframe_stream_types(builder_ctx, resolver_ctx)
+    register_recordbatch_stream_types(builder_ctx, resolver_ctx)
+    register_parallel_stream_types(builder_ctx, resolver_ctx)
+
+
+# Those builtin builders and resolvers will be registered by default, for better
+# import-and-play experience.
+register_builtin_stream_types(default_builder_context, default_resolver_context)
