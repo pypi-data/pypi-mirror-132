aeb761a8fa67d3c44d2e2704e89f2ba70c5d13a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:54 +0000

    Add package disgames/2.1.1/disgames-2.1.1.tar.gz

---
+++ packages/disgames/disgames-2.1.1.tar.gz/disgames-2.1.1/disgames/constants.py
@@ -0,0 +1,17 @@
+from .mixins import *
+
+ALL_GAMES = [
+    TicTacToe,
+    Hangman,
+    MadLib,
+    Chess,
+    Minesweeper,
+    Sokoban,
+    RPS,
+    Connect4,
+    _2048,
+    Checkers,
+    Sudoko,
+    TicTacToeReactions,
+    Battleships,
+]
