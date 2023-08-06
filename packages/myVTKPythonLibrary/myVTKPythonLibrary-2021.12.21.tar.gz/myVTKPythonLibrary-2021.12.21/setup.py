cd397ea72139c0313548628330161383c31ef1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:44:09 +0000

    Add package myVTKPythonLibrary/2021.12.21/myVTKPythonLibrary-2021.12.21.tar.gz

---
+++ packages/myVTKPythonLibrary/myVTKPythonLibrary-2021.12.21.tar.gz/myVTKPythonLibrary-2021.12.21/setup.py
@@ -0,0 +1,33 @@
+import datetime
+import os
+import setuptools
+
+# version = os.environ['CI_COMMIT_TAG']
+version = datetime.date.today().strftime("%Y.%m.%d")
+
+setuptools.setup(
+    name="myVTKPythonLibrary",
+    version=version,
+    author="Martin Genet",
+    author_email="martin.genet@polytechnique.edu",
+    description=open("README.md", "r").readlines()[1][:-1],
+    long_description = open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    url="https://gitlab.inria.fr/mgenet/myVTKPythonLibrary",
+    packages=["myVTKPythonLibrary"],
+    license="GPLv3",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['numpy', 'vtk', 'myPythonLibrary'],
+    # python_requires='>=3.6',
+)
+
+# keyring set https://upload.pypi.org/legacy martin.genet
+# keyring set https://upload.pypi.org martin.genet
+
+# python setup.py sdist bdist_wheel
+
+# twine upload dist/*
