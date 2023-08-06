3c17208fb370d6fbe3484a79570ef3203912bad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:35 +0000

    Add package Pro-video-tools/1.0/Pro-video-tools-1.0.tar.gz

---
+++ packages/Pro-video-tools/Pro-video-tools-1.0.tar.gz/Pro-video-tools-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name='Pro-video-tools',
+    version=1.0,
+    description='Este pacote te permite fazer operações em videos e camera',
+    author='jhonatan de souza',
+    author_email='jhonatantools@gmail.com',
+    keywords=['video','camera'],
+    packages=find_packages()
+)
