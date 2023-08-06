70bd60e2ae3257d233b65c5edb04517f33b34e17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:43 +0000

    Add package eabr-functions/2.4.1/eabr-functions-2.4.1.tar.gz

---
+++ packages/eabr-functions/eabr-functions-2.4.1.tar.gz/eabr-functions-2.4.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eabr-functions",
+    version="2.4.1",
+    author="Walter José Avelino da Silva",
+    author_email="walter.avelin@gmail.com",
+    description="EABR Functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/walteravelino/eabr-functions",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "botocore",
+        "psycopg2-binary",
+        "cx-Oracle",
+        "pymongo",
+        "pymssql",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
