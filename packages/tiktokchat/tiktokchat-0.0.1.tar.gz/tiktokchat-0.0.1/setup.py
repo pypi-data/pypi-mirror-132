5c266f4d0e0053be6fec73a3b99b0c3189427659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:36 +0000

    Add package tiktokchat/0.0.1/tiktokchat-0.0.1.tar.gz

---
+++ packages/tiktokchat/tiktokchat-0.0.1.tar.gz/tiktokchat-0.0.1/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Learn more: https://github.com/kennethreitz/setup.py
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='tiktokchat',
+    version='0.0.1',
+    description='TikTok Chat API',
+    long_description=readme,
+    author='Mykola Odnosumov',
+    author_email='mkloouo@gmail.com',
+    url='https://github.com/mkloouo/tiktokchat',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
