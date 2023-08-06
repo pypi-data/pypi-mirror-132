ae7a2805fc7b55ea901ee04c4ed97137e60f4b81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:18 +0000

    Add package byte-live-room-biubiu/0.0.2/byte-live-room-biubiu-0.0.2.tar.gz

---
+++ packages/byte-live-room-biubiu/byte-live-room-biubiu-0.0.2.tar.gz/byte-live-room-biubiu-0.0.2/src/byte_live_room_biubiu/starp.py
@@ -0,0 +1,18 @@
+from PySide6.QtWidgets import QApplication
+from Widget import Widget
+import sys
+
+
+def run():
+    app = QApplication([])
+
+    widget = Widget()
+    widget.resize(850, 670)
+    widget.show()
+
+    sys.exit(app.exec())
+
+#
+# if __name__ == "__main__":
+#     run()
+#
