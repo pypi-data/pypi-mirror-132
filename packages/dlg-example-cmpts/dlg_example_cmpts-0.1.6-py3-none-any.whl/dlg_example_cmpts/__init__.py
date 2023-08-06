61ae1f242ffead6d1ed02cf920abd6ae3c4bd0f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:32 +0000

    Add package dlg-example-cmpts/0.1.6/dlg_example_cmpts-0.1.6-py3-none-any.whl

---
+++ packages/dlg-example-cmpts/dlg_example_cmpts-0.1.6-py3-none-any.whl/dlg_example_cmpts/__init__.py
@@ -0,0 +1,24 @@
+__package__ = "dlg_example_cmpts"
+# The following imports are the binding to the DALiuGE system
+# extend the following as required
+from .apps import (
+    AdvUrlRetrieve,
+    ExtractColumn,
+    FileGlob,
+    GenericGather,
+    MyBranch,
+    PickOne,
+    String2JSON,
+)
+from .data import MyDataDROP
+
+__all__ = [
+    "MyBranch",
+    "MyDataDROP",
+    "FileGlob",
+    "PickOne",
+    "String2JSON",
+    "ExtractColumn",
+    "AdvUrlRetrieve",
+    "GenericGather",
+]
