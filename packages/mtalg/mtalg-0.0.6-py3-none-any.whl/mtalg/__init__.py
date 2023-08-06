8196ab7678a37712e6efa9b55cc93e1a778c8540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:58 +0000

    Add package mtalg/0.0.6/mtalg-0.0.6-py3-none-any.whl

---
+++ packages/mtalg/mtalg-0.0.6-py3-none-any.whl/mtalg/__init__.py
@@ -0,0 +1,4 @@
+from .__about__ import (__title__, __version__,
+                        __about__, __email__,
+                        __authors__, __url__)
+from .alg import add, sub, div, mul, pow, std
