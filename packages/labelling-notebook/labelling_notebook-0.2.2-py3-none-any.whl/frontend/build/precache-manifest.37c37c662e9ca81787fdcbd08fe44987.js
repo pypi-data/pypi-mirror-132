b35c6dffd1f91a77067323ee7423558df6a9fe4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:23 +0000

    Add package labelling-notebook/0.2.2/labelling_notebook-0.2.2-py3-none-any.whl

---
+++ packages/labelling-notebook/labelling_notebook-0.2.2-py3-none-any.whl/frontend/build/precache-manifest.37c37c662e9ca81787fdcbd08fe44987.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ac820c1c0954cb256c03a8c47b157c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c22ace8fdb262ec3df84",
+    "url": "/static/css/2.f6cb0e92.chunk.css"
+  },
+  {
+    "revision": "c22ace8fdb262ec3df84",
+    "url": "/static/js/2.d409bad2.chunk.js"
+  },
+  {
+    "revision": "558d9ba715e8896fff53a7afdf83c7f7",
+    "url": "/static/js/2.d409bad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4743e408580010a37320",
+    "url": "/static/js/main.45166f15.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
