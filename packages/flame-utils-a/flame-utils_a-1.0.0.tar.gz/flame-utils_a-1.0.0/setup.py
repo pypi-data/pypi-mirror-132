ebffedfe2c24de27643e6e69a9c66cdf0e9b76ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:38 +0000

    Add package flame-utils-a/1.0.0/flame-utils_a-1.0.0.tar.gz

---
+++ packages/flame-utils-a/flame-utils_a-1.0.0.tar.gz/flame-utils_a-1.0.0/setup.py
@@ -0,0 +1,25 @@
+import pathlib
+from setuptools import setup
+
+HERE=pathlib.Path(__file__).parent
+
+setup(
+    name ="flame-utils_a",
+    version = "1.0.0",
+    author = "Urmila",
+    author_email = "urmilaraj18@gmail.com",
+    description = "Anomaly detection on scientific datasets",
+    long_description ="README",
+    long_description_content_type = "text/markdown",
+    license="MIT",
+    classifiers = [
+       "License :: OSI Approved :: MIT License",
+       "Programming Language :: Python :: 3.6",
+       "Programming Language :: Python :: 3.7",
+       "Programming Language :: Python :: 3.8",
+    ],
+    packages=["flame_utils"],
+    install_requires=["numpy","progressbar","matplotlib"],
+)
+
+
