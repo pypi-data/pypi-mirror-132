c92c6e9d80b20ca879b5c8afaac70007b5ee4334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:53 +0000

    Add package cloudshell-shell-connectivity-flow/3.0.0/cloudshell-shell-connectivity-flow-3.0.0.zip

---
+++ packages/cloudshell-shell-connectivity-flow/cloudshell-shell-connectivity-flow-3.0.0.zip/cloudshell-shell-connectivity-flow-3.0.0/tests/cloudshell/shell/flows/connectivity/models/test_connectivity_model.py
@@ -0,0 +1,21 @@
+from cloudshell.shell.flows.connectivity.models.connectivity_model import (
+    ConnectivityActionModel,
+)
+
+
+def test_connectivity_action_model(action_request):
+    action = ConnectivityActionModel.parse_obj(action_request)
+    assert action.action_id == action_request["actionId"]
+    assert action.type is action.type.REMOVE_VLAN
+    assert action.type.value == "removeVlan"
+    assert action.connection_id == action_request["connectionId"]
+    assert action.connection_params.vlan_id == "10-11"
+    assert action.connection_params.mode is action.connection_params.mode.TRUNK
+    assert action.connection_params.mode.value == "Trunk"
+    assert action.connection_params.vlan_service_attrs.qnq is False
+    assert action.connection_params.vlan_service_attrs.ctag == ""
+    assert action.connector_attrs.interface == "mac address"
+    assert action.action_target.name == "centos"
+    assert action.action_target.address == "full address"
+    assert action.custom_action_attrs.vm_uuid == "vm_uid"
+    assert action.custom_action_attrs.vnic == "vnic"
