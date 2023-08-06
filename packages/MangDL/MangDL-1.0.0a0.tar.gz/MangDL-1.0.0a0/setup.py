8be6bb4236afb80d7f1eee5a6cff4f912bbb9ec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:08 +0000

    Add package MangDL/1.0.0a0/MangDL-1.0.0a0.tar.gz

---
+++ packages/MangDL/MangDL-1.0.0a0.tar.gz/MangDL-1.0.0a0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages, setup
+
+with open("version", "r") as f:
+    version = f.read().strip()
+
+setup(
+    name="MangDL",
+	author="whinee",
+	author_email="whinyaan@gmail.com",
+    version=version,
+    description="The most inefficent Manga downloader for PC",
+    long_description="file: docs/README.md",
+    long_description_content_type="text/markdown",
+    url="https://github.com/MangDL/MangDL",
+	license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+	packages=find_packages(),
+	include_package_data=True,
+    python_requires=">=3.9",
+	install_requires=[
+        "BeautifulSoup4",
+		"click",
+        "httpx",
+		"lxml",
+        "patool",
+        "pyyaml",
+        "tabulate",
+        "toml",
+        "tqdm",
+        "yachalk",
+        "yarl",
+	],
+	entry_points = {
+        'console_scripts': ['mangdl=mangdl.cli:cli'],
+    },
+)
