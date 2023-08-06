4e4ffc66b495ce2ec718e97aff30191a0262fe40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:29 +0000

    Add package NonameOrm/1.0.4/NonameOrm-1.0.4-cp38-cp38-win_amd64.whl

---
+++ packages/NonameOrm/NonameOrm-1.0.4-cp38-cp38-win_amd64.whl/NonameOrm/DB/DB.pyi
@@ -0,0 +1,33 @@
+from typing import Callable, Coroutine
+
+from NonameOrm.DB.Connector import BaseConnector
+
+
+class DB:
+    def __init__(self, connector: BaseConnector, *args, **kwargs): ...
+
+    @classmethod
+    def create(cls, *args, **kwargs) -> DB: ...
+
+    @property
+    def execute(self) -> Coroutine or Callable: ...
+
+    def GenerateTable(self) -> DB: ...
+
+    @property
+    def instance(self) -> DB: ...
+
+    @property
+    def connector(self) -> BaseConnector:...
+
+    @property
+    def ConnectorType(self): ...
+
+    @classmethod
+    def getInstance(cls) -> DB: ...
+
+    @property
+    def executeSql(self)-> Callable or Coroutine:...
+
+    @classmethod
+    def getInstanceWithCheck(cls) -> DB: ...
