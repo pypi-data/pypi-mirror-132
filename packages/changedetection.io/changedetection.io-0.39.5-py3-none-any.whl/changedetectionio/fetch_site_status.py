ed572ea457509cefc0662547e09c3cb9274994ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:58 +0000

    Add package changedetection.io/0.39.5/changedetection.io-0.39.5-py3-none-any.whl

---
+++ packages/changedetection.io/changedetection.io-0.39.5-py3-none-any.whl/changedetectionio/fetch_site_status.py
@@ -0,0 +1,179 @@
+import time
+from changedetectionio import content_fetcher
+import hashlib
+from inscriptis import get_text
+import urllib3
+from . import html_tools
+import re
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+# Some common stuff here that can be moved to a base class
+class perform_site_check():
+
+    def __init__(self, *args, datastore, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.datastore = datastore
+
+    def strip_ignore_text(self, content, list_ignore_text):
+        import re
+        ignore = []
+        ignore_regex = []
+        for k in list_ignore_text:
+
+            # Is it a regex?
+            if k[0] == '/':
+                ignore_regex.append(k.strip(" /"))
+            else:
+                ignore.append(k)
+
+        output = []
+        for line in content.splitlines():
+
+            # Always ignore blank lines in this mode. (when this function gets called)
+            if len(line.strip()):
+                regex_matches = False
+
+                # if any of these match, skip
+                for regex in ignore_regex:
+                    try:
+                        if re.search(regex, line, re.IGNORECASE):
+                            regex_matches = True
+                    except Exception as e:
+                        continue
+
+                if not regex_matches and not any(skip_text in line for skip_text in ignore):
+                    output.append(line.encode('utf8'))
+
+        return "\n".encode('utf8').join(output)
+
+
+
+    def run(self, uuid):
+        timestamp = int(time.time())  # used for storage etc too
+
+        changed_detected = False
+        stripped_text_from_html = ""
+
+        watch = self.datastore.data['watching'][uuid]
+
+        update_obj = {'previous_md5': self.datastore.data['watching'][uuid]['previous_md5'],
+                      'history': {},
+                      "last_checked": timestamp
+                      }
+
+        extra_headers = self.datastore.get_val(uuid, 'headers')
+
+        # Tweak the base config with the per-watch ones
+        request_headers = self.datastore.data['settings']['headers'].copy()
+        request_headers.update(extra_headers)
+
+        # https://github.com/psf/requests/issues/4525
+        # Requests doesnt yet support brotli encoding, so don't put 'br' here, be totally sure that the user cannot
+        # do this by accident.
+        if 'Accept-Encoding' in request_headers and "br" in request_headers['Accept-Encoding']:
+            request_headers['Accept-Encoding'] = request_headers['Accept-Encoding'].replace(', br', '')
+
+        # @todo check the failures are really handled how we expect
+
+        else:
+            timeout = self.datastore.data['settings']['requests']['timeout']
+            url = self.datastore.get_val(uuid, 'url')
+
+            # Pluggable content fetcher
+            prefer_backend = watch['fetch_backend']
+            if hasattr(content_fetcher, prefer_backend):
+                klass = getattr(content_fetcher, prefer_backend)
+            else:
+                # If the klass doesnt exist, just use a default
+                klass = getattr(content_fetcher, "html_requests")
+
+
+            fetcher = klass()
+            fetcher.run(url, timeout, request_headers)
+            # Fetching complete, now filters
+            # @todo move to class / maybe inside of fetcher abstract base?
+
+            # @note: I feel like the following should be in a more obvious chain system
+            #  - Check filter text
+            #  - Is the checksum different?
+            #  - Do we convert to JSON?
+            # https://stackoverflow.com/questions/41817578/basic-method-chaining ?
+            # return content().textfilter().jsonextract().checksumcompare() ?
+
+            is_html = True
+            css_filter_rule = watch['css_filter']
+            if css_filter_rule and len(css_filter_rule.strip()):
+                if 'json:' in css_filter_rule:
+                    stripped_text_from_html = html_tools.extract_json_as_string(content=fetcher.content, jsonpath_filter=css_filter_rule)
+                    is_html = False
+                else:
+                    # CSS Filter, extract the HTML that matches and feed that into the existing inscriptis::get_text
+                    stripped_text_from_html = html_tools.css_filter(css_filter=css_filter_rule, html_content=fetcher.content)
+
+            if is_html:
+                # CSS Filter, extract the HTML that matches and feed that into the existing inscriptis::get_text
+                html_content = fetcher.content
+                if css_filter_rule and len(css_filter_rule.strip()):
+                    html_content = html_tools.css_filter(css_filter=css_filter_rule, html_content=fetcher.content)
+
+                # get_text() via inscriptis
+                stripped_text_from_html = get_text(html_content)
+
+            # We rely on the actual text in the html output.. many sites have random script vars etc,
+            # in the future we'll implement other mechanisms.
+
+            update_obj["last_check_status"] = fetcher.get_last_status_code()
+            update_obj["last_error"] = False
+
+
+            # If there's text to skip
+            # @todo we could abstract out the get_text() to handle this cleaner
+            if len(watch['ignore_text']):
+                stripped_text_from_html = self.strip_ignore_text(stripped_text_from_html, watch['ignore_text'])
+            else:
+                stripped_text_from_html = stripped_text_from_html.encode('utf8')
+
+
+            fetched_md5 = hashlib.md5(stripped_text_from_html).hexdigest()
+
+            blocked_by_not_found_trigger_text = False
+
+            if len(watch['trigger_text']):
+                blocked_by_not_found_trigger_text = True
+                for line in watch['trigger_text']:
+                    # Because JSON wont serialize a re.compile object
+                    if line[0] == '/' and line[-1] == '/':
+                        regex = re.compile(line.strip('/'), re.IGNORECASE)
+                        # Found it? so we don't wait for it anymore
+                        r = re.search(regex, str(stripped_text_from_html))
+                        if r:
+                            blocked_by_not_found_trigger_text = False
+                            break
+
+                    elif line.lower() in str(stripped_text_from_html).lower():
+                        # We found it don't wait for it.
+                        blocked_by_not_found_trigger_text = False
+                        break
+
+
+            # could be None or False depending on JSON type
+            # On the first run of a site, watch['previous_md5'] will be an empty string
+            if not blocked_by_not_found_trigger_text and watch['previous_md5'] != fetched_md5:
+                changed_detected = True
+
+                # Don't confuse people by updating as last-changed, when it actually just changed from None..
+                if self.datastore.get_val(uuid, 'previous_md5'):
+                    update_obj["last_changed"] = timestamp
+
+                update_obj["previous_md5"] = fetched_md5
+
+            # Extract title as title
+            if is_html:
+                if self.datastore.data['settings']['application']['extract_title_as_title'] or watch['extract_title_as_title']:
+                    if not watch['title'] or not len(watch['title']):
+                        update_obj['title'] = html_tools.extract_element(find='title', html_content=fetcher.content)
+
+
+        return changed_detected, update_obj, stripped_text_from_html
---
+++ packages/changedetection.io/changedetection.io-0.39.5-py3-none-any.whl/changedetectionio/update_worker.py
@@ -0,0 +1,127 @@
+import threading
+import queue
+import time
+
+# Requests for checking on the site use a pool of thread Workers managed by a Queue.
+class update_worker(threading.Thread):
+    current_uuid = None
+
+    def __init__(self, q, notification_q, app, datastore, *args, **kwargs):
+        self.q = q
+        self.app = app
+        self.notification_q = notification_q
+        self.datastore = datastore
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        from changedetectionio import fetch_site_status
+
+        update_handler = fetch_site_status.perform_site_check(datastore=self.datastore)
+
+        while not self.app.config.exit.is_set():
+
+            try:
+                uuid = self.q.get(block=False)
+            except queue.Empty:
+                pass
+
+            else:
+                self.current_uuid = uuid
+                from changedetectionio import content_fetcher
+
+                if uuid in list(self.datastore.data['watching'].keys()):
+
+                    changed_detected = False
+                    contents = ""
+                    update_obj= {}
+
+                    try:
+                        now = time.time()
+                        changed_detected, update_obj, contents = update_handler.run(uuid)
+
+                        # Always record that we atleast tried
+                        self.datastore.update_watch(uuid=uuid, update_obj={'fetch_time': round(time.time() - now, 3)})
+
+                    except PermissionError as e:
+                        self.app.logger.error("File permission error updating", uuid, str(e))
+                    except content_fetcher.EmptyReply as e:
+                        self.datastore.update_watch(uuid=uuid, update_obj={'last_error':str(e)})
+
+                    except Exception as e:
+                        self.app.logger.error("Exception reached processing watch UUID:%s - %s", uuid, str(e))
+                        self.datastore.update_watch(uuid=uuid, update_obj={'last_error': str(e)})
+
+                    else:
+                        if update_obj:
+                            try:
+                                self.datastore.update_watch(uuid=uuid, update_obj=update_obj)
+                                if changed_detected:
+                                    n_object = {}
+                                    # A change was detected
+                                    fname = self.datastore.save_history_text(watch_uuid=uuid, contents=contents)
+
+                                    # Update history with the stripped text for future reference, this will also mean we save the first
+                                    # Should always be keyed by string(timestamp)
+                                    self.datastore.update_watch(uuid, {"history": {str(update_obj["last_checked"]): fname}})
+
+                                    watch = self.datastore.data['watching'][uuid]
+
+                                    print (">> Change detected in UUID {} - {}".format(uuid, watch['url']))
+
+                                    # Notifications should only trigger on the second time (first time, we gather the initial snapshot)
+                                    if len(watch['history']) > 1:
+
+                                        dates = list(watch['history'].keys())
+                                        # Convert to int, sort and back to str again
+                                        # @todo replace datastore getter that does this automatically
+                                        dates = [int(i) for i in dates]
+                                        dates.sort(reverse=True)
+                                        dates = [str(i) for i in dates]
+
+                                        prev_fname = watch['history'][dates[1]]
+
+
+                                        # Did it have any notification alerts to hit?
+                                        if len(watch['notification_urls']):
+                                            print(">>> Notifications queued for UUID from watch {}".format(uuid))
+                                            n_object['notification_urls'] = watch['notification_urls']
+                                            n_object['notification_title'] = watch['notification_title']
+                                            n_object['notification_body'] = watch['notification_body']
+                                            n_object['notification_format'] = watch['notification_format']
+
+                                        # No? maybe theres a global setting, queue them all
+                                        elif len(self.datastore.data['settings']['application']['notification_urls']):
+                                            print(">>> Watch notification URLs were empty, using GLOBAL notifications for UUID: {}".format(uuid))
+                                            n_object['notification_urls'] = self.datastore.data['settings']['application']['notification_urls']
+                                            n_object['notification_title'] = self.datastore.data['settings']['application']['notification_title']
+                                            n_object['notification_body'] = self.datastore.data['settings']['application']['notification_body']
+                                            n_object['notification_format'] = self.datastore.data['settings']['application']['notification_format']
+                                        else:
+                                            print(">>> NO notifications queued, watch and global notification URLs were empty.")
+
+                                        # Only prepare to notify if the rules above matched
+                                        if 'notification_urls' in n_object:
+                                            # HTML needs linebreak, but MarkDown and Text can use a linefeed
+                                            if n_object['notification_format'] == 'HTML':
+                                                line_feed_sep = "</br>"
+                                            else:
+                                                line_feed_sep = "\n"
+
+                                            from changedetectionio import diff
+                                            n_object.update({
+                                                'watch_url': watch['url'],
+                                                'uuid': uuid,
+                                                'current_snapshot': str(contents),
+                                                'diff_full': diff.render_diff(prev_fname, fname, line_feed_sep=line_feed_sep),
+                                                'diff': diff.render_diff(prev_fname, fname, True, line_feed_sep=line_feed_sep)
+                                            })
+
+                                            self.notification_q.put(n_object)
+
+                            except Exception as e:
+                                print("!!!! Exception in update_worker !!!\n", e)
+
+                self.current_uuid = None  # Done
+                self.q.task_done()
+
+            self.app.config.exit.wait(1)
