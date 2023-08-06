39fbdcfdfecbca79801daa1f549e777359ac04a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:39 +0000

    Add package NMisS/0.1.7/NMisS-0.1.7-py3-none-any.whl

---
+++ packages/NMisS/NMisS-0.1.7-py3-none-any.whl/NMisS/__init__.py
@@ -0,0 +1,48 @@
+from . import cases_1_3, cases_4_6, cases_7_8
+
+__doc__ = 'Сквозь кровь и пот родилась эта библиотека.' \
+          '\n[инсайд-запись работы нашей команды над проектом: https://www.youtube.com/watch?v=pTZaNHZGsQo]' \
+          '\n\nМодули в библиотеке:' \
+          '\n* approximation' \
+          '\n* equations' \
+          '\n* integ_diff' \
+          '\n* matrix' \
+          '\n* optimization' \
+          '\n* soLAE' \
+          '\n* spectral' \
+
+__all__ = ['__doc__']
+__all__ += ['__all__']
+__all__ = ['__doc__']
+__all__ += [
+    'simulated_annealing',
+    'temperatura',
+    'inlet',
+    'random_dist',
+    'matr',
+    'AntColony',
+    'graph_1',
+    'runoptimisationscript'
+]
+__all__ += [
+    'Crammer_method', 'Determinant', 'Lagrange',
+    'approximation_linear_func', 'approximation_quadratic_func',
+    'normal_distibution', 'Eulers_method', 'Eulers_Coshi_method',
+    'Runge_Kutta_method', 'Adams_method', 'Eulers_method_system',
+    'Eulers_Coshi_method_system', 'Runge_Kutta_method_system',
+    'make_plots', 'make_plots_wavelet', 'Read_csv_real',
+    'Frequency_spectrum', 'Wavelet_function', 'Approximation_Comparison',
+    'continuos_wavelet_three_dimensional'
+]
+__all__ += [
+    'f', 'fdx', 'F', 'integ', 'diff',
+    'Print_M', 'text_for_help', 'matrix_example',
+    'Gen_M', 'symbols_gen', 'beaty_output',
+    'CSV_files_A', 'Input_M', 'ways_of_inputing_matrix',
+    'Vector_maker', 'GJ', 'join2', 'GJ2', 'join',
+    'Yakob', 'det2', 'minor', 'determinant',
+    'Print_M', 'Command_List', 'Input_M_T',
+    'Input_M', 'Addiction', 'Substarction',
+    'Multiply_by_number', 'Multiply', 'Transpose'
+]
+
