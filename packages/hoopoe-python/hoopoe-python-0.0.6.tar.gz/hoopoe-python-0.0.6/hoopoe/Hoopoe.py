837d1b13e5825200494d3e2400de15b597b99c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:31 +0000

    Add package hoopoe-python/0.0.6/hoopoe-python-0.0.6.tar.gz

---
+++ packages/hoopoe-python/hoopoe-python-0.0.6.tar.gz/hoopoe-python-0.0.6/hoopoe/Hoopoe.py
@@ -0,0 +1,39 @@
+import requests
+
+import hoopoe.constants as constants
+from hoopoe.utils import handle_response
+
+class Hoopoe:
+    def __init__(self, api_key, version, base_url=constants.base_url):
+        if not api_key:
+            raise ValueError("api_key is required")
+        if version and not version:
+            raise ValueError("version is not valid")
+
+        self.api_key = api_key
+        self.base_url = base_url
+        self.version = version or constants.version
+        self.session = requests.Session()
+        self.session.headers.update(
+            {
+                "X-API-Key": "{}".format(self.api_key),
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            }
+        )
+
+    def timestamp(self):
+        url = f"{self.base_url}/v{self.version}/timestamp/"
+        response = self.session.get(url)
+
+        return handle_response(response)
+        
+    def upupa(self, message, extra={}):
+        url = f"{self.base_url}/v{self.version}/upupa/"
+        data = {
+            "message": message,
+            "extra": extra,
+        }
+        response = self.session.post(url, json=data)
+        
+        return handle_response(response)
