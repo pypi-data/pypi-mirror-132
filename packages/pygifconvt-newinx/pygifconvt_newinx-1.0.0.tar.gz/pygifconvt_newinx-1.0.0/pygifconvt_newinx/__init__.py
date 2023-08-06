57f566e3407ad323b4c3e6212ba2e4db3ffcf248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:52 +0000

    Add package pygifconvt-newinx/1.0.0/pygifconvt_newinx-1.0.0.tar.gz

---
+++ packages/pygifconvt-newinx/pygifconvt_newinx-1.0.0.tar.gz/pygifconvt_newinx-1.0.0/pygifconvt_newinx/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Pygifconvt: a very serious Python library for image to gif converter
+
+# TODO
+파이썬 기초에 나온단다.
+__init__ 파일이 없으면, 패키지로 인식하지 않음.
+
+- 설치 방법
+pip install pypi
+pip install github
+"""
+__title__ = "Pygifconvt"
+__author__ = "Eunki7"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2020 Eunki7"
