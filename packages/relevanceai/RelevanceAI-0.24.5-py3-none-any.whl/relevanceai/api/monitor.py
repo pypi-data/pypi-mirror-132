65da4eaa8a4914d0345b47583dc324f467222556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:55 +0000

    Add package relevanceai/0.24.5/RelevanceAI-0.24.5-py3-none-any.whl

---
+++ packages/relevanceai/RelevanceAI-0.24.5-py3-none-any.whl/relevanceai/api/monitor.py
@@ -0,0 +1,46 @@
+"""All Dataset related functions
+"""
+from relevanceai.base import Base
+
+
+class Monitor(Base):
+    def __init__(self, project, api_key, base_url):
+        self.project = project
+        self.api_key = api_key
+        self.base_url = base_url
+        super().__init__(project, api_key, base_url)
+
+    def health(
+        self, dataset_id: str, output_format: str = "json", verbose: bool = True
+    ):
+        """ 
+        Gives you a summary of the health of your vectors, e.g. how many documents with vectors are missing, how many documents with zero vectors 
+        
+        Parameters
+        ----------
+        dataset_id : string
+            Unique name of dataset
+        """
+        return self.make_http_request(
+            endpoint=f"datasets/{dataset_id}/monitor/health",
+            method="GET",
+            output_format=output_format,
+            verbose=verbose,
+        )
+
+
+    def stats(self, dataset_id: str, output_format: str = "json", verbose: bool = True):
+        """ 
+        All operations related to monitoring
+        
+        Parameters
+        ----------
+        dataset_id : string
+            Unique name of dataset
+        """
+        return self.make_http_request(
+            endpoint=f"datasets/{dataset_id}/monitor/stats",
+            method="GET",
+            output_format=output_format,
+            verbose=verbose,
+        )
