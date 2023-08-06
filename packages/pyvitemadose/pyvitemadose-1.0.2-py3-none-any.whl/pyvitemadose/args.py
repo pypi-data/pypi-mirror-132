d794d839dc47b66cd71d0b64ba9eb878d5ac7d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:12 +0000

    Add package pyvitemadose/1.0.2/pyvitemadose-1.0.2-py3-none-any.whl

---
+++ packages/pyvitemadose/pyvitemadose-1.0.2-py3-none-any.whl/pyvitemadose/args.py
@@ -0,0 +1,52 @@
+"""
+pygitscrum argparse gestion
+"""
+
+import argparse
+import sys
+
+
+def compute_args():
+    """
+    check args and return them
+    """
+    my_parser = argparse.ArgumentParser(
+        description="pyvitemadose displays available schedules of covid vaccine for your departement in France given in parameter",
+        epilog="""
+        Full documentation at: <https://github.com/thib1984/pyvitemadose>.
+        Report bugs to <https://github.com/thib1984/pyvitemadose/issues>.
+        MIT Licence.
+        Copyright (c) 2021 thib1984.
+        This is free software: you are free to change and redistribute it.
+        There is NO WARRANTY, to the extent permitted by law.
+        Written by thib1984.""",
+    )
+    my_parser.add_argument(
+        "departement",
+        metavar="departement",
+        type=str,
+        nargs="?",
+        help="numero departement",
+    )
+    my_parser.add_argument(
+        "-b",
+        "--before",
+        metavar="before",
+        type=str,
+        nargs="?",
+        help="before date yyyy-mm-dd",
+    )    
+    my_parser.add_argument(
+        "-u",
+        "--update",
+        action="store_true",
+        help="update pyvitemadose",
+    )
+
+    # if no parameter
+    if len(sys.argv) == 1:
+        my_parser.print_help()
+        sys.exit(0)
+
+    args = my_parser.parse_args()
+    return args
