23c4bac11ddbf7edf2f132633d767517aac85daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:51 +0000

    Add package flame-package-b/1.0.0/flame_package_b-1.0.0.tar.gz

---
+++ packages/flame-package-b/flame_package_b-1.0.0.tar.gz/flame_package_b-1.0.0/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import setup
+
+HERE=pathlib.Path(__file__).parent
+
+setup(
+    name ="flame_package_b",
+    version = "1.0.0",
+    author = "Urmila",
+    author_email = "urmilaraj18@gmail.com",
+    description = "testing package",
+    long_description ="README",
+    long_description_content_type = "text/markdown",
+    license="MIT",
+    classifiers = [
+       "License :: OSI Approved :: MIT License",
+       "Programming Language :: Python :: 3.6",
+       "Programming Language :: Python :: 3.7",
+       "Programming Language :: Python :: 3.8",
+    ],
+    packages=["package"],
+    install_requires=[],
+    include_package_data=True,
+)
+
+
