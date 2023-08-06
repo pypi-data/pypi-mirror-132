a54f69036a87e3e0afa03f64ea45be7118636e6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:41 +0000

    Add package lzy-MySpecifyWindow/0.1/lzy-MySpecifyWindow-0.1.tar.gz

---
+++ packages/lzy-MySpecifyWindow/lzy-MySpecifyWindow-0.1.tar.gz/lzy-MySpecifyWindow-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='lzy-MySpecifyWindow',
+      version='0.1',
+      author_email='mvli@qq.com',
+      py_modules=['MySpecifyWindow']
+      )
+
+# python setup.py sdist
+# twine upload dist/*
