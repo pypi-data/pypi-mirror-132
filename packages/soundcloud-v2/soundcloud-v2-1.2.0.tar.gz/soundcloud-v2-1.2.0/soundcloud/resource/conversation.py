335b1be84985719814e0216e7ee0831923877761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:53 +0000

    Add package soundcloud-v2/1.2.0/soundcloud-v2-1.2.0.tar.gz

---
+++ packages/soundcloud-v2/soundcloud-v2-1.2.0.tar.gz/soundcloud-v2-1.2.0/soundcloud/resource/conversation.py
@@ -0,0 +1,16 @@
+import datetime
+from dataclasses import dataclass
+from typing import List
+
+from soundcloud.resource.base import BaseData
+from soundcloud.resource.message import Message
+from soundcloud.resource.user import BasicUser
+
+@dataclass
+class Conversation(BaseData):
+    id: str
+    last_message: Message
+    read: bool
+    started_at: datetime.datetime
+    summary: str
+    users: List[BasicUser]
