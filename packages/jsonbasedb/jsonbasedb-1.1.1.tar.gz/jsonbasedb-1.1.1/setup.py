d1302592e90228e89e694016e46bc59c010b2b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:21 +0000

    Add package jsonbasedb/1.1.1/jsonbasedb-1.1.1.tar.gz

---
+++ packages/jsonbasedb/jsonbasedb-1.1.1.tar.gz/jsonbasedb-1.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='jsonbasedb',
+  version='1.1.1',
+  author='BlitzJB',
+  author_email='blitz04.dev@gmail.com',
+  description='A simple library for interacting with jsonbase',
+  long_description=open('README.md').read(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/BlitzJB/jsonbasedb',
+  packages=find_packages(),
+  install_requires=['requests'],
+  keywords=['python', 'json', 'database', 'jsonbase'],
+  classifiers=[
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python :: 3",
+    "Operating System :: Unix",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: Microsoft :: Windows",
+  ]
+)
