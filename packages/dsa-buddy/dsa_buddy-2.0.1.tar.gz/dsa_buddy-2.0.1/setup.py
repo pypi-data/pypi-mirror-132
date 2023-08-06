0e1d0260eca739cc3583f6672e8b3629d9b35ec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:48 +0000

    Add package dsa-buddy/2.0.1/dsa_buddy-2.0.1.tar.gz

---
+++ packages/dsa-buddy/dsa_buddy-2.0.1.tar.gz/dsa_buddy-2.0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+# reading long description from file
+long_description = "DSA buddy client\n@Copyright 2021-22 [Botoservices]"
+
+
+# specify requirements of your package here
+REQUIREMENTS = ['requests']
+
+# some more details
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    ]
+
+# calling the setup function 
+setup(name='dsa_buddy',
+      version='2.0.1',
+      description='dsa_buddy client for DSA preparation and code submission utlity',
+      long_description=long_description,
+      url='',
+      author='Botoservices',
+      author_email='botoservices@gmail.com',
+      license='MIT',
+      packages=['submit'],
+      entry_points ={
+            'console_scripts': [
+                'dsa_buddy = dsa_buddy.submit:main'
+            ]
+    },
+      classifiers=CLASSIFIERS,
+      install_requires=REQUIREMENTS,
+      keywords='dsa buddy coding python'
+      )
