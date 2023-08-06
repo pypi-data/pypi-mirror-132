0a8ad205d330af3a377fda4d1436301536f12019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:06 +0000

    Add package aomaker/1.1.3/aomaker-1.1.3.tar.gz

---
+++ packages/aomaker/aomaker-1.1.3.tar.gz/aomaker-1.1.3/aomaker/extension/recording/addons.py
@@ -0,0 +1,4 @@
+from aomaker.extension.recording.recording import Record
+
+addons=[Record('iotiot.yaml', filter_str='iot.staging.com | ~hq Path\:\/alarm_record',
+save_response=True, save_headers=False)]
