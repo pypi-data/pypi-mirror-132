759282b8eb8c57e564e1895b659d19d04ea073f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:33 +0000

    Add package attrdict2/0.0.1/attrdict2-0.0.1-py3-none-any.whl

---
+++ packages/attrdict2/attrdict2-0.0.1-py3-none-any.whl/attrdict2/__init__.py
@@ -0,0 +1,10 @@
+"""
+attrdict contains several mapping objects that allow access to their
+keys as attributes.
+"""
+from attrdict2.mapping import AttrMap
+from attrdict2.dictionary import AttrDict
+from attrdict2.default import AttrDefault
+
+
+__all__ = ['AttrMap', 'AttrDict', 'AttrDefault']
