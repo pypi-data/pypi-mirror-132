e7a8832d47463c406635e8f8a6bc93fa739530df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:47 +0000

    Add package portcheckerio/0.1.3/portcheckerio-0.1.3-py3-none-any.whl

---
+++ packages/portcheckerio/portcheckerio-0.1.3-py3-none-any.whl/portchecker/__init__.py
@@ -0,0 +1,7 @@
+"""
+Portchecker is an alternative way to retrieve the port status of a given hostname or IP address.
+If a hostname is provided, the IP address will be obtained and queried.
+This module works with both public IPv4 and IPv6 addresses (if IPv6 is available on the client).
+Private IP addresses are excluded from the checks.
+"""
+__version__ = '0.1.3'
