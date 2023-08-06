9d02287f9c6517d464702e327bc87b34421ed3cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:20 +0000

    Add package omspy/0.1.5/omspy-0.1.5.tar.gz

---
+++ packages/omspy/omspy-0.1.5.tar.gz/omspy-0.1.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['omspy', 'omspy.brokers', 'omspy.orders']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0', 'pendulum>=2.1.2,<3.0.0', 'pydantic>=1.8.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'omspy',
+    'version': '0.1.5',
+    'description': '',
+    'long_description': '# Omspy\n\nomspy is a broker agnostic order management system with a common api, advanced order types and more\n',
+    'author': 'Ubermensch',
+    'author_email': 'uberdeveloper001@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/uberdeveloper/omspy',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
