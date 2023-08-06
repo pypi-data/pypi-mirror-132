980997886ee8097fa96c2ae744a5a284492df5ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:13 +0000

    Add package timefrequency/0.0.0.dev0/timefrequency-0.0.0.dev0.tar.gz

---
+++ packages/timefrequency/timefrequency-0.0.0.dev0.tar.gz/timefrequency-0.0.0.dev0/README.md
@@ -0,0 +1,5 @@
+[![DOI](https://zenodo.org/badge/370470893.svg)](https://zenodo.org/badge/latestdoi/370470893)
+
+# timefrequency
+
+---
---
+++ packages/timefrequency/timefrequency-0.0.0.dev0.tar.gz/timefrequency-0.0.0.dev0/timefrequency/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+#
+# Author: Lars B. Rollik <L.B.Rollik@protonmail.com>
+
+import logging
+
+__version__ = "0.0.0.dev0"
+__author__ = "Lars B. Rollik"
