2e9f64e0f570dd7c26c1848c92d60c02831ffee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:35 +0000

    Add package distributions-vc623/0.1/distributions_vc623-0.1.tar.gz

---
+++ packages/distributions-vc623/distributions_vc623-0.1.tar.gz/distributions_vc623-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_vc623',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_vc623'],
+      author='Victor Chen',
+      author_email='victor.chen623@gmail.com',
+      zip_safe=False)
