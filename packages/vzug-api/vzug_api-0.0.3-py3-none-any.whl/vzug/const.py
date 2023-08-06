e0a826b99d147f449859ce06427f2ecd62fa851e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:42 +0000

    Add package vzug-api/0.0.3/vzug_api-0.0.3-py3-none-any.whl

---
+++ packages/vzug-api/vzug_api-0.0.3-py3-none-any.whl/vzug/const.py
@@ -0,0 +1,8 @@
+VERSION = '0.0.3'
+
+ENDPOINT_AI = 'ai'
+ENDPOINT_HH = 'hh'
+COMMAND_GET_STATUS = 'getDeviceStatus'
+COMMAND_GET_MODEL_DESC = 'getModelDescription'
+COMMAND_GET_PROGRAM = 'getProgram'
+COMMAND_GET_COMMAND = 'getCommand'
