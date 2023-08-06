f189691ae615839c4f3c8d2b17bc57d771303014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:55 +0000

    Add package quotesx/0.0.1/quotesx-0.0.1.tar.gz

---
+++ packages/quotesx/quotesx-0.0.1.tar.gz/quotesx-0.0.1/quotesx/__init__.py
@@ -0,0 +1,1 @@
+from quotesx import quotesx
---
+++ packages/quotesx/quotesx-0.0.1.tar.gz/quotesx-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A package to get quotes'
+LONG_DESCRIPTION = 'Get quotes with only one line of code!'
+
+# Setting up
+setup(
+    name="quotesx",
+    version=VERSION,
+    author="Mohammed Daniyal",
+    author_email="danides450@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['quote', 'simple', 'effiecient', 'easy'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
