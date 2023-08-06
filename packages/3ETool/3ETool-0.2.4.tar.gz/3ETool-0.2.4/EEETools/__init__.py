e897e5a95948fd87e5e852027e77f3888fab2a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:43 +0000

    Add package 3ETool/0.2.4/3ETool-0.2.4.tar.gz

---
+++ packages/3ETool/3ETool-0.2.4.tar.gz/3ETool-0.2.4/EEETools/__init__.py
@@ -0,0 +1,2 @@
+from EEETools.Tools.main_tools import paste_default_excel_file, paste_components_documentation, paste_user_manual
+from EEETools.Tools.main_tools import calculate, launch_connection_debug, launch_network_display
