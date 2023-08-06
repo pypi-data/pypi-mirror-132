4d11d2c34dfe513ef4e72a03f363cdce72b270ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:37 +0000

    Add package django-2fa/0.4.0/django-2fa-0.4.0.tar.gz

---
+++ packages/django-2fa/django-2fa-0.4.0.tar.gz/django-2fa-0.4.0/django_2fa/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from django_2fa.models import Device
+
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+  list_display = ('name', 'setup_complete', 'owner', 'created')
+  list_filter = ('setup_complete',)
+  date_hierarchy = 'created'
+  search_fields = ('name', 'owner__username', 'owner__email', 'owner__first_name', 'owner__last_name')
+
+  raw_id_fields = ('owner',)
+
+  fields = ('name', 'device_type', 'counter', 'counter_expire', 'setup_complete', 'owner')
