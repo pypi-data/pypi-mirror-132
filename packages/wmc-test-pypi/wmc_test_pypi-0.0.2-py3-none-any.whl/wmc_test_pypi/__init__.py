0a7a65f2881a81cef6c93e4b69a514edc22f2122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:14 +0000

    Add package wmc-test-pypi/0.0.2/wmc_test_pypi-0.0.2-py3-none-any.whl

---
+++ packages/wmc-test-pypi/wmc_test_pypi-0.0.2-py3-none-any.whl/wmc_test_pypi/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.0.2"
+
+from .hello import hello, hello_world
+
+__all__ = (
+    "__version__",
+    "hello",
+    "hello_world",
+)
---
+++ packages/wmc-test-pypi/wmc_test_pypi-0.0.2-py3-none-any.whl/wmc_test_pypi/hello.py
@@ -0,0 +1,6 @@
+def hello():
+    print("hello")
+
+
+def hello_world():
+    print("hello world")
