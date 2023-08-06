22cbd2a63a984d0435b73602de751345c9be04b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:36 +0000

    Add package apiaudio/0.12.2/apiaudio-0.12.2.tar.gz

---
+++ packages/apiaudio/apiaudio-0.12.2.tar.gz/apiaudio-0.12.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="apiaudio",
+    version="0.12.2",
+    author="salih",
+    author_email="salih@aflorithmic.ai",
+    description="Python SDK for api.audio API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aflorithmic/aflr_python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['requests >= 2.20; python_version >= "3.0"'],
+    python_requires=">=3.6",
+)
