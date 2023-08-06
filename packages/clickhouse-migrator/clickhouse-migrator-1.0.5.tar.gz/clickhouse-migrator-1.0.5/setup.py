dfbba39e6c2ac28b4e6a60fbe623a09f0d5685c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:34 +0000

    Add package clickhouse-migrator/1.0.5/clickhouse-migrator-1.0.5.tar.gz

---
+++ packages/clickhouse-migrator/clickhouse-migrator-1.0.5.tar.gz/clickhouse-migrator-1.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='clickhouse-migrator',
+      packages=find_packages(),
+      version='1.0.5',
+      description='Migration library for Clickhouse',
+      author='Delium Engineering',
+      install_requires=['pandas', 'clickhouse_driver'],
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      home_page='https://github.com/delium/clickhouse-migrator',
+      tests_require=['pytest==5.1.1'],
+      test_suite='tests',
+      license='MIT',
+      license_file='LICENSE',
+      python_requires='>=3.6')
