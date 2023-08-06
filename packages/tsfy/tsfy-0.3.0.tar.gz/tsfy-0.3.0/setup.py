975bf3c4edc1e4130e73b09a01117a3454617ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:34 +0000

    Add package tsfy/0.3.0/tsfy-0.3.0.tar.gz

---
+++ packages/tsfy/tsfy-0.3.0.tar.gz/tsfy-0.3.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tsfy']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'tsfy',
+    'version': '0.3.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Xiaocong Sun',
+    'author_email': 'xiaocong.sun@unianalysis.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
