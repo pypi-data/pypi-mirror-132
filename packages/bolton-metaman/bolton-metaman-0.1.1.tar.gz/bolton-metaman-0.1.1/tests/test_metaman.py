6d4eb8dd2a8fed27dcbe91dec83c3c225e8062c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:00 +0000

    Add package bolton-metaman/0.1.1/bolton-metaman-0.1.1.tar.gz

---
+++ packages/bolton-metaman/bolton-metaman-0.1.1.tar.gz/bolton-metaman-0.1.1/tests/test_metaman.py
@@ -0,0 +1,24 @@
+"""Tests for the metaman package."""
+
+from __future__ import annotations
+
+from pathlib import Path
+import sys
+
+from _pytest.monkeypatch import MonkeyPatch
+from pytest import mark
+
+from metaman import Inspector
+
+
+params = mark.parametrize
+
+
+def test_inspector__SYS_PATH_BUG(monkeypatch: MonkeyPatch) -> None:
+    """Regression test for bug in Inspector.
+
+    This bug occurs when sys.path contains Path objects instead of strings.
+    """
+    monkeypatch.setattr("sys.path", [Path(p) for p in sys.path])
+    inspector = Inspector()
+    assert hasattr(inspector, "module_name")
