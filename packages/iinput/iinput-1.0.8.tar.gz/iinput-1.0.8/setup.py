3d2160ad344cee280fffd75844003effbb8139a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:44 +0000

    Add package iinput/1.0.8/iinput-1.0.8.tar.gz

---
+++ packages/iinput/iinput-1.0.8.tar.gz/iinput-1.0.8/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'iinput',
+  packages = ['iinput'],
+  install_requires=[
+    'keyboard',
+  ],
+  version = '1.0.8',
+  license='MIT',
+  description = '',
+  author = 'Dante Biase',
+  url = 'https://github.com/dante-biase/iinput',
+  download_url = 'https://github.com/dante-biase/iinput/archive/v1.0.8.tar.gz',
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+  ],
+)
