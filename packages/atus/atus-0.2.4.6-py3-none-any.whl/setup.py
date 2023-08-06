cf195464d087d0d300a659bc7dc6f772913f8481
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:58 +0000

    Add package atus/0.2.4.6/atus-0.2.4.6-py3-none-any.whl

---
+++ packages/atus/atus-0.2.4.6-py3-none-any.whl/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="atus",
+    version="0.2.4.6",
+    author="High Elo Devs",
+    author_email="atusdevs@gmail.com",
+    description="Tool for analysis",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/HighEloDevs/Analysis-Tool-for-Undergrad-Students",
+    project_urls={
+        "Bug Tracker": "https://github.com/HighEloDevs/Analysis-Tool-for-Undergrad-Students/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages_dir={'src':'src'},
+    packages=["src", ""],
+    include_package_data=True,
+    python_requires=">=3.7",
+    entry_points={
+        'console_scripts': ['atus = main:main'],
+        'gui_scripts': ['atus = main:main']
+    },
+    install_requires=[
+        "setuptools>=42",
+        "wheel",
+        "matplotlib==3.4.2",
+        "pandas",
+        "numpy",
+        "scipy",
+        "lmfit",
+        "matplotlib_backend_qtquick",
+        "pyqt5",
+        "requests",
+    ]
+)   
