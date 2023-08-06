00df0f9bb6d422eed7d0de2fa988e19c4756da02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:19 +0000

    Add package vurf/0.10.0a2/vurf-0.10.0a2.tar.gz

---
+++ packages/vurf/vurf-0.10.0a2.tar.gz/vurf-0.10.0a2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['vurf', 'vurf.parser']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.0.0,<8.1.0', 'tomli>=2.0.0,<2.1.0']
+
+entry_points = \
+{'console_scripts': ['vurf = vurf.cli:main']}
+
+setup_kwargs = {
+    'name': 'vurf',
+    'version': '0.10.0a2',
+    'description': "Viliam's Universal Requirements Format",
+    'long_description': '# VURF\n\n> Viliam\'s Universal Requirements Format\n\n## What it is\n*VURF* is a format, parser, and CLI for saving packages into Python-ish looking file.\n\n### Example packages.vurf\n```python\nwith pip:\n  vurf\n  black\n  if at_work:\n    ql-cq\n    ql-orange\nwith brew:\n  nnn  # terminal file manager\n```\n\n### Usage\n!TODO\n\n## Grammar\n*VURF* has [grammar](./vurf/grammar.lark) and LALR(1) parser implemented in [Lark](https://github.com/lark-parser/lark).\nIt aims to look like Python code as much as possible.\n\n### Keywords\n* `with [section]` - specifies "section" of requirements file. Different sections usually have different installers.\n* `if [condition]:` - conditions for including packages. See [Conditions](##Conditions) sections.\n* `elif [condition]:`\n* `else:`\n* `...` - ellipsis - placeholder for empty section.\n\n### Packages\n* are saved as `[name]  # [comment]`\n* `name` can be almost any valid package name (cannot start with "." or contain tabs or newline characters)\n* names containing spaces must be quoted. E.g. `\'multi word package name\'`\n* comments are optional\n\n## Config\n\nNote: *VURF* will automatically create config file on the first run.\n\n### Config file format\n```toml\n# Where packages file is saved\npackages_location = "/Users/viliam/packages.vurf"\n# Name of the default section\ndefault_section = "brew"\n\n# Sections can be though of as installers for different packages\n# Value of the section is the command for installing packages with `vurf install`\n[sections]\nbrew = "brew install"\ncask = "brew install --cask"\npython = "pip install --quiet --user"\n\n# Parameters are constants that can be accessed from conditionals\n[parameters]\nhostname = "mac"\nprimary_computer = true\nfs = "apfs"\n```\n\n## CLI\n```\n$ vurf\nUsage: vurf [OPTIONS] COMMAND [ARGS]...\n\nOptions:\n  -q, --quiet  Don\'t produce unnecessary output.\n  --version    Show the version and exit.\n  --help       Show this message and exit.\n\nCommands:\n  add       Add package(s).\n  config    Edit config file.\n  default   Print default section.\n  edit      Edit packages file.\n  format    Format packages file.\n  has       Exit with indication if package is in packages.\n  install   Install packages.\n  packages  Print list of packages.\n  print     Print contents of packages file.\n  remove    Remove package(s).\n  sections  Print list of sections.\n```\n\n## Conditions\n!TODO\n\n## Hooks\n!TODO\n',
+    'author': 'Viliam Valent',
+    'author_email': 'vurf@valent.email',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/ViliamV/vurf',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/vurf/vurf-0.10.0a2.tar.gz/vurf-0.10.0a2/vurf/cli.py
@@ -0,0 +1,196 @@
+from functools import wraps
+import sys
+from typing import Callable, Iterable, Optional
+from types import SimpleNamespace
+import click
+from vurf.parser import parse
+from pathlib import Path
+import tomli
+from shutil import copyfile
+
+
+APP_NAME = "VURF"
+SECTION_ENV = f"{APP_NAME}_SECTION"
+CONFIG_NAME = "config.toml"
+PACKAGES_NAME = "packages.vurf"
+
+
+section_option = click.option(
+    "-s",
+    "--section",
+    required=False,
+    envvar=SECTION_ENV,
+    help=f"Defaults to `default_section` from config. Reads {SECTION_ENV} env variable.",
+)
+
+
+def ensure_config(quiet):
+    config_path = Path(click.get_app_dir(APP_NAME))
+    if not config_path.exists():
+        config_path.mkdir(parents=True)
+    config_file = config_path / CONFIG_NAME
+    default_packages_file = Path.home() / PACKAGES_NAME
+    if not config_file.is_file():
+        if not quiet:
+            click.secho("Config file not found...", fg="bright_black")
+            click.secho(f"Creating default config {config_file}", fg="bright_black")
+        copyfile(Path(__file__).parent / "config.toml", config_file)
+    with config_file.open('rb') as opened:
+        config = tomli.load(opened)
+    if not Path(config["packages_location"]).is_file():
+        if not quiet:
+            click.secho("Packages file not found...", fg="bright_black")
+            click.secho(f"Creating default {default_packages_file}", fg="bright_black")
+            click.secho("You can change it later in the config.", fg="bright_black")
+        copyfile(Path(__file__).parent / "default.vurf", default_packages_file)
+    return config
+
+
+def write_packages(ctx):
+    with open(ctx.obj.config["packages_location"], "w") as f:
+        f.write(ctx.obj.root.to_string())
+
+
+def no_traceback(f: Callable) -> Callable:
+    @wraps(f)
+    def wrapper(*args, **kwds):
+        try:
+            return f(*args, **kwds)
+        except Exception as e:
+            sys.stderr.write(" ".join(e.args) + "\n")
+            sys.exit(1)
+
+    return wrapper
+
+
+@click.group()
+@click.pass_context
+@click.option("-q", "--quiet", is_flag=True, help="Don't produce unnecessary output.")
+@click.version_option(package_name=APP_NAME.lower())
+@no_traceback
+def main(ctx, quiet):
+    config = ensure_config(quiet)
+    with open(Path(config["packages_location"])) as f:
+        root = parse(f)
+    ctx.obj = ctx.ensure_object(SimpleNamespace)
+    ctx.obj.config = config
+    ctx.obj.root = root
+    ctx.obj.quiet = quiet
+
+
+@main.command(help="Add package(s).")
+@section_option
+@click.argument("packages", nargs=-1)
+@click.pass_context
+@no_traceback
+def add(ctx, section: Optional[str], packages: Iterable[str]):
+    if section is None:
+        section = ctx.obj.config["default_section"]
+    for package in packages:
+        ctx.obj.root.add_package(section, package)
+    write_packages(ctx)
+
+
+@main.command(help="Remove package(s).")
+@section_option
+@click.argument("packages", nargs=-1)
+@click.pass_context
+@no_traceback
+def remove(ctx, section: Optional[str], packages: Iterable[str]):
+    if section is None:
+        section = ctx.obj.config["default_section"]
+    for package in packages:
+        ctx.obj.root.remove_package(section, package)
+    write_packages(ctx)
+
+
+@main.command(help="Print default section.")
+@click.pass_context
+@no_traceback
+def default(ctx):
+    click.echo(ctx.obj.config["default_section"])
+
+
+@main.command(help="Exit with indication if package is in packages.")
+@section_option
+@click.argument("package")
+@click.pass_context
+@no_traceback
+def has(ctx, section: Optional[str], package: str):
+    if section is None:
+        section = ctx.obj.config["default_section"]
+    ctx.exit(int(not ctx.obj.root.has_package(section, package)))
+
+
+@main.command(help="Print list of packages.")
+@section_option
+@click.option(
+    "--separator",
+    required=False,
+    type=str,
+    default="\n",
+    help='Section separator. Defaults to "\\n".',
+)
+@click.pass_context
+@no_traceback
+def packages(ctx, section, separator):
+    click.echo(
+        ctx.obj.root.get_packages(section, ctx.obj.config["parameters"], separator)
+    )
+
+
+@main.command(help="Print list of sections.")
+@click.option(
+    "--separator",
+    required=False,
+    type=str,
+    default="\n",
+    help='Section separator. Defaults to "\\n".',
+)
+@click.pass_context
+@no_traceback
+def sections(ctx, separator):
+    click.echo(ctx.obj.root.get_sections(separator))
+
+
+@main.command(help="Install packages.")
+@section_option
+@click.pass_context
+@no_traceback
+def install(ctx, section):
+    click.echo(
+        ctx.obj.root.install(
+            section, ctx.obj.config["sections"], ctx.obj.config["parameters"]
+        )
+    )
+
+
+@main.command(name="print", help="Print contents of packages file.")
+@click.pass_context
+@no_traceback
+def print_(ctx):
+    click.echo_via_pager(ctx.obj.root.to_string())
+
+
+@main.command(help="Format packages file.")
+@click.pass_context
+@no_traceback
+def format(ctx):
+    write_packages(ctx)
+
+
+@main.command(help="Edit packages file.")
+@click.pass_context
+@no_traceback
+def edit(ctx):
+    click.edit(filename=ctx.obj.config["packages_location"])
+
+
+@main.command(help="Edit config file.")
+@no_traceback
+def config():
+    click.edit(filename=f"{click.get_app_dir(APP_NAME)}/{CONFIG_NAME}")
+
+
+if __name__ == "__main__":
+    main()
