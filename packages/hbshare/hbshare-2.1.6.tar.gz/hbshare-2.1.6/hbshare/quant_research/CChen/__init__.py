07c6f7d118c5b016d834cce8aa18713cf4b509f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:06 +0000

    Add package hbshare/2.1.6/hbshare-2.1.6.tar.gz

---
+++ packages/hbshare/hbshare-2.1.6.tar.gz/hbshare-2.1.6/hbshare/quant_research/CChen/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#coding:utf-8
+
+"""
+@author: Meng.lv
+@contact: meng.lv@howbuy.com
+@software: PyCharm
+@file: __init__.py.py
+@time: 2020/4/22 13:39
+"""
+
+
+from hbshare.quant_research.CChen.load_data import *
+from hbshare.quant_research.CChen.fund_perf import *
+# from hbshare.CChen.output import *
+from hbshare.quant_research.CChen.fut import *
