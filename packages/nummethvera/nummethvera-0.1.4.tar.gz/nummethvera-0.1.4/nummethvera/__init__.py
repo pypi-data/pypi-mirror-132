61cd0dd66c3b9f112cfe6f90f0f6e590d55092b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:58 +0000

    Add package nummethvera/0.1.4/nummethvera-0.1.4.tar.gz

---
+++ packages/nummethvera/nummethvera-0.1.4.tar.gz/nummethvera-0.1.4/nummethvera/__init__.py
@@ -0,0 +1,13 @@
+from . import approxinterp
+from . import diffequat
+from . import diffintegr
+from . import matrixop
+from . import otjigants
+from . import slau
+from . import veivletfurie
+
+__version__ = '0.1.4'
+__all__ = """
+    approxinterp.read_csv
+    approxinterp.polinom_chebyshev
+    """
