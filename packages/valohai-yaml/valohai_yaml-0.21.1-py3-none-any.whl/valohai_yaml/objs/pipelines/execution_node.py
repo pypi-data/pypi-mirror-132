21ac370e4471920158d7ea9b612d3cea033e6916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:07 +0000

    Add package valohai-yaml/0.21.1/valohai_yaml-0.21.1-py3-none-any.whl

---
+++ packages/valohai-yaml/valohai_yaml-0.21.1-py3-none-any.whl/valohai_yaml/objs/pipelines/execution_node.py
@@ -0,0 +1,33 @@
+from typing import List, Optional
+
+from ...lint import LintResult
+from ...types import LintContext, NodeOverrideDict
+from .node import Node
+from .node_action import NodeAction
+
+
+class ExecutionNode(Node):
+    """Represents an execution node within a pipeline definition."""
+
+    type = 'execution'
+
+    def __init__(
+        self,
+        *,
+        name: str,
+        step: str,
+        actions: Optional[List[NodeAction]] = None,
+        override: Optional[NodeOverrideDict] = None
+    ) -> None:
+        super().__init__(name=name, actions=actions)
+        if override is None:
+            override = {}
+        self.step = step
+        self.override = override
+
+    def lint(self, lint_result: LintResult, context: LintContext) -> None:
+        super().lint(lint_result, context)
+        config = context['config']
+        pipeline = context['pipeline']
+        if self.step not in config.steps:
+            lint_result.add_error(f'Pipeline {pipeline.name} node {self.name} step {self.step} does not exist')
