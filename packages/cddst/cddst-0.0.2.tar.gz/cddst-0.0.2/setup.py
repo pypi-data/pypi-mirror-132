1a74b32b8d6b26ceecaf45904ae481f2aca66cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:18 +0000

    Add package cddst/0.0.2/cddst-0.0.2.tar.gz

---
+++ packages/cddst/cddst-0.0.2.tar.gz/cddst-0.0.2/setup.py
@@ -0,0 +1,21 @@
+# #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time : 2021/12/29 11:20
+# @Author : BruceLong
+# @FileName: setup.py
+# @Email   : 18656170559@163.com
+# @Software: PyCharm
+# @Blog ：http://www.cnblogs.com/yunlongaimeng/
+from setuptools import setup, find_packages
+
+setup(
+    name="cddst",
+    version="0.0.2",
+    packages=find_packages(),
+    author="BruceLong",
+    license="MIT Licence",
+    author_email="18656170559@163.com",
+    include_package_data=True,
+    platforms="any",
+    install_requires=['PyMySQL>=1.0.2']
+)
