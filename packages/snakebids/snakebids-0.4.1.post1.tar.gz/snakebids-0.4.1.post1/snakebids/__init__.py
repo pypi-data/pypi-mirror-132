531149a25ddd64c4347239cdab8eabbe8113881e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:57 +0000

    Add package snakebids/0.4.1.post1/snakebids-0.4.1.post1.tar.gz

---
+++ packages/snakebids/snakebids-0.4.1.post1.tar.gz/snakebids-0.4.1.post1/snakebids/__init__.py
@@ -0,0 +1,24 @@
+__submodules__ = ["bids"]
+
+# <AUTOGEN_INIT>
+from snakebids.bids import (
+    bids,
+    filter_list,
+    generate_inputs,
+    get_filtered_ziplist_index,
+    get_wildcard_constraints,
+    print_boilerplate,
+    write_derivative_json,
+)
+
+__all__ = [
+    "bids",
+    "filter_list",
+    "generate_inputs",
+    "get_filtered_ziplist_index",
+    "get_wildcard_constraints",
+    "print_boilerplate",
+    "write_derivative_json",
+]
+
+# </AUTOGEN_INIT>
