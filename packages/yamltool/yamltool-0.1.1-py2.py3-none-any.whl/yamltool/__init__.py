d48db3fe47c4a1d562072ff9902feb859f03a852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:29 +0000

    Add package yamltool/0.1.1/yamltool-0.1.1-py2.py3-none-any.whl

---
+++ packages/yamltool/yamltool-0.1.1-py2.py3-none-any.whl/yamltool/__init__.py
@@ -0,0 +1,9 @@
+"""YAML tool, a clone of the json.tool Python module for YAML.
+
+This tool provides a simple command line interface to validate and pretty-print
+YAML documents while trying to preserve as much as possible from the original
+documents (like comments and anchors).
+"""
+
+
+__version__ = "0.1.1"
