e5e80ffb3491a68e35a95df360992a44241fb155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:07 +0000

    Add package m-pytest-core/1.0.3/m_pytest_core-1.0.3.tar.gz

---
+++ packages/m-pytest-core/m_pytest_core-1.0.3.tar.gz/m_pytest_core-1.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='m_pytest_core',
+    version='1.0.3',
+    packages=['m_pytest_core', 'm_pytest_core.core', 'm_pytest_core.core.db', 'm_pytest_core.core.utils',
+              'm_pytest_core.files'],
+    url='https://github.com/AlekseiMiasnikov/pytest_core',
+    license='Free',
+    author='myasnikov_am',
+    author_email='lexalightning@gmail.com',
+    description='The framework core from pytest.'
+)
