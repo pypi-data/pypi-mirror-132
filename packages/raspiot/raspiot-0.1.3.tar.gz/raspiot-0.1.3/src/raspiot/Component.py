885e3295a1ede940b2e09f09db78a7f820b9316f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:46 +0000

    Add package raspiot/0.1.3/raspiot-0.1.3.tar.gz

---
+++ packages/raspiot/raspiot-0.1.3.tar.gz/raspiot-0.1.3/src/raspiot/Component.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+from threading import Thread
+
+class Component(ABC, Thread):
+    @abstractmethod
+    def configure(self, config):
+        return NotImplemented
+
+    @abstractmethod
+    def run(self):
+        return NotImplemented
+
+    def __del__(self):
+        self.terminate()
+
+class Generator(Component):
+    def __init__(self):
+        super().__init__()
+        self.configure()
+
+    def run(self):
+        self.send()
+
+    @abstractmethod
+    def send(self):
+        return NotImplemented
+
+class Reactor(Component):
+    def __init__(self):
+        super().__init__()
+        self.configure()
+
+    def run(self):
+        self.react()
+
+    @abstractmethod
+    def react(self):
+        return NotImplemented
+
+
+__all__ = ['Component', 'Generator', 'Reactor']
