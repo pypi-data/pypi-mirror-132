29bcf0e0696db994e78e01a841e690ceeb4e73ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:55 +0000

    Add package redgifs/0.0.0/redgifs-0.0.0.tar.gz

---
+++ packages/redgifs/redgifs-0.0.0.tar.gz/redgifs-0.0.0/setup.py
@@ -0,0 +1,14 @@
+import re
+from setuptools import setup
+
+setup(
+    name='redgifs',
+    author='scrazzz',
+    url='https://github.com/scrazzz/',
+    version='0.0.0',
+    license='MIT',
+    description='',
+    long_description='',
+    long_description_content_type='text/markdown',
+    python_requires='>=3.7.0'
+)
