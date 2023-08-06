de9b259bbfb9724febd0cec8d9017b9414704e69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:00 +0000

    Add package salure-tfx-extensions/0.2.5/salure_tfx_extensions-0.2.5.tar.gz

---
+++ packages/salure-tfx-extensions/salure_tfx_extensions-0.2.5.tar.gz/salure_tfx_extensions-0.2.5/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+# IMPORTANT: ALSO UPDATE IN 'VERSION' FILE FOR CI DOCKER BUILD
+VERSION_MAJOR = '0'
+VERSION_MINOR = '2'
+VERSION_PATCH = '5'
+
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='salure_tfx_extensions',
+    version='{}.{}.{}'.format(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH),
+    python_requires='>=3.6',
+    description='TFX components, helper functions and pipeline definition, developed by Salure',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Salure',
+    author_email='bi@salure.nl',
+    license='Salure License',
+    packages=find_packages(),
+    package_data={'salure_tfx_extensions': ['proto/*.proto']},
+    install_requires=[
+        'beam-nuggets>=0.15.1,<0.16',
+        'tfx>=0.27.0,<0.28.0',
+        'tensorflow>=2.6.0',
+        'pandas>=0.25,<2',
+        'PyMySQL>=1,<2',
+        'salure-helpers==15.9.0',
+        'kfp>=0.4,<0.5'
+    ],
+    zip_safe=False
+)
