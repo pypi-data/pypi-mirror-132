c39924df6e36360dbf2a0ae700b0fe6a24e06ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:09 +0000

    Add package nkdcv-python/1.1.2/nkdcv-python-1.1.2.tar.gz

---
+++ packages/nkdcv-python/nkdcv-python-1.1.2.tar.gz/nkdcv-python-1.1.2/nkdcv/__init__.py
@@ -0,0 +1,8 @@
+from nkdcv.HandTrackingModule import HandDetector
+from nkdcv.FaceMeshModule import FaceMeshDetector
+from nkdcv.FaceDetectionModule import FaceDetector
+from nkdcv.PoseModule import PoseDetector
+from nkdcv.Utils import stackImages, cornerRect
+from nkdcv.FPS import FPS
+from nkdcv.SerialModule import SerialObject
+from nkdcv.PIDModule import PID
