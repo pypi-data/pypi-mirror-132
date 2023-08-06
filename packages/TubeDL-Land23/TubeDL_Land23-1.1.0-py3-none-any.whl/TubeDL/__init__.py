f76e52d576f5cbabb03b2343e13dfde2f791ee74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:30 +0000

    Add package TubeDL-Land23/1.1.0/TubeDL_Land23-1.1.0-py3-none-any.whl

---
+++ packages/TubeDL-Land23/TubeDL_Land23-1.1.0-py3-none-any.whl/TubeDL/__init__.py
@@ -0,0 +1,14 @@
+from TubeDL import tube
+import sys
+def Setup():
+    args = []
+    #if (sys.argv[0] == os.path.basename(__file__)):
+    #    args = sys.argv[1:]
+    #else:
+    #    args = sys.argv
+    args = sys.argv[1:]
+    if (len(args) == 0):
+        tube.ShowHelp()
+        tube.Close()
+
+    tube.Main(args)
