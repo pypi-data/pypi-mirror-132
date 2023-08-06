18e9b55ae73b97fe6f98a5aba89b77ac079feca9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:22 +0000

    Add package svo-slicer/1.0.2/svo-slicer-1.0.2.tar.gz

---
+++ packages/svo-slicer/svo-slicer-1.0.2.tar.gz/svo-slicer-1.0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='svo-slicer',
+    version='1.0.2',
+    scripts=['svo_slicer.py', 'utils.py'],
+    description="a package to slice .svo files into a deliverable compressed folder"
+)
