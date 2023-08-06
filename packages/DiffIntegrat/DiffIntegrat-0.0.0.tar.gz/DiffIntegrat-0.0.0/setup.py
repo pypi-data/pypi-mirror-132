53f998794f7e443743a972829fc1f7360acad603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:23 +0000

    Add package DiffIntegrat/0.0.0/DiffIntegrat-0.0.0.tar.gz

---
+++ packages/DiffIntegrat/DiffIntegrat-0.0.0.tar.gz/DiffIntegrat-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='DiffIntegrat',
+      version='0.0.0',
+      description='Differentiation and Integration',
+      packages=['DiffIntegrat'],
+      install_requires=['matplotlib', 'prettytable'],
+      author_email='slava.esakov@mail.ru',
+      license='MIT',
+      zip_safe=False)
