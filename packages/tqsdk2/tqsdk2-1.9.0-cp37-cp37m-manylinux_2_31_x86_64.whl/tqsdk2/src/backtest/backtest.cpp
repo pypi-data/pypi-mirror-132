e48a6ff320e209f1e739783db6ffc29990d599f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:54 +0000

    Add package tqsdk2/1.9.0/tqsdk2-1.9.0-cp37-cp37m-manylinux_2_31_x86_64.whl

---
+++ packages/tqsdk2/tqsdk2-1.9.0-cp37-cp37m-manylinux_2_31_x86_64.whl/tqsdk2/src/backtest/backtest.cpp
@@ -0,0 +1,14 @@
+/*******************************************************************************
+ * @file backtest.cpp
+ * @brief TqSDK2 回测类
+ * @copyright 上海信易信息科技股份有限公司 版权所有
+ ******************************************************************************/
+#include "backtest.h"
+
+BackTest::BackTest(timestamp start_dt, timestamp end_dt) {
+  m_service = md::BackTestService::Create(start_dt.time_since_epoch().count(), end_dt.time_since_epoch().count());
+}
+
+std::shared_ptr<BackTestService> BackTest::GetService() {
+  return m_service;
+}
