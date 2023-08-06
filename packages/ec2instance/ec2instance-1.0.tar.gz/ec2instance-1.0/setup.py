ceee9db7f986b119965d09efdc50ada446253f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:49 +0000

    Add package ec2instance/1.0/ec2instance-1.0.tar.gz

---
+++ packages/ec2instance/ec2instance-1.0.tar.gz/ec2instance-1.0/setup.py
@@ -0,0 +1,36 @@
+import io
+import os
+
+from setuptools import find_packages, setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = '\n' + f.read()
+
+setup(
+    name='ec2instance',
+    version='1.0',
+    description="Quickly launch an EC2 instance for small tasks. The instance's lifecycle is tied to this process,",
+    keywords='ec2instance',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author="John Miller",
+    author_email='john@johngm.com',
+    python_requires='>=3.7.0',
+    url='https://github.com/personalcomputer/ec2instance',
+    packages=find_packages(include=['ec2instance', 'ec2instance.*']),
+    install_requires=[
+        'boto3',
+        'iso8601',
+        'pycryptodomex',
+    ],
+    include_package_data=True,
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    zip_safe=False
+)
