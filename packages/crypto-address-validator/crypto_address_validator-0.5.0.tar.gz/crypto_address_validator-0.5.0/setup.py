765e85123f30dcdd9701690d42a8c9b713d41c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:39 +0000

    Add package crypto-address-validator/0.5.0/crypto_address_validator-0.5.0.tar.gz

---
+++ packages/crypto-address-validator/crypto_address_validator-0.5.0.tar.gz/crypto_address_validator-0.5.0/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['crypto_address_validator',
+ 'crypto_address_validator.validators',
+ 'crypto_address_validator.validators.crypto']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['base58>=2.1.1,<3.0.0', 'bech32>=1.2.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'crypto-address-validator',
+    'version': '0.5.0',
+    'description': 'Simple validation tool for Bitcoin and other altcoin addresses.',
+    'long_description': None,
+    'author': 'null-po1nter',
+    'author_email': 'me.nullptr@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
