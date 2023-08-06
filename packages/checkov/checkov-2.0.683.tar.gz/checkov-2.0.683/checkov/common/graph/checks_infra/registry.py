11cfaff45d4e3687c05a2030a9b7cd27caf0af04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:51 +0000

    Add package checkov/2.0.683/checkov-2.0.683.tar.gz

---
+++ packages/checkov/checkov-2.0.683.tar.gz/checkov-2.0.683/checkov/common/graph/checks_infra/registry.py
@@ -0,0 +1,39 @@
+import logging
+from typing import List, Dict, Any
+
+from networkx import DiGraph
+
+from checkov.common.graph.checks_infra.base_check import BaseGraphCheck
+from checkov.common.graph.checks_infra.base_parser import BaseGraphCheckParser
+from checkov.common.models.enums import CheckResult
+from checkov.runner_filter import RunnerFilter
+
+
+class BaseRegistry:
+    def __init__(self, parser: BaseGraphCheckParser) -> None:
+        self.checks: List[BaseGraphCheck] = []
+        self.parser = parser
+
+    def load_checks(self):
+        raise NotImplementedError
+
+    def run_checks(
+        self, graph_connector: DiGraph, runner_filter: RunnerFilter
+    ) -> Dict[BaseGraphCheck, List[Dict[str, Any]]]:
+        check_results = {}
+        for check in self.checks:
+            if not runner_filter.should_run_check(check.id, check.bc_id):
+                continue
+            logging.debug(f'Running graph check: {check.id}')
+            passed, failed = check.run(graph_connector)
+            evaluated_keys = check.get_evaluated_keys()
+            check_result = self._process_check_result(passed, [], CheckResult.PASSED, evaluated_keys)
+            check_result = self._process_check_result(failed, check_result, CheckResult.FAILED, evaluated_keys)
+            check_results[check] = check_result
+        return check_results
+
+    @staticmethod
+    def _process_check_result(results, processed_results, result, evaluated_keys) -> List[Dict[str, Any]]:
+        for vertex in results:
+            processed_results.append({"result": result, "entity": vertex, "evaluated_keys": evaluated_keys})
+        return processed_results
