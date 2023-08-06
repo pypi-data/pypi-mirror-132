65a2d5dde83de35ef27b50f5f66e01ea69b7043a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:50 +0000

    Add package simple-toolbelt/0.0.1/simple_toolbelt-0.0.1.tar.gz

---
+++ packages/simple-toolbelt/simple_toolbelt-0.0.1.tar.gz/simple_toolbelt-0.0.1/README.md
@@ -0,0 +1,26 @@
+# python-simple-toolbelt
+
+A growing collection of simple utilities functions for Python
+
+## Installation
+
+```bash
+pip install simple_toolbelt
+```
+
+## Functions
+
+- [path.ensure_dir](#pathensure_dir)
+
+___
+
+#### path.ensure_dir
+
+Create the directories, include subs, if don't exist.
+
+```python
+from simple_toolbath.path import ensure_dir
+
+with open(ensure_dir('this/path/to/file.txt'), 'w') as f:
+    ...
+```
---
+++ packages/simple-toolbelt/simple_toolbelt-0.0.1.tar.gz/simple_toolbelt-0.0.1/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+_name = 'simple_toolbelt'
+_desc = 'A growing collection of simple utilities functions for Python'
+_url = 'https://github.com/mbiemann/python-simple-toolbelt'
+_reqs = []
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+long_description = str(long_description) + '\n___\n' + 'Check on GitHub: ' + '\n' + _url
+
+setuptools.setup(
+    name=_name,
+    version='0.0.1',
+    author='Marcell Biemann',
+    author_email='mbiemann@gmail.com',
+    description=_desc,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=_url,
+    project_urls={
+        'Bug Tracker': _url + '/issues',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    packages=[_name],
+    python_requires='>=3.6',
+    install_requires=_reqs
+)
