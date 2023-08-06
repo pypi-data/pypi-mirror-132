d475152463a0a7574244e2b80fd456936a6ae983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:25 +0000

    Add package openssl-ocsp-responder/0.9.9/openssl_ocsp_responder-0.9.9.tar.gz

---
+++ packages/openssl-ocsp-responder/openssl_ocsp_responder-0.9.9.tar.gz/openssl_ocsp_responder-0.9.9/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+  name = 'openssl_ocsp_responder',
+  packages = ['openssl_ocsp_responder'],
+  version = '0.9.9',
+  license='MIT',
+  description = 'Simple wrapper for OpenSSL OCSP server',
+  author = 'Eyal Brami',
+  author_email = 'eyal.brami@redis.com',
+  url = 'https://github.com/eyalbrami1/openssl_ocsp_responder',
+  download_url = 'https://github.com/eyalbrami1/openssl_ocsp_responder/archive/refs/tags/v0.9.9.tar.gz',
+  keywords = ['ocsp', 'ssl', 'openssl'],
+  install_requires=[
+          'pyopenssl',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 2.7',
+  ],
+)
