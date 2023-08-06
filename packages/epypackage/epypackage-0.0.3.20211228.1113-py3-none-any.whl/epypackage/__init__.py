2f905a514b4e4b0babadb9311ad2f9425e89160c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:22 +0000

    Add package epypackage/0.0.3.20211228.1113/epypackage-0.0.3.20211228.1113-py3-none-any.whl

---
+++ packages/epypackage/epypackage-0.0.3.20211228.1113-py3-none-any.whl/epypackage/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time : 2021-12-28 18:14
+# @Author : LiGuangLong
+# @File : __init__.py.py
+# @Software: PyCharm
+
+# ========== 核心支持库命令 ==========
+from epypackage.redise import *  # 算数运算
+
+
+
+
+__version__ = '0.0.3'
