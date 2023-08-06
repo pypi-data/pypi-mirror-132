59e183ff90c1fd4eed0690422ef6e2d2792d2c33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:08 +0000

    Add package bric-analysis-libraries/0.0.12/bric-analysis-libraries-0.0.12.tar.gz

---
+++ packages/bric-analysis-libraries/bric-analysis-libraries-0.0.12.tar.gz/bric-analysis-libraries-0.0.12/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open( 'README.md', 'r' ) as f:
+    long_desc = f.read()
+
+setuptools.setup(
+    name='bric-analysis-libraries',
+    version = '0.0.12',
+    author='Brian Carlsen',
+    author_email = 'carlsen.bri@gmail.com',
+    description = 'An assortment of analysis libraries.',
+    long_description = long_desc,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/bicarlsen/analysis-libraries.git',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'scipy',
+        'h5py'
+    ],
+    package_data = {
+        'pl': [ 'data/*' ]
+    },
+
+)
