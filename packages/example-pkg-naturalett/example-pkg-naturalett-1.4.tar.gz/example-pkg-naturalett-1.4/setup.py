a431a79908f9f86238c81876328c0b098c72c097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:38 +0000

    Add package example-pkg-naturalett/1.4/example-pkg-naturalett-1.4.tar.gz

---
+++ packages/example-pkg-naturalett/example-pkg-naturalett-1.4.tar.gz/example-pkg-naturalett-1.4/setup.py
@@ -0,0 +1,14 @@
+import setuptools, os
+
+setuptools.setup(
+    name="example-pkg-naturalett",  # Replace with your own username
+    version=os.environ.get("BUILD_VERSION", None),
+    author="Foo Bar",
+    author_email="lidor.ettinger@example.com",
+    description="A small example package",
+    url="https://github.com/pypa/sampleproject",
+    packages=['calculate'],
+    include_package_data=True,
+    scripts=['calculate/calc.py'],
+    python_requires='>=3.6',
+)
