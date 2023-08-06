15a2d02ef39ef86e726d495a077a627e29069dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:30 +0000

    Add package mca-projen-jsii-example/0.0.0/mca-projen-jsii-example-0.0.0.tar.gz

---
+++ packages/mca-projen-jsii-example/mca-projen-jsii-example-0.0.0.tar.gz/mca-projen-jsii-example-0.0.0/src/mca_projen_jsii_example/__init__.py
@@ -0,0 +1,38 @@
+'''
+# replace this
+'''
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from ._jsii import *
+
+
+class Hello(metaclass=jsii.JSIIMeta, jsii_type="mca-projen-jsii-example.Hello"):
+    '''My Hello class.'''
+
+    def __init__(self) -> None:
+        jsii.create(self.__class__, self, [])
+
+    @jsii.member(jsii_name="sayHello")
+    def say_hello(self, name: builtins.str) -> builtins.str:
+        '''My sayHello function.
+
+        :param name: Someone who calls.
+
+        :return: Greetings
+        '''
+        return typing.cast(builtins.str, jsii.invoke(self, "sayHello", [name]))
+
+
+__all__ = [
+    "Hello",
+]
+
+publication.publish()
