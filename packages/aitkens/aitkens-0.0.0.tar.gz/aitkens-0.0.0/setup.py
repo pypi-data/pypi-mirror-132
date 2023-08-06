04698d489a7bd36f2111afe51a176e981a8b5aca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:14 +0000

    Add package aitkens/0.0.0/aitkens-0.0.0.tar.gz

---
+++ packages/aitkens/aitkens-0.0.0.tar.gz/aitkens-0.0.0/setup.py
@@ -0,0 +1,25 @@
+import pathlib
+
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+
+setup(
+    name='aitkens',
+    version='0.0.0',
+    packages=['aitkens'],
+    url='https://github.com/jftsang/aitkens',
+    license='CC BY 4.0',
+    author='J. M. F. Tsang',
+    author_email='j.m.f.tsang@cantab.net',
+    description='Aitken\'s delta-squared acceleration',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    install_requires=['numpy'],
+    extras_require={'develop': ['hypothesis']},
+)
