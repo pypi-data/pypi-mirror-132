3fac740356d0e488a133d346c2b155add108a649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:09 +0000

    Add package datalibsbf/0.1/datalibsbf-0.1.tar.gz

---
+++ packages/datalibsbf/datalibsbf-0.1.tar.gz/datalibsbf-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+ 
+setup(name='datalibsbf',
+      version='0.1',
+      url='https://github.com/grupo-sbf/data-libs',
+      author='Matheus Beltrão',
+      author_email='matheus.beltrao@gruposbf.com.br',
+      description='Lib de disponibilizacao de funcoes genericas criadas dentro do time de dados',
+      packages=find_packages(exclude=['tests']),
+      long_description=open('README.md').read(),
+      zip_safe=False)
