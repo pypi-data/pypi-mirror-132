a806dc417afde71c3e6b83c3f2c08a56138a5edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:57 +0000

    Add package dinteractions-Paginator/2.0.0a1/dinteractions_Paginator-2.0.0a1.tar.gz

---
+++ packages/dinteractions-Paginator/dinteractions_Paginator-2.0.0a1.tar.gz/dinteractions_Paginator-2.0.0a1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="dinteractions_Paginator",
+    version="2.0.0a1",
+    description="Unofficial discord-interactions multi page embed handler",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JUGADOR123/dinteractions-Paginator",
+    author="Jugador123",
+    author_email="momocordova@gmail.com",
+    license="GNU",
+    packages=["dinteractions_Paginator"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["discord-py-interactions", "interactions-wait-for"],
+)
