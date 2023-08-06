825357cdc4ea4e036634edfaf70bf625c20932cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:30 +0000

    Add package bodo-platform-extensions/1.0.0/bodo_platform_extensions-1.0.0.tar.gz

---
+++ packages/bodo-platform-extensions/bodo_platform_extensions-1.0.0.tar.gz/bodo_platform_extensions-1.0.0/bodo_platform_extensions/__init__.py
@@ -0,0 +1,20 @@
+from .package_installation_magics import PackageInstallationMagics
+from .adls_setup_magic import ADLSSetupMagics
+from .hostfile_update_magic import HostfileUpdateMagic
+from .parallel_shell_magic import ParallelShellMagics
+
+
+def load_ipython_extension(ipython):
+    """
+    Register the magics with IPython
+    """
+    ipython.register_magics(PackageInstallationMagics)
+    ipython.register_magics(ADLSSetupMagics)
+    ipython.register_magics(HostfileUpdateMagic)
+    ipython.register_magics(ParallelShellMagics)
+
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
