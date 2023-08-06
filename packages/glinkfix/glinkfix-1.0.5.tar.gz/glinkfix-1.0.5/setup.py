961b8e8d838ce0240bd479f2830fb236bb7e0f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:44 +0000

    Add package glinkfix/1.0.5/glinkfix-1.0.5.tar.gz

---
+++ packages/glinkfix/glinkfix-1.0.5.tar.gz/glinkfix-1.0.5/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Setup script"""
+
+from setuptools import setup
+
+setup(
+    project_urls={
+        "Source Code": "https://github.com/geozeke/glinkfix",
+        "Bug Tracker": "https://github.com/geozeke/glinkfix/issues"
+    }
+)
