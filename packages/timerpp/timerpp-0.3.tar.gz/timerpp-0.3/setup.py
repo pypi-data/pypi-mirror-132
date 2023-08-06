d91789a5285ec09ec0a3f6111c666fb517d392b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:14 +0000

    Add package timerpp/0.3/timerpp-0.3.tar.gz

---
+++ packages/timerpp/timerpp-0.3.tar.gz/timerpp-0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from pybind11.setup_helpers import Pybind11Extension
+
+
+ext_modules = [
+    Pybind11Extension("timerpp", ["python_binding.cpp", "timerpp.h"])
+]
+
+
+setup(
+    long_description=open("README.md", "r").read(),
+    name="timerpp",
+    version="0.3",
+    description="timer class",
+    author="Pascal Eberlein",
+    author_email="pascal@eberlein.io",
+    url="https://github.com/nbdy/timerpp",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords="timer soft-timer",
+    packages=find_packages(),
+    long_description_content_type="text/markdown",
+    ext_modules=ext_modules
+)
