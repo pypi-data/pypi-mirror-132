2a5841aaaa048a01b73215693e00d761e4f93e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:47 +0000

    Add package ansible-api/0.5.1/ansible-api-0.5.1.tar.gz

---
+++ packages/ansible-api/ansible-api-0.5.1.tar.gz/ansible-api-0.5.1/src/ansible_api/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# A restful HTTP API for ansible
+# Base on ansible-runner and sanic
+# Github <https://github.com/lfbear/ansible-api>
+# Author: lfbear
+
+
+# update at 2019.10.29
+__version__ = '0.5.1'
+
+RTM_TYPE_OVERVIEW = 5
+RTM_TYPE_DETAIL = 6
+RTM_CHANNEL_DEFAULT = '#DEAULT#'
+
