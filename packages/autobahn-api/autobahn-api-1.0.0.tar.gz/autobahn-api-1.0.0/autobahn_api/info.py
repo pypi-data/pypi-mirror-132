fd88567e362343812b60a2f484916404f2c16458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:53 +0000

    Add package autobahn-api/1.0.0/autobahn-api-1.0.0.tar.gz

---
+++ packages/autobahn-api/autobahn-api-1.0.0.tar.gz/autobahn-api-1.0.0/autobahn_api/info.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+App info, license, version
+Python 3.10
+Date created: December 28th, 2021
+Date modified: -
+"""
+
+import sys
+
+VERSION = '1.0.0'
+
+
+def app_info():
+    print("\n\n   ######## Fetch Data from the Autobahn API #######")
+    print('   #              Created by niftycode             #')
+    print(f'   #                 Version {VERSION}                 #')
+    print('   #                  MIT License                  #')
+    print('   #################################################\n\n')
+    print()
+    sys.exit()
---
+++ packages/autobahn-api/autobahn-api-1.0.0.tar.gz/autobahn-api-1.0.0/autobahn_api/main.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+A Python wrapper for the Autobahn API
+provided by https://bund.dev/
+Version: 1.0
+Python 3.10
+Date created: December 28th, 2021
+Date modified: -
+"""
+
+import argparse
+import logging
+from pprint import pprint
+
+from autobahn_api import data
+from autobahn_api import info
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger()
+
+
+def get_parser() -> argparse.ArgumentParser:
+    """
+    Create a command line parser.
+    Returns:
+        argparse.ArgumentParser: Created parser
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-r",
+        "--roadworks",
+        type=str,
+        required=False,
+        help="list of roadworks"
+    )
+
+    parser.add_argument(
+        "-p",
+        "--parking",
+        type=str,
+        required=False,
+        help="list of service areas"
+    )
+
+    parser.add_argument(
+        "-v",
+        "--version",
+        required=False,
+        action="store_true",
+        help="show the version"
+    )
+
+    return parser
+
+
+def main():
+    """
+    Invoke the parser and evaluate the result.
+    """
+    parser = get_parser()
+    args = parser.parse_args()
+
+    if args.roadworks:
+        data_type = "roadworks"
+        endpoint_manager = data.EndpointManager(args.roadworks, data_type)
+        roadworks_data = endpoint_manager.fetch_data()
+        pprint(roadworks_data)
+    elif args.parking:
+        data_type = "parking_lorry"
+        endpoint_manager = data.EndpointManager(args.parking, data_type)
+        parking_lorry_data = endpoint_manager.fetch_data()
+        pprint(parking_lorry_data)
+    elif args.version:
+        info.app_info()
+    else:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
