5fc7b1f8686633641cec27928ce2cc34c548bab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:02 +0000

    Add package shrinemaiden/0.2.3/shrinemaiden-0.2.3-py3-none-any.whl

---
+++ packages/shrinemaiden/shrinemaiden-0.2.3-py3-none-any.whl/shrinemaiden/__init__.py
@@ -0,0 +1,9 @@
+# __init__.py
+import shrinemaiden.transcript
+import shrinemaiden.transcript_class
+import shrinemaiden.train
+import shrinemaiden.utils
+import shrinemaiden.charmap
+
+# Version of the package
+__version__ = "0.2.3"
