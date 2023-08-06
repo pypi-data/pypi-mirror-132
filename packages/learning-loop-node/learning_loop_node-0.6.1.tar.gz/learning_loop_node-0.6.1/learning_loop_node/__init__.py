e3ccd53b19ef0045bcbfb889406ad238277767de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:54 +0000

    Add package learning-loop-node/0.6.1/learning_loop_node-0.6.1.tar.gz

---
+++ packages/learning-loop-node/learning_loop_node-0.6.1.tar.gz/learning_loop_node-0.6.1/learning_loop_node/__init__.py
@@ -0,0 +1,7 @@
+import sys
+if sys.version_info.major >= 3 and sys.version_info.minor >= 7:  # most code needs at least python 3.7
+    from .trainer.trainer_node import TrainerNode
+    from .converter.converter_node import ConverterNode
+
+from .detector.detector_node import DetectorNode
+from .loop import loop
