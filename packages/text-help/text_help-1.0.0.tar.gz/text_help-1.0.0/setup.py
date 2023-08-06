98b64a2e56d1540af24d6b7db56091735e1071ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:04 +0000

    Add package text-help/1.0.0/text_help-1.0.0.tar.gz

---
+++ packages/text-help/text_help-1.0.0.tar.gz/text_help-1.0.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['text_help']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'text-help',
+    'version': '1.0.0',
+    'description': "Text('Some text').translit(lang), .upper(), .lower(), .rus_to_eng(), eng_to_rus(); Cipher('Text for cipher').transposing(), .to_A1Z26(lang), from_A1Z26(lang); Support langs: Eng, Rus.",
+    'long_description': None,
+    'author': 'Dima',
+    'author_email': 'ludina.work1@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
