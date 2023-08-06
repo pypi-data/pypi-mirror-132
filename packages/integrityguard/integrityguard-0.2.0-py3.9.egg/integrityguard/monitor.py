7159689871e31b51dd398de65ccf7019de320728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:47 +0000

    Add package integrityguard/0.2.0/integrityguard-0.2.0-py3.9.egg

---
+++ packages/integrityguard/integrityguard-0.2.0-py3.9.egg/integrityguard/monitor.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+import time
+import logging
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+
+def monitor():
+    logging.basicConfig(filename="",
+                        level=logging.INFO,
+                        format='[%(asctime)s] %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    event_handler = LoggingEventHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        observer.stop()
+        observer.join()
