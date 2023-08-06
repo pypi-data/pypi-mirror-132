6375faa2d0e1188773a6a7cae0c297928eda7032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:50 +0000

    Add package async-mixin/0.0.1/async_mixin-0.0.1.tar.gz

---
+++ packages/async-mixin/async_mixin-0.0.1.tar.gz/async_mixin-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['async_mixin']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiohttp>=3.8.1,<4.0.0', 'asyncio-throttle>=1.0.2,<2.0.0']
+
+setup_kwargs = {
+    'name': 'async-mixin',
+    'version': '0.0.1',
+    'description': 'Boilerplate for an Asynchronous HTTP REST Client as a Mixin',
+    'long_description': None,
+    'author': 'Andrew Rainboldt',
+    'author_email': 'arainboldt@protonmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
