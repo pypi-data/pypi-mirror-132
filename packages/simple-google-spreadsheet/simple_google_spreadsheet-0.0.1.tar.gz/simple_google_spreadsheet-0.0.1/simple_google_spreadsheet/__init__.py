e5b44565e0006c38051bb312400117a95c0622d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:33 +0000

    Add package simple-google-spreadsheet/0.0.1/simple_google_spreadsheet-0.0.1.tar.gz

---
+++ packages/simple-google-spreadsheet/simple_google_spreadsheet-0.0.1.tar.gz/simple_google_spreadsheet-0.0.1/simple_google_spreadsheet/__init__.py
@@ -0,0 +1,17 @@
+from simple_google_spreadsheet.spreadsheet import (
+    SpreadSheet,
+    TokenFileTypeException,
+    CredentialsFileTypeException,
+    CredentialsFileNotFoundException,
+    InvalidSheet,
+    convert1toA
+)
+
+__all__ = [
+    'SpreadSheet',
+    'TokenFileTypeException',
+    'CredentialsFileTypeException',
+    'CredentialsFileNotFoundException',
+    'InvalidSheet',
+    'convert1toA'
+]
