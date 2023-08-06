498693bf1ba68190c781cf8cfb17c9dd0cb3f66c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:41 +0000

    Add package odin-messages/0.2.2/odin-messages-0.2.2.tar.gz

---
+++ packages/odin-messages/odin-messages-0.2.2.tar.gz/odin-messages-0.2.2/odin_messages/base.py
@@ -0,0 +1,14 @@
+from typing import List
+from pydantic import BaseModel
+
+
+class BaseEventMessage(BaseModel):
+    event: str
+    time: float
+    strategy_id: str
+
+class BaseEventMessageArray(BaseModel):
+    event: str
+    time: float
+    strategy_id: str
+    messages: List[BaseEventMessage]
