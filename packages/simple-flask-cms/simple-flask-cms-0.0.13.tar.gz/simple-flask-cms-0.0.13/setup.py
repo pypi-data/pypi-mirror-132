a5db743580c372d0a259469bc360f68f9ba4a26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:57 +0000

    Add package simple-flask-cms/0.0.13/simple-flask-cms-0.0.13.tar.gz

---
+++ packages/simple-flask-cms/simple-flask-cms-0.0.13.tar.gz/simple-flask-cms-0.0.13/setup.py
@@ -0,0 +1,44 @@
+import pathlib
+from setuptools import setup
+import os
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="simple-flask-cms",
+    version="0.0.13",
+    description="A minimal CMS for flask",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/mousetail/simple-flask-cms",
+    author="Maurits van Riezen (mousetail)",
+    author_email="mousetail+pypi@mousetail.nl",
+    license="GPL-2",
+    classifiers=[
+
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Development Status :: 4 - Beta",
+        "Framework :: Flask",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)"
+    ],
+    packages=["simple_flask_cms", "simple_flask_cms.database_providers"],
+    include_package_data=True,
+    package_dir={
+        'simple_flask_cms': 'simple_flask_cms'
+    },
+    package_data={
+        'simple_flask_cms': [
+            'static/*'
+            'static/generated/*',
+            'templates/cms/*',
+            'templates/cms/example_templates/*',
+        ]
+    },
+    install_requires=["flask", "markdown", "flask_sqlalchemy", 'bleach', 'pillow', 'pydantic'],
+)
---
+++ packages/simple-flask-cms/simple-flask-cms-0.0.13.tar.gz/simple-flask-cms-0.0.13/simple_flask_cms/__init__.py
@@ -0,0 +1,4 @@
+from .cms_blueprint import cms
+from . import config
+from . import database_providers
+from . import dataclasses
