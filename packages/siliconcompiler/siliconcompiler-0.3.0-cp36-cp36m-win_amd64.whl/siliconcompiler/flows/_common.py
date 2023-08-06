1fbda602d586e9e3a6ff6343047da124d5e1087c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:35 +0000

    Add package siliconcompiler/0.3.0/siliconcompiler-0.3.0-cp36-cp36m-win_amd64.whl

---
+++ packages/siliconcompiler/siliconcompiler-0.3.0-cp36-cp36m-win_amd64.whl/siliconcompiler/flows/_common.py
@@ -0,0 +1,20 @@
+def setup_frontend(chip):
+    ''' Return list of frontend steps to be prepended to flowgraph as list of
+    (step, tool) tuples. '''
+
+    frontend = chip.get('frontend')
+    frontend_flow = []
+
+    if frontend in ('verilog', 'systemverilog'):
+        frontend_flow.append(('import', 'surelog'))
+    elif frontend == 'chisel':
+        frontend_flow.append(('import', 'chisel'))
+    elif frontend == 'c':
+        frontend_flow.append(('import', 'bambu'))
+    else:
+        raise ValueError(f'Unsupported frontend: {frontend}')
+
+    if frontend == 'systemverilog':
+        frontend_flow.append(('convert', 'sv2v'))
+
+    return frontend_flow
