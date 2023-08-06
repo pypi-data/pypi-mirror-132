af5c9cfaa106b312e469f779da524d4cc91861fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:24 +0000

    Add package Evison/0.1.3/Evison-0.1.3.tar.gz

---
+++ packages/Evison/Evison-0.1.3.tar.gz/Evison-0.1.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+
+setuptools.setup(
+    name="Evison",
+    version="0.1.3",
+    author="Jones Lin",
+    author_email="jonneslin@gmail.com",
+    description="Feature map visualization",
+    url="https://github.com/JonnesLin/Evison",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'torchviz'
+    ]
+)
