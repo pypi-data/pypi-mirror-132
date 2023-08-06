7460f17aadf2c63dd70ad67578a07fd1c9b764ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:34 +0000

    Add package dash-mdc-neptune/0.1.2/dash_mdc_neptune-0.1.2-py3-none-any.whl

---
+++ packages/dash-mdc-neptune/dash_mdc_neptune-0.1.2-py3-none-any.whl/dash_mdc_neptune/_imports_.py
@@ -0,0 +1,21 @@
+from .Dashboard import Dashboard
+from .Box import Box
+from .Tab import Tab
+from .Table import Table
+from .Toggle import Toggle
+from .Typography import Typography
+from .NavBar import NavBar
+from .Page import Page
+from .Section import Section
+
+__all__ = [
+    "Dashboard",
+    "Box",
+    "Tab",
+    "Table",
+    "Toggle",
+    "Typography",
+    "NavBar",
+    "Page",
+    "Section"
+]
