e598da0a8ac7b6e154e2ca126171af673ef93be2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:04 +0000

    Add package fipiran/0.6.0/fipiran-0.6.0.tar.gz

---
+++ packages/fipiran/fipiran-0.6.0.tar.gz/fipiran-0.6.0/fipiran/data_service.py
@@ -0,0 +1,119 @@
+from typing import TypedDict as _TypedDict
+
+from jdatetime import datetime as _jdatetime
+import pandas as _pd
+
+from . import _fipiran
+
+
+_jstrptime = _jdatetime.strptime
+
+
+def mutual_fund_list() -> _pd.DataFrame:
+    """Also see fipiran.fund.funds function."""
+    return _pd.read_html(_fipiran('DataService/ExportMFList'))[0]
+
+
+def auto_complete_fund(id_: str) -> list[
+    _TypedDict('FundAutoComplete', {'RegNo': int, 'Name': str})
+]:
+    return _fipiran('DataService/AutoCompletefund', (('id', id_),), True)
+
+
+def mutual_fund_data(
+    name: str, start_date: str, end_date: str,
+    reg_no: int = None
+) -> _pd.DataFrame:
+    """Return history of NAV, units, issue, and cancel.
+
+    There various functions in this library to retrieve `reg_no`. If you
+        want the names and numbers for all funds, you can use
+        `mutual_fund_list`, but if only a single fund is desired the
+        `auto_complete_fund` function can be used.
+    `reg_no` is optional and if left out then the first result of
+        `auto_complete_fund` will be used.
+
+    Date parameters should be strings representing SH dates e.g.:
+        '1394/01/01'
+    """
+    if reg_no is None:
+        d = auto_complete_fund(name)[0]
+        reg_no = d['RegNo']
+        name = d['Name']
+    xls = _fipiran('DataService/ExportMF', (
+        ('Mutualpara', name),
+        ('RegNoN', reg_no),
+        ('MFStart', start_date),
+        ('MFEnd', end_date)))
+    df = _pd.read_html(xls)[0]
+    df['Date'] = df['Date'].apply(_jstrptime, args=('%Y/%m/%d',))
+    df['AghazFaliat'] = df['AghazFaliat'].apply(_jstrptime, args=('%Y/%m/%d',))
+    return df
+
+
+def auto_complete_index(id_: str) -> list[
+    _TypedDict('IndexAutoComplete', {'LVal30': str, 'InstrumentID': str})
+]:
+    return _fipiran('DataService/AutoCompleteindex', (('id', id_),), True)
+
+
+def export_index(
+    lval30: str, start_date: str | int, end_date: str | int,
+    instrument_id: str = None
+) -> _pd.DataFrame:
+    """Return history of requested index.
+
+    Use the `auto_complete_index` function to retrieve lval30 and instrument_id
+        of the desired index.
+    `instrument_id` is optional and if left out then the first result of
+        `auto_complete_symbol` will be used.
+    Date parameters should be SH dates in YYYYMMDD format e.g.:
+        '13940101'
+    """
+    if instrument_id is None:
+        d = auto_complete_index(lval30)[0]
+        lval30 = d['LVal30']
+        instrument_id = d['InstrumentID']
+    xls = _fipiran('DataService/ExportIndex', (
+        ('indexpara', lval30),
+        ('inscodeindex', instrument_id),
+        ('indexStart', start_date),
+        ('indexEnd', end_date)))
+    df = _pd.read_html(xls)[0]
+    df['dateissue'] = df['dateissue'].apply(str).apply(_jstrptime, args=('%Y%m%d',))
+    return df
+
+
+def auto_complete_symbol(id_: str) -> list[
+    _TypedDict('SymbolAutoComplete', {'LVal18AFC': str, 'InstrumentID': str})
+]:
+    return _fipiran('DataService/AutoCompletesymbol', (('id', id_),), True)
+
+
+def export_symbol(
+    lval18afc: str, start_date: str | int, end_date: str | int,
+    instrument_id: str = None
+) -> _pd.DataFrame:
+    """Return history of requested index.
+
+    Use the `auto_complete_symbol` function to retrieve `lval18afc` and
+        `instrument_id` of the desired symbol.
+    `instrument_id` is optional and if left out then the first result of
+        `auto_complete_symbol` will be used.
+
+    Date parameters should be SH dates in YYYYMMDD format e.g.:
+        '13940101'
+    """
+    if instrument_id is None:
+        d = auto_complete_symbol(lval18afc)[0]
+        lval18afc = d['LVal18AFC']
+        instrument_id = d['InstrumentID']
+    xls = _fipiran('DataService/Exportsymbol', (
+        ('symboldatapara', lval18afc),
+        ('inscodesymbol', instrument_id),
+        ('symbolStart', start_date),
+        ('symbolEnd', end_date)))
+    df = _pd.read_html(xls)[0]
+    df['PDate'] = df['PDate'].apply(str).apply(_jstrptime, args=('%Y%m%d',))
+    df['GDate'] = _pd.to_datetime(df['GDate'], format='%Y%m%d')
+    return df
---
+++ packages/fipiran/fipiran-0.6.0.tar.gz/fipiran-0.6.0/fipiran/fund.py
@@ -0,0 +1,61 @@
+from functools import partial as _partial
+
+from jdatetime import datetime as _jdatetime
+from pandas import DataFrame as _DataFrame, to_datetime as _to_datetime, \
+    read_html as _read_html, to_numeric as _to_numeric, NA as _NA
+from . import _api, _fipiran
+
+
+class FundProfile:
+    __slots__ = 'reg_no'
+
+    def __init__(self, reg_no: int | str):
+        self.reg_no = reg_no
+
+    def __repr__(self):
+        return f'{type(self).__name__}({self.reg_no!r})'
+
+    def asset_allocation(self) -> dict:
+        """Return a dict where values are percentage of each kind of asset."""
+        return _api(f'chart/getfundchartasset?regno={self.reg_no}')
+
+    def issue_cancel_history(self) -> _DataFrame:
+        j = _api(f'chart/getfundchart?regno={self.reg_no}')
+        df = _DataFrame(j)
+        df['date'] = _to_datetime(df['date'])
+        df.set_index('date', inplace=True)
+        return df
+
+    def nav_history(self) -> _DataFrame:
+        j = _api(f'chart/getfundnetassetchart?regno={self.reg_no}')
+        df = _DataFrame(j)
+        df['date'] = _to_datetime(df['date'])
+        df.set_index('date', inplace=True)
+        return df
+
+    def info(self):
+        return _api(f'fund/getfund?regno={self.reg_no}')['item']
+
+
+def funds() -> _DataFrame:
+    """Also see fipiran.data_service.mutual_fund_list function."""
+    return _DataFrame(_api('fund/fundlist')['items'])
+
+
+def average_returns() -> _DataFrame:
+    return _read_html(_fipiran('Fund/MFBazdehAVG'))[0]
+
+
+def ratings() -> _DataFrame:
+    df = _read_html(_fipiran('Fund/Rating'))[0]
+    # Cannot use iloc in LHS, see: https://stackoverflow.com/questions/52395179
+    df[df.columns[1:4]] = df.iloc[:, 1:4].apply(_to_numeric, args=('coerce',))
+    _parse_jdate = _partial(_jdatetime.strptime, format='%Y/%m/%d')
+
+    def _jdate_na(s: str):
+        try:
+            return _parse_jdate(s)
+        except ValueError:
+            return _NA
+    df.iloc[:, -1] = df.iloc[:, -1].apply(_jdate_na)
+    return df
