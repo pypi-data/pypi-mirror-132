cd78dbc654c001d8d5e03d890a45b9a9c96b2b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:04 +0000

    Add package bw2parameters/0.7/bw2parameters-0.7.tar.gz

---
+++ packages/bw2parameters/bw2parameters-0.7.tar.gz/bw2parameters-0.7/bw2parameters/__init__.py
@@ -0,0 +1,9 @@
+__version__ = (0, 7)
+
+from .mangling import (
+    FormulaSubstitutor,
+    mangle_formula,
+    prefix_parameter_dict,
+    substitute_in_formulas,
+)
+from .parameter_set import ParameterSet
