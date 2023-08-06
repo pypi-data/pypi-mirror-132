9562d1b4c12968e8ea90a26b9341be2605614fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:30 +0000

    Add package autobahn-api/1.0.6/autobahn_api-1.0.6-py2.py3-none-any.whl

---
+++ packages/autobahn-api/autobahn_api-1.0.6-py2.py3-none-any.whl/autobahn_api/info.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+App info, license, version
+Python 3.10
+Date created: December 28th, 2021
+"""
+
+import sys
+
+VERSION = '1.0.6'
+
+
+def app_info():
+    print("\n\n   ######## Fetch Data from the Autobahn API #######")
+    print('   #              Created by niftycode             #')
+    print(f'   #                 Version {VERSION}                 #')
+    print('   #                  MIT License                  #')
+    print('   #################################################\n\n')
+    print()
+    sys.exit()
