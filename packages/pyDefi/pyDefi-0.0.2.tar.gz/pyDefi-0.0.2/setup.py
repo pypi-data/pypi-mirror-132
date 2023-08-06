54ab1ab54388cb72b03a9125a7e31fc36b890617
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:22 +0000

    Add package pyDefi/0.0.2/pyDefi-0.0.2.tar.gz

---
+++ packages/pyDefi/pyDefi-0.0.2.tar.gz/pyDefi-0.0.2/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='pyDefi',
+        version='0.0.2',
+        description='Python SDK for the DefiChain',
+        author='Richard Strnad',
+        author_email='richard@strnad.ch',
+        url='',
+        packages=['pyDefi'],
+        install_requires=[
+            'jsonrpcclient',
+            'requests'
+        ]
+     )
