53c385361f6e74d5256be7fe822cad900319adfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:58 +0000

    Add package cmdline-mini/1.0.1/cmdline_mini-1.0.1-py3-none-any.whl

---
+++ packages/cmdline-mini/cmdline_mini-1.0.1-py3-none-any.whl/cmdline/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from .cmdline import Option, cmdline_args, system_args, redirect_stdout, add_pythonpath
+
+__all__ = (
+    'Option',
+    'cmdline_args',
+    'system_args',
+    'redirect_stdout',
+    'add_pythonpath'
+)
