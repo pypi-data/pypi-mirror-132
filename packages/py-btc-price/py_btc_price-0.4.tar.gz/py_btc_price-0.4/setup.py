36958944be433f45f3cd6c820336441363667a75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:54 +0000

    Add package py-btc-price/0.4/py_btc_price-0.4.tar.gz

---
+++ packages/py-btc-price/py_btc_price-0.4.tar.gz/py_btc_price-0.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+VERSION = '0.4'
+DESCRIPTION = ''
+
+setup(
+    name = 'py_btc_price',
+    packages = ['py_btc_price'],
+    version = VERSION,
+    license='MIT',
+    description = DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    author = 'Eduardo Ismael García Pérez',
+    author_email = 'eduardo78d@gmail.com',
+    url = 'https://github.com/eduardogpg/py_btc_price',
+    # download_url = 'https://github.com/eduardogpg/py_btc_price/archive/refs/tags/0.4.tar.gz',
+    keywords = ['Criptocurrency', 'BTC', 'Bitcoin Price', 'BTC price'],
+    install_requires=[ 
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
