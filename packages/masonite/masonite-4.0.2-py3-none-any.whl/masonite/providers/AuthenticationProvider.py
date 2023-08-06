9e42bb3af0f7318e053e31aa6924fc93a0cd61d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:52 +0000

    Add package masonite/4.0.2/masonite-4.0.2-py3-none-any.whl

---
+++ packages/masonite/masonite-4.0.2-py3-none-any.whl/masonite/providers/AuthenticationProvider.py
@@ -0,0 +1,21 @@
+from ..foundation import response_handler
+from ..request import Request
+from ..response import Response
+from ..authentication import Auth
+from ..authentication.guards import WebGuard
+from ..configuration import config
+from .Provider import Provider
+
+
+class AuthenticationProvider(Provider):
+    def __init__(self, application):
+        self.application = application
+
+    def register(self):
+        auth = Auth(self.application).set_configuration(config("auth.guards"))
+        auth.add_guard("web", WebGuard(self.application))
+
+        self.application.bind("auth", auth)
+
+    def boot(self):
+        pass
