1d041b5e48e4819f8903953ed934f362dc3e148a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:29 +0000

    Add package zpylib/0.0.4/zpylib-0.0.4.tar.gz

---
+++ packages/zpylib/zpylib-0.0.4.tar.gz/zpylib-0.0.4/src/zpylib/Build/build.py
@@ -0,0 +1,43 @@
+import os
+from Compiler import compiler
+
+
+class Build(object):
+
+    def __init__(self, filename, target_type):
+        self.cwd = os.getcwd()
+        self.filename = filename
+        self.target_type = target_type
+        self.file_type = self.getFileType()
+        self.file = self.readFile()
+        self.build()
+
+    def getFileType(self):
+        return self.filename.split('.')[1]
+
+    def readFile(self):
+        try:
+            with open(self.filename) as raw:
+                script = raw.read()
+                raw.close()
+            return script
+        except Exception as e:
+            raise Exception(f"错误: 找不到文件 {self.filename}\n目录: {self.cwd}")
+
+    def build(self):
+        if self.target_type and self.target_type not in ['py', 'zpy']:
+            raise Exception(f"错误: 目标格式 {self.target_type} 只能是 py 或 zpy")
+            return
+        elif self.file_type not in ['py', 'zpy']:
+            raise Exception(f"错误: 文件格式 {self.file_type} 只能是 py 或 zpy")
+            return
+        elif self.target_type == self.file_type:
+            print("警告: 原格式与目标格式相同")
+            return
+        elif self.target_type is None:
+            if self.file_type == 'zpy': self.target_type ='py'
+            elif self.file_type == 'py': self.target_type ='zpy'
+            else: raise Exception(f"错误: 文件格式 {self.file_type} 只能是 py 或 zpy")
+
+        result = compiler.run(self.file, self.target_type)
+        return result
