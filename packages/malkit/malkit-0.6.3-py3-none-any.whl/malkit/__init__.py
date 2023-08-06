a506e93fb368036a128134e45f161691f716af55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:58 +0000

    Add package malkit/0.6.3/malkit-0.6.3-py3-none-any.whl

---
+++ packages/malkit/malkit-0.6.3-py3-none-any.whl/malkit/__init__.py
@@ -0,0 +1,25 @@
+from .byte import convert_binary_to_byte_seq, convert_binary_to_byte_seq_parallel
+from .image import convert_binary_to_image, convert_binary_to_image_parallel, resize_image, resize_image_parallel
+from .utils import (
+    build_srcs_dsts,
+    categorize_folders,
+    convert_bytes_to_binary,
+    convert_bytes_to_binary_parallel,
+    split_labels,
+)
+
+__all__ = [
+    "convert_binary_to_byte_seq",
+    "convert_binary_to_byte_seq_parallel",
+    "convert_binary_to_image",
+    "convert_binary_to_image_parallel",
+    "resize_image",
+    "resize_image_parallel",
+    "categorize_folders",
+    "split_labels",
+    "build_srcs_dsts",
+    "convert_bytes_to_binary",
+    "convert_bytes_to_binary_parallel",
+]
+
+__version__ = "0.6.3"
---
+++ packages/malkit/malkit-0.6.3-py3-none-any.whl/malkit/_parallel.py
@@ -0,0 +1,19 @@
+from typing import Any, Callable, Iterable, Optional
+
+from joblib import Parallel, delayed
+
+from ._typing import FilePath
+
+
+def execute_parallel(
+    function: Callable[[FilePath, FilePath], None],
+    srcs: Iterable[FilePath],
+    dsts: Iterable[FilePath],
+    *,
+    n_jobs: Optional[int] = None,
+    **kwargs: Any,
+) -> None:
+    """Execute function in parallel."""
+
+    delayed_function = delayed(function)
+    Parallel(n_jobs=n_jobs, **kwargs)(delayed_function(src, dst) for src, dst in zip(srcs, dsts))
