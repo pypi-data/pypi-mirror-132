dd2993a4243cfb210be7b7c4418f2fc88b918371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:19 +0000

    Add package asyncrd/2.1.5/asyncrd-2.1.5.tar.gz

---
+++ packages/asyncrd/asyncrd-2.1.5.tar.gz/asyncrd-2.1.5/asyncrd/query.py
@@ -0,0 +1,34 @@
+import asyncio, typing
+from .exceptions import CatchException, RedisException
+
+class Result():
+    def __init__(self, result : str):
+        self.result : str = result          
+    
+
+class Set():
+    def __init__(self, query : str):
+        self.query : str = query
+            
+class Get():
+    def __init__(self, query : str):
+        self.query : str = query
+
+class Query():
+    def __init__(self, connection):
+        self.reader = connection.reader
+        self.writer = connection.writer
+        
+    async def do_query(self, protocol : typing.Union[Get, Set]):
+        data_ = protocol.query+"\r\n"
+        self.writer.write(data_.encode())
+        await self.writer.drain()
+        data = await self.reader.read(100)
+        res = Result(data.decode('utf-8'))
+        res = res.result
+        res = res.replace("\r", "")
+        res = res.replace("\n", "")
+        catching = CatchException(text=text)
+        await catching.catch_error()
+        return res
+    
