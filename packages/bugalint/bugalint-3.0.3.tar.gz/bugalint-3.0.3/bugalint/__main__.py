13705f98f3a38a86f2f9ab8a31e0f11a2ae80bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:03 +0000

    Add package bugalint/3.0.3/bugalint-3.0.3.tar.gz

---
+++ packages/bugalint/bugalint-3.0.3.tar.gz/bugalint-3.0.3/bugalint/__main__.py
@@ -0,0 +1,4 @@
+"""Enables running the module with python -m"""
+
+from bugalint.cli import main
+main()
---
+++ packages/bugalint/bugalint-3.0.3.tar.gz/bugalint-3.0.3/setup.py
@@ -0,0 +1,40 @@
+"""Setup script for bugalint"""
+
+from setuptools import setup  # type: ignore
+
+setup(
+    name='bugalint',
+    version='3.0.3',
+    url='https://github.com/bugale/bugalint',
+    license='MIT',
+    author='Bugale',
+    author_email='bugale@bugalit.com',
+    description='An abstraction for running linters on code',
+    packages=['bugalint'],
+    package_data={'': ['*.ini']},
+    zip_safe=False,
+    platforms='any',
+    install_requires=['lintly', 'flake8', 'mypy', 'pylint'],
+    entry_points={
+        'console_scripts': [
+            'bugalint = bugalint.cli:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+    ]
+)
