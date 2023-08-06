e191dc65ac7a17b9fd9c2939e13b7de7068d644c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:32 +0000

    Add package sphong-colorpalette/0.0.1/sphong-colorpalette-0.0.1.tar.gz

---
+++ packages/sphong-colorpalette/sphong-colorpalette-0.0.1.tar.gz/sphong-colorpalette-0.0.1/README.md
@@ -0,0 +1,16 @@
+# colorpalette
+Python color palettes for scientific publication
+
+
+# Dependencies
+
+```
+pip install ipykernel
+pip install matplotlib
+```
+
+# Installation
+
+```
+pip install colorpalette
+```
---
+++ packages/sphong-colorpalette/sphong-colorpalette-0.0.1.tar.gz/sphong-colorpalette-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+ 
+setup(name='sphong-colorpalette',
+      version='0.0.1',
+      description='Python color palettes for scientific publication',
+      author='Seungpyo Hong',
+      author_email='sp1020@gmail.com',
+      url='',
+      packages=['colorpalette'],
+      install_requires=['ipykernel', 'matplotlib'],
+     )
