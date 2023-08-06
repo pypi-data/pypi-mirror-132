5ef93a79184f746e47ae0c9320649c3e708757f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:09 +0000

    Add package great-expectations/0.13.48/great_expectations-0.13.48-py3-none-any.whl

---
+++ packages/great-expectations/great_expectations-0.13.48-py3-none-any.whl/great_expectations/types/__init__.py
@@ -0,0 +1,25 @@
+from .configurations import ClassConfig
+
+
+class DictDot:
+    def __getitem__(self, item):
+        if isinstance(item, int):
+            return list(self.__dict__.keys())[item]
+        return getattr(self, item)
+
+    def __setitem__(self, key, value):
+        setattr(self, key, value)
+
+    def __delitem__(self, key):
+        delattr(self, key)
+
+    def __contains__(self, key):
+        return hasattr(self, key)
+
+    def __len__(self):
+        return len(self.__dict__)
+
+
+class SerializableDictDot(DictDot):
+    def to_json_dict(self) -> dict:
+        raise NotImplementedError
