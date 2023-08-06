3c65a7893efdcfa4d915d75532decfb4911522c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:52 +0000

    Add package asyncrd/2.0.9/asyncrd-2.0.9-py3-none-any.whl

---
+++ packages/asyncrd/asyncrd-2.0.9-py3-none-any.whl/asyncrd/connection.py
@@ -0,0 +1,24 @@
+import asyncio, socket
+from urllib.parse import urlparse
+from .query import Query, Get, Set
+
+class ConnectionProtocol():
+    def __init__(self, connection_url : str):
+        self.connection_url = urlparse(connection_url)
+        self.hostname = self.connection_url.hostname
+        self.port = self.connection_url.port
+        
+    async def connect(self):
+        self.reader, self.writer = await asyncio.open_connection(self.hostname, self.port)
+        
+    async def get(self, query : str):
+        data = Query(self)
+        result = await data.do_query(Get(query=query))
+        await asyncio.sleep(2)
+        return result
+
+    async def set(self, query : str):
+        data = Query(self)
+        result = await data.do_query(Set(query=query))
+        await asyncio.sleep(2)
+        return result
