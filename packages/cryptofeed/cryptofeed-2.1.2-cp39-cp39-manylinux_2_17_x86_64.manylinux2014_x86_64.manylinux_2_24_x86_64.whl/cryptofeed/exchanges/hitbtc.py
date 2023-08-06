59cc74883bf5bc3b8ac0005b29fc96688f2c8f87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:23:46 +0000

    Add package cryptofeed/2.1.2/cryptofeed-2.1.2-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_24_x86_64.whl

---
+++ packages/cryptofeed/cryptofeed-2.1.2-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_24_x86_64.whl/cryptofeed/exchanges/hitbtc.py
@@ -0,0 +1,22 @@
+'''
+Copyright (C) 2017-2021  Bryant Moscon - bmoscon@gmail.com
+
+Please see the LICENSE file for the terms and conditions
+associated with this software.
+'''
+import logging
+
+from cryptofeed.defines import HITBTC
+from cryptofeed.exchanges import Bequant
+
+LOG = logging.getLogger('feedhandler')
+
+
+class HitBTC(Bequant):
+    id = HITBTC
+    symbol_endpoint = 'https://api.hitbtc.com/api/2/public/symbol'
+    websocket_endpoint = {
+        'market': 'wss://api.hitbtc.com/api/2/ws/public',
+        'trading': 'wss://api.hitbtc.com/api/2/ws/trading',
+        'account': 'wss://api.hitbtc.com/api/2/ws/account',
+    }
