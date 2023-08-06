e8e800daccc50d9049338060018a5aa21bbbca19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:48 +0000

    Add package simple-component-folder/0.0.2/simple-component-folder-0.0.2.tar.gz

---
+++ packages/simple-component-folder/simple-component-folder-0.0.2.tar.gz/simple-component-folder-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='simple-component-folder', 
+   version='0.0.2',
+   description='A useful slider module',
+#   py_modules=['setup.py','main_program', 'simplecomponent'], 
+   author='Siamese',
+   author_email='null@null.com',
+   license='MIT',
+ 
+   packages=find_packages('simple-component-folder'),
+   package_dir={'': 'simple-component-folder'},  
+   install_requires=['ipython==7.30.1', 'streamlit==1.2.0', ] 
+   )
+#'numpy==1.14.5', 'python_version>="0.0.2'
