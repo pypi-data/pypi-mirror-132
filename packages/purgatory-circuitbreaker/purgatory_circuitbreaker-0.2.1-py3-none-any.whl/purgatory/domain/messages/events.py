f62ead300593aaec06b5fd2902be9eea81cd7c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:47 +0000

    Add package purgatory-circuitbreaker/0.2.1/purgatory_circuitbreaker-0.2.1-py3-none-any.whl

---
+++ packages/purgatory-circuitbreaker/purgatory_circuitbreaker-0.2.1-py3-none-any.whl/purgatory/domain/messages/events.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import Optional
+
+from .base import Event
+
+
+@dataclass(frozen=True)
+class CircuitBreakerStateChanged(Event):
+    name: str
+    state: str
+    opened_at: Optional[float]
+
+
+@dataclass(frozen=True)
+class CircuitBreakerFailed(Event):
+    name: str
+    failure_count: int
+
+
+@dataclass(frozen=True)
+class CircuitBreakerRecovered(Event):
+    name: str
