b35e80c7687655b6e614d1665903d0be013764e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:35 +0000

    Add package locr/0.4.0/locr-0.4.0.tar.gz

---
+++ packages/locr/locr-0.4.0.tar.gz/locr-0.4.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+packages = ['locr']
+
+requires = [
+    'bs4',
+    'requests'
+]
+
+setuptools.setup(
+    name="locr",
+    version="0.4.0",
+    author="Andromeda Yelton",
+    description="Tools for fetching OCRed text of Library of Congress items.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thatandromeda/locr",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=packages,
+    package_dir={"locr": "locr"},
+    python_requires=">=3.0",
+    install_requires=requires,
+)
