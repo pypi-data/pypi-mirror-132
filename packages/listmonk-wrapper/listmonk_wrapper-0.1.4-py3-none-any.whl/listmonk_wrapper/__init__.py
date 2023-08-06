463dc69eb1b64b6d716e7726bc8964e2e02b7ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:24 +0000

    Add package listmonk-wrapper/0.1.4/listmonk_wrapper-0.1.4-py3-none-any.whl

---
+++ packages/listmonk-wrapper/listmonk_wrapper-0.1.4-py3-none-any.whl/listmonk_wrapper/__init__.py
@@ -0,0 +1,14 @@
+#
+# Created on Wed Dec 22 2021
+#
+# Copyright (c) 2021 Lenders Cooperative, a division of Summit Technology Group, Inc.
+#
+
+"""Top-level package for listmonk_wrapper."""
+
+__author__ = """Michael McClelland"""
+__email__ = "mmcclelland@thesummitgrp.com"
+__version__ = "0.1.4"
+
+
+from .client import ListMonkClient
