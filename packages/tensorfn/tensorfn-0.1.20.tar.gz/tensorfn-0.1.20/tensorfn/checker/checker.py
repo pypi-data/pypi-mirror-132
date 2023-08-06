ee3ae0a91492cc821dafbfa48b79c98a9432d4ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:24 +0000

    Add package tensorfn/0.1.20/tensorfn-0.1.20.tar.gz

---
+++ packages/tensorfn/tensorfn-0.1.20.tar.gz/tensorfn-0.1.20/tensorfn/checker/checker.py
@@ -0,0 +1,31 @@
+import os
+
+try:
+    import private
+
+except ImportError:
+    private = None
+
+from tensorfn import distributed as dist
+from tensorfn.checker.backend import Local
+
+
+class Checker:
+    def __init__(self, storages=None, reporters=None):
+        self.storages = storages
+        self.reporters = reporters
+
+    def save(self, data, name):
+        if dist.is_primary():
+            for storage in self.storages:
+                storage.save(data, name)
+
+    def checkpoint(self, obj, name):
+        if dist.is_primary():
+            for storage in self.storages:
+                storage.checkpoint(obj, name)
+
+    def log(self, step=None, **kwargs):
+        if dist.is_primary():
+            for reporter in self.reporters:
+                reporter.log(step, **kwargs)
