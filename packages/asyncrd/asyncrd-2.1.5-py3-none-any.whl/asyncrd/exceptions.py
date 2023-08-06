6e75a1a3ff72f73c37913857748912430b6446e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:19 +0000

    Add package asyncrd/2.1.5/asyncrd-2.1.5-py3-none-any.whl

---
+++ packages/asyncrd/asyncrd-2.1.5-py3-none-any.whl/asyncrd/exceptions.py
@@ -0,0 +1,20 @@
+import asyncio, typing
+import aioconsole
+
+class RedisException(Exception):
+    def __init__(self, message : str):
+        super().__init__(message)
+
+
+class CatchException():
+    def __init__(self, text : str):
+        self.text = text
+        
+    async def catch_error(self):
+        if self.text.starswith("-ERR"):
+            text = self.text.split("-ERR ")
+            raise RedisException(text[1])
+            return
+        return self.text
+            
+        
