0e108ed7055c1cf6a5f220b3db801839c13855f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:20 +0000

    Add package mloperations/0.0.5/mloperations-0.0.5-py3-none-any.whl

---
+++ packages/mloperations/mloperations-0.0.5-py3-none-any.whl/mlops/__init__.py
@@ -0,0 +1,10 @@
+"""mlops is a package for conducting MLOps, including versioning of datasets and
+models."""
+
+from __future__ import absolute_import
+
+__version__ = '0.0.5'
+
+import mlops.dataset
+import mlops.errors
+import mlops.model
