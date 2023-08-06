f5d1834a879f465f3350efbcd33afd975c51a93d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:34 +0000

    Add package bp-apollo-client/0.0.6/bp_apollo_client-0.0.6-py3-none-any.whl

---
+++ packages/bp-apollo-client/bp_apollo_client-0.0.6-py3-none-any.whl/bp_apollo_client/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from .apollo_client import ApolloClient
+from .apollo_client import ApolloData
+
+__version__ = "0.0.6"
+__author__ = 'foxliu'
+__email__ = 'foxliu2012@gmail.com'
