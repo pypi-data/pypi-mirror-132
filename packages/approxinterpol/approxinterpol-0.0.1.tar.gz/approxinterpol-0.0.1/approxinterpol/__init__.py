15a55350b1fadc4b135e04d72fbb7f9c591534a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:02 +0000

    Add package approxinterpol/0.0.1/approxinterpol-0.0.1.tar.gz

---
+++ packages/approxinterpol/approxinterpol-0.0.1.tar.gz/approxinterpol-0.0.1/approxinterpol/__init__.py
@@ -0,0 +1,14 @@
+"""
+``approxinterpol``
+=====================
+
+Библиотека для получения функции из ряда точек
+при помощи алгоритмов аппроксимации и интерполяции.
+
+"""
+from .approxinterpol import (quadratic_function, linear_function, splitting_appr,
+                             newton_interpolation, lagrange_interpolation, cubic_spline_interpolation)
+
+__author__ = 'Mark Kozlov'
+
+__version__ = "0.0.1"
