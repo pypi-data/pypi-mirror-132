d59bd0c5e2a6d415534958d95532a468d0b17b55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:46 +0000

    Add package fractal-toolkit/0.2.9/fractal-toolkit-0.2.9.tar.gz

---
+++ packages/fractal-toolkit/fractal-toolkit-0.2.9.tar.gz/fractal-toolkit-0.2.9/fractal/__init__.py
@@ -0,0 +1,21 @@
+"""Fractal is a scaffolding toolkit for building SOLID logic for your Python applications."""
+
+__version__ = "0.2.9"
+
+from abc import ABC
+
+
+class FractalException(Exception):
+    pass
+
+
+class Fractal(ABC):
+    def __init__(self):
+        if not hasattr(self, "settings"):
+            raise FractalException(
+                "Fractal service doesn't have a 'settings' attribute."
+            )
+        if not hasattr(self, "context"):
+            raise FractalException(
+                "Fractal service doesn't have a 'context' attribute."
+            )
