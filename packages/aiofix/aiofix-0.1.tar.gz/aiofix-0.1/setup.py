49ad0941f21280e088b6e693ac195d4c4d42e8b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:37 +0000

    Add package aiofix/0.1/aiofix-0.1.tar.gz

---
+++ packages/aiofix/aiofix-0.1.tar.gz/aiofix-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+     name='aiofix',  
+     version='0.1',
+     scripts=['aiofix'] ,
+     author="fwang",
+     author_email="fwang@mowentech.com",
+     description="Place holder!",
+     long_description="Place Holder!",
+     url="https://aiofix.com",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Development Status :: 1 - Planning",
+     ],
+ )
