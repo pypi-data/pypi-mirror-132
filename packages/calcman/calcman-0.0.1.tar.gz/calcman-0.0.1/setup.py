22a139683d4f1d9858d8844cb03f5f7c8a14436f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:42 +0000

    Add package calcman/0.0.1/calcman-0.0.1.tar.gz

---
+++ packages/calcman/calcman-0.0.1.tar.gz/calcman-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='calcman',
+    version='0.0.1',
+    description='This is a Python Package which will help you to calculate two numbers easily with only 2 lines of code!',
+    author='Arinjoy Nath',
+    author_email='arinjoy762@gmail.com',
+    url='',
+    packages=[],
+)
