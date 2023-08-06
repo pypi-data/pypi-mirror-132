fbdfd66617632a801e4220d0626197c15810639b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:38 +0000

    Add package optimizationalgo/0.0.9/optimizationalgo-0.0.9.tar.gz

---
+++ packages/optimizationalgo/optimizationalgo-0.0.9.tar.gz/optimizationalgo-0.0.9/optimizationalgo/__init__.py
@@ -0,0 +1,23 @@
+"""
+``optimizationalgo``
+=====================
+
+Библиотека, реализующая оптимизационные алгоритмы для задач разного рода.
+В данный момент поддерживаются алгоритмы для задачи коммовояжёра.
+Для подробной информации по этой задаче пройдите в optimizationalgo.voyage
+
+Доступные субпакеты
+---------------------
+voyage
+    Алгоритмы решения задачи коммивояжёра
+
+"""
+from .voyage import simulated_annealing
+from .voyage import ants_colony
+from .voyage import create_visual
+
+__author__ = 'Марк Козлов, Вячеслав Есаков, Артём Радайкин, Александр Савостьянов, Лев Памбухчян'
+
+__version__ = "0.0.9"
+
+__all__ = ['ants_colony', 'simulated_annealing', 'create_visual']
