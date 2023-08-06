b372b4b8b3529ebf30e52a11406b2a8db22a52b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:29 +0000

    Add package BnpInstaller/1.0.4/BnpInstaller-1.0.4-py3-none-any.whl

---
+++ packages/BnpInstaller/BnpInstaller-1.0.4-py3-none-any.whl/BnpInstaller/BnpInstaller.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import typer
+import bcml.util
+from bcml.install import install_mod
+from bcml.install import link_master_mod
+
+app = typer.Typer()
+
+@app.command()
+def install(bnp: Path, update: bool = False, remerge: bool = False):
+    print(f'Installing {bnp} . . .')
+    install_mod(mod=Path(bnp), updated=bool(update), merge_now=bool(remerge))
+    link_master_mod()
+
+if __name__ == '__main__':
+    app()
