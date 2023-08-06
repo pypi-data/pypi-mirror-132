536640d118c4bf31c7763ccf0679b8d75ba27108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:11 +0000

    Add package overlapslib/0.1.1/overlapslib-0.1.1-py3-none-any.whl

---
+++ packages/overlapslib/overlapslib-0.1.1-py3-none-any.whl/overlap_libs/over.py
@@ -0,0 +1,14 @@
+from datetime import date
+from collections import namedtuple
+
+
+def getOverlaps(startdate,enddate,stardate1,enddate1) -> date:
+    Range = namedtuple('Range', ['start', 'end'])
+    r1 = Range(start = date(startdate), end = date(enddate))
+    r2 = Range(start = date(stardate1), end = date(enddate1))
+    latest_start = max(r1.start, r2.start)
+    earliest_end = min(r1.end, r2.end)
+    delta = (earliest_end - latest_start).days + 1
+    overlap = max(0, delta)
+    if overlap != 0:
+        return "Overlaps"
---
+++ packages/overlapslib/overlapslib-0.1.1-py3-none-any.whl/overlap_libs/p.py
@@ -0,0 +1,3 @@
+from overlap_libs import over
+
+print(over(2021,11,25,2021,11,25,2021,11,25,2021,11,25))
