f99b985a8258ba55937e00eac2cac9cfbcfc2b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:12 +0000

    Add package particula/0.0.3/particula-0.0.3.tar.gz

---
+++ packages/particula/particula-0.0.3.tar.gz/particula-0.0.3/README.md
@@ -0,0 +1,17 @@
+# *PARTICULA*
+
+Particula is a simple, fast, and powerful particle simulator, or at least two of the three, we hope. It is a simple particle system that is designed to be easy to use and easy to extend. The goal is to have a robust aerosol (gas phase _and_ particle phase) simulation system that can be used to answer scientific questions that arise for experiments and research discussions.
+
+(_DISCLAIMER_ [GitHub Copilot](https://copilot.github.com/) is heavily involved in the development of this project and especially in the writing of these paragraphs! _DISCLAIMER_)
+
+(_WARNING_ `particula` is in early and messy development. It is not ready for production use yet, but you are welcome to use it anyway --- even better, you're welcome to contribute! _WARNING_)
+
+## Goals & conduct
+
+The main goal is to develop an aerosol particle model that is usable, efficient, and productive. In this process, we all will learn developing models in Python and associated packages. Let us all be friendly, respectful, and nice to each other. Any code added to this repository is automatically owned by all. Please speak up if something (even if trivial) bothers you. Talking through things always helps. This is an open-source project, so feel free to contribute, however small or big your contribution may be.
+
+We follow the Google Python style guide [here](https://google.github.io/styleguide/pyguide.html). We have contribution guidelines [here](https://github.com/uncscode/particula/blob/main/docs/CONTRIBUTING.md) and a code of conduct [here](https://github.com/uncscode/particula/blob/main/docs/CODE_OF_CONDUCT.md) as well.
+
+## Usage & development
+
+The development of this model will be illustrated through Jupyter notebooks ([here](https://github.com/uncscode/particula/blob/main/docs)) that will be put together in the form of a Jupyter book on our [website](https://uncscode.github.io/particula). You can install `particula` from PyPI or conda-forge with `pip install particula` or `conda install -c conda-forge particula`.
---
+++ packages/particula/particula-0.0.3.tar.gz/particula-0.0.3/particula/aerosol_dynamics/environment.py
@@ -0,0 +1,69 @@
+"""environment class
+"""
+
+import numpy as np
+from particula import u
+from particula.aerosol_dynamics import physical_parameters as pp
+
+
+class Environment:
+    """
+    Sets the environment class
+    with properties such as temperature and pressure
+    and derived properties such as air viscosity.
+    """
+    def __init__(self, temperature, pressure):
+        """Function calls for enviornment class."""
+        self._temperature = temperature
+        self._pressure = pressure
+
+    @u.wraps(u.K, [None])
+    def temperature(self) -> float:
+        """Returns the temperature of the environment."""
+        return self._temperature
+
+    @u.wraps(u.Pa, [None])
+    def pressure(self) -> float:
+        """Returns the pressure of the environment."""
+        return self._pressure
+
+    @u.wraps(u.kg / u.m / u.s, [None])
+    def dynamic_viscosity_air(self) -> float:
+        """Returns the dynamic viscosity of air. [kg/m/s]
+
+        The dynamic viscosity is calculated using the 3 parameter
+        Sutherland Viscosity Law.
+        """
+        mu_ref = 1.716e-5 * u.Pa * u.s  # Viscosity at T_REF
+        t_ref = 273.15 * u.K
+        suth_const = 110.4 * u.K  # Sutherland constant
+
+        return (
+            mu_ref *
+            (self.temperature()/t_ref)**(3/2) *
+            (t_ref + suth_const) / (self.temperature() + suth_const)
+        )
+
+    # mean free path of air in m
+    @u.wraps(u.m, [None])
+    def mean_free_path_air(self) -> float:
+        """Returns the mean free path of this environment. [m]
+
+        The mean free path is the average distance traveled by a molecule
+        between collisions with other molecules.
+        """
+        # Molecular weight of air in kg/mol
+        molecular_weight = (28.9644 * u.g / u.mol).to_base_units()
+
+        return (
+            (
+                2 * self.dynamic_viscosity_air()
+                / self.pressure()
+                / (
+                    8*molecular_weight
+                    / (
+                        np.pi*pp.GAS_CONSTANT*self.temperature()
+                    )
+                )**0.5
+            ).to_base_units()
+        )
