c66f462fda7d73bf2931f1845c03c5306cbdb4b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:19 +0000

    Add package mathbox/0.0.4/mathbox-0.0.4.tar.gz

---
+++ packages/mathbox/mathbox-0.0.4.tar.gz/mathbox-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mathbox',
+    python_requires='>=3.8.0',
+    version='0.0.4',
+    description="A math toolbox.",
+    author='freedeaths',
+    author_email='register917@gmail.com',
+    url='https://github.com/freedeaths/mathbox-py.git',
+    #package_dir={'': 'mathbox'},
+    packages=find_packages(exclude=["tests"]),
+    #packages=['mathbox', 'mathbox/*'],
+    install_requires=[],
+    include_package_data=True,
+    license="MIT",
+    keywords=['math', 'data-processing'],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
