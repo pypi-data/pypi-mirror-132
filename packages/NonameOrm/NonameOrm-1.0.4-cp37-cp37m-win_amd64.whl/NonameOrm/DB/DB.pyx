28d7b083a6e534f7e9691ba1cab28b11551a0cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:29 +0000

    Add package NonameOrm/1.0.4/NonameOrm-1.0.4-cp37-cp37m-win_amd64.whl

---
+++ packages/NonameOrm/NonameOrm-1.0.4-cp37-cp37m-win_amd64.whl/NonameOrm/DB/DB.pyx
@@ -0,0 +1,58 @@
+# from typing import Coroutine, Callable
+#
+from NonameOrm.Error.DBError import DBInstanceCreateError, DBInstanceError
+
+cdef public DB instance = None
+
+cdef class DB:
+    def __init__(self, *args, **kwargs):
+        if not kwargs.get('cr'):
+            raise DBInstanceCreateError()
+        self._connector = kwargs.get('connector')
+        global loop
+
+    @classmethod
+    def create(cls, *args, **kwargs):
+        if isinstance(cls.getInstance(), DB):
+            raise DBInstanceError()
+        kwargs['cr'] = True
+        global instance
+        instance = DB(cls, *args, **kwargs)
+        return instance
+
+    def GenerateTable(self):
+        self._connector.GenerateTable()
+        return self
+
+    @property
+    def connector(self):
+        return self._connector
+
+    @property
+    def execute(self):
+        if self._connector.isAsync:
+            return self.connector.asyncProcess
+        else:
+            return self.connector.process
+
+    @property
+    def executeSql(self):
+        return self.connector.execute
+
+    @property
+    def instance(self):
+        return instance
+
+    @property
+    def ConnectorType(self):
+        return self.connector.Type
+
+    @classmethod
+    def getInstance(cls):
+        return instance
+
+    @classmethod
+    def getInstanceWithCheck(cls):
+        if not instance:
+            raise
+        return instance
