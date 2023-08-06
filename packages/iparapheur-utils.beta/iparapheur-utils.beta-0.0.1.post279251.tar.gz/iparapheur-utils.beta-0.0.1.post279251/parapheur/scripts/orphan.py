c462cc98f14e13d0ac7f0f8fa8d3a91b6b217ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:00 +0000

    Add package iparapheur-utils.beta/0.0.1.post279251/iparapheur-utils.beta-0.0.1.post279251.tar.gz

---
+++ packages/iparapheur-utils.beta/iparapheur-utils.beta-0.0.1.post279251.tar.gz/iparapheur-utils.beta-0.0.1.post279251/parapheur/scripts/orphan.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# import parse
+# import isExists
+from parapheur.parapheur import pprint, parse, isExists
+from urlparse import urlparse
+from parapheur.parapheur.database import connect, execute
+import socket
+import os
+
+defaut_iparapheur_root = "/opt/iParapheur/"
+tomcat_conf_subdir = "tomcat/shared/classes/"
+alfresco_conf_file = "alfresco-global.properties"
+queries = []
+query_root = "database/queries/orphans/"
+exit_code = 0
+
+# Get data from alfresco-global.properties for connecting database
+isExists.isexistsdirectory(defaut_iparapheur_root, False)
+isExists.isexistssubdir(defaut_iparapheur_root, tomcat_conf_subdir, False)
+isExists.isexistsfile(defaut_iparapheur_root + tomcat_conf_subdir, alfresco_conf_file, False)
+alfresco_conf = parse.parse(defaut_iparapheur_root + tomcat_conf_subdir + alfresco_conf_file)
+
+# Get the dabatase connexion
+cnx = connect.connect(alfresco_conf)
+
+# Execute queries
+query_files = os.listdir(query_root)
+for file in query_files:
+    f = open(query_root + file, "r")
+    query = f.readline()
+    queries.append(query)
+queries_responses = execute.execute(cnx, queries)
+
+# Return exit code for both queries
+for response in queries_responses:
+    if response != 0:
+        exit_code = 1
+
+if exit_code == 1:
+    print(
+        "Il y a des noeuds orphelins en base de données. Vous pouvez utilisez la commande 'ph-ipclean' pour les "
+        "supprimer et générer les index")
+else:
+    print("Il n'y a pas de noeud orphelin en base de données.")
+
+exit(exit_code)
