4dd3246f3e9bb5d07cb784dc46f6f3a8144c78e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:11 +0000

    Add package bolton-typist/0.1.0/bolton-typist-0.1.0.tar.gz

---
+++ packages/bolton-typist/bolton-typist-0.1.0.tar.gz/bolton-typist-0.1.0/CHANGELOG.md
@@ -0,0 +1,22 @@
+# Changelog for `typist`
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog], and this project adheres to
+[Semantic Versioning].
+
+[Keep a Changelog]: https://keepachangelog.com/en/1.0.0/
+[Semantic Versioning]: https://semver.org/
+
+
+## [Unreleased](https://github.com/python-boltons/typist/compare/0.1.0...HEAD)
+
+No notable changes have been made.
+
+
+## [0.1.0](https://github.com/python-boltons/typist/releases/tag/0.1.0
+
+### Miscellaneous
+
+* Port `typist` library from (original) `bugyi-lib` library.
+* First Release.
---
+++ packages/bolton-typist/bolton-typist-0.1.0.tar.gz/bolton-typist-0.1.0/src/typist/_core.py
@@ -0,0 +1,58 @@
+"""The typist package's catch-all module.
+
+You should only add code to this module when you are unable to find ANY other
+module to add it to.
+"""
+
+from __future__ import annotations
+
+import datetime as dt
+from enum import Enum
+from pathlib import Path
+from typing import Any, Callable, List, NoReturn, TypeVar, Union, get_args
+
+
+C = TypeVar("C", bound=Callable)
+E = TypeVar("E", bound=Exception)
+T = TypeVar("T")
+
+DateLike = Union[str, dt.date, dt.datetime]
+PathLike = Union[str, Path]
+
+
+def assert_never(value: NoReturn) -> NoReturn:
+    """
+    Raises an AssertionError. This function can be used to achieve
+    exhaustiveness checking with mypy.
+
+    REFERENCE: https://hakibenita.com/python-mypy-exhaustive-checking
+    """
+    raise AssertionError(f"Unhandled value: {value} ({type(value).__name__})")
+
+
+def literal_to_list(
+    literal: Any,
+) -> List[Union[None, bool, bytes, int, str, Enum]]:
+    """
+    Convert a typing.Literal into a list.
+
+    Examples:
+        >>> from typing import Literal
+        >>> literal_to_list(Literal['a', 'b', 'c'])
+        ['a', 'b', 'c']
+
+        >>> literal_to_list(Literal['a', 'b', Literal['c', 'd', Literal['e']]])
+        ['a', 'b', 'c', 'd', 'e']
+
+        >>> literal_to_list(Literal['a', 'b', Literal[1, 2, Literal[None]]])
+        ['a', 'b', 1, 2, None]
+    """
+    result = []
+
+    for arg in get_args(literal):
+        if arg is None or isinstance(arg, (bool, bytes, int, str, Enum)):
+            result.append(arg)
+        else:
+            result.extend(literal_to_list(arg))
+
+    return result
