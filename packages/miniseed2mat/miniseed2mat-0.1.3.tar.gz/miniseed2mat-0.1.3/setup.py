b3c3143baa4baf41308611e46f4a7c2e18cc8ff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:34 +0000

    Add package miniseed2mat/0.1.3/miniseed2mat-0.1.3.tar.gz

---
+++ packages/miniseed2mat/miniseed2mat-0.1.3.tar.gz/miniseed2mat-0.1.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['miniseed2mat']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['obspy>=1.2.2,<2.0.0', 'scipy>=1.7.3,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['mseed2mat = miniseed2mat.main:convertmseed2mat']}
+
+setup_kwargs = {
+    'name': 'miniseed2mat',
+    'version': '0.1.3',
+    'description': 'Analyzing MiniSEED seismic data in MATLAB',
+    'long_description': None,
+    'author': 'Utpal Kumar',
+    'author_email': 'utpalkumar50@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://www.earthinversion.com/utilities/converting-mseed-data-to-mat-and-analyzing-in-matlab/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<3.11',
+}
+
+
+setup(**setup_kwargs)
