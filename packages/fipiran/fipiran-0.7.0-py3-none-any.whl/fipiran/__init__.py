aaec6d187b66fc969095b2af63e9b890a9c63fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:07 +0000

    Add package fipiran/0.7.0/fipiran-0.7.0-py3-none-any.whl

---
+++ packages/fipiran/fipiran-0.7.0-py3-none-any.whl/fipiran/__init__.py
@@ -0,0 +1,19 @@
+__version__ = '0.7.0'
+
+from requests import get as _get
+
+
+_FIPIRAN = 'https://www.fipiran.ir/'
+_YK = ''.maketrans('يك', 'یک')
+_API = 'https://fund.fipiran.ir/api/v1/'
+
+
+def _api(path) -> dict | list:
+    return _get(_API + path).json()
+
+
+def _fipiran(path: str, data=None, json_resp=False) -> str | dict | list:
+    resp = _get(f'{_FIPIRAN}{path}', data)
+    if json_resp is True:
+        return resp.json()
+    return resp.content.decode().translate(_YK)
