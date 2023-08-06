b3e1f3e4b8939836ba3075f3ebbee99d3b0e513f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:40 +0000

    Add package motivus/1.2.0/motivus-1.2.0.tar.gz

---
+++ packages/motivus/motivus-1.2.0.tar.gz/motivus-1.2.0/setup.py
@@ -0,0 +1,30 @@
+import pathlib
+import setuptools
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setuptools.setup(
+    name="motivus",
+    version="1.2.0",
+    description="Motivus client library",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://motivus.cl",
+    author="Motivus SpA",
+    author_email="info@motivus.cl",
+    license="GPLv3",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=["motivus"],
+    install_requires=["websockets", "asyncio"],
+    include_package_data=True,
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'motivus=motivus.cli:cli'
+        ]
+    }
+)
