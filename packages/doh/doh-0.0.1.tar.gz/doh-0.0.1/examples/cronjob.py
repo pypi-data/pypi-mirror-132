7bec5c746a0c25ca5e5883d4055d0ff8cda378ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:35 +0000

    Add package doh/0.0.1/doh-0.0.1.tar.gz

---
+++ packages/doh/doh-0.0.1.tar.gz/doh-0.0.1/examples/cronjob.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+"""A cronjob script to report doh daily.
+
+Cron line example:
+30 5 * * 0-4 $HOME/auto-doh/examples/cronjob.py --url URL --cookie "COOKIE" --ifttt-key KEY >> $HOME/auto-doh/examples/.cronjob.log 2>&1  # noqa: E501
+"""
+
+import argparse
+import logging
+import random
+import time
+from datetime import date
+
+import requests
+
+import cronjob_cfg_api as cron_cfg
+from doh import DohAPIClient, Report
+
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s [%(levelname)-8s] - %(message)s""",
+                    datefmt="%Y-%m-%d %H:%M:%S""")
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", required=True, help="The doh API report url")
+    parser.add_argument("--cookie", required=True,
+                        help="The doh user cookie (received after passing "
+                             "captcha)")
+    parser.add_argument("--ifttt-key",
+                        help="The personal IFTTT key is required in order to "
+                             "send notifications")
+    parser.add_argument("--sleep-time", type=int, default=600,
+                        help="The maximum amount of seconds to sleep before "
+                             "reporting (default %(default)s)")
+
+    args = parser.parse_args()
+    logging.info(f"Provided args: {args}")
+    return args
+
+
+def check_for_skip():
+    return date.today() in cron_cfg.get_skipdates(as_date=True)
+
+
+def random_sleep(max_sleep_time: int):
+    sleep_time = random.randint(0, max_sleep_time)
+    time.sleep(sleep_time)
+
+    logging.info(f"Woke up from a {sleep_time} seconds sleep")
+
+
+def notify(ifttt_key: str, message: str):
+    try:
+        response = requests.get(
+            f"https://maker.ifttt.com/trigger/Notify/with/key/{ifttt_key}?value1={message}")  # noqa
+
+    except requests.RequestException:
+        logging.info("Failed to send GET request to IFTTT", exc_info=True)
+    else:
+        if response.ok:
+            logging.info("Successfully alerted using IFTTT")
+        else:
+            logging.error("Failed to alert using IFTTT (received "
+                          f"{response.status_code}) - {response.text}")
+
+
+def main():
+    args = _parse_args()
+
+    if not cron_cfg.is_enabled():
+        logging.info("DOH is disabled by environment variable")
+        return
+
+    if check_for_skip():
+        logging.info("Skipping today")
+        return
+
+    random_sleep(args.sleep_time)
+
+    client = DohAPIClient(args.url, args.cookie)
+    response = client.report(Report.PRESENT)
+
+    if args.ifttt_key is not None:
+        notify(args.ifttt_key,
+               message=f"Report returned {response.status_code}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/doh/doh-0.0.1.tar.gz/doh-0.0.1/examples/flask_app.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""A small web application to control remotely the doh cronjob config.
+
+**Though the flask app still works, it is recommended to use the telegram bot **
+
+Use nohup to run without hangups:
+nohup ./flask_app.py >> .flask_app.log 2>&1 &
+"""
+
+import json
+import os
+from datetime import datetime
+from pathlib import Path
+
+from flask import Flask, Blueprint
+
+LOGS_PATH = Path(__file__).parent / ".cronjob.log"
+CONFIG_FILE = Path(__file__).parent / "config.json"
+
+app = Flask(__name__)
+doh_bp = Blueprint("doh", __name__, url_prefix="/doh")
+
+with open(CONFIG_FILE, "r") as fh:
+    config = json.load(fh)
+
+
+@doh_bp.route("/home")
+def home():
+    return "Home"
+
+
+@doh_bp.route("/logs")
+def logs():
+    try:
+        with open(LOGS_PATH, "r") as fh:
+            return "</br>".join(fh.readlines())
+    except FileNotFoundError:
+        return "FileNotFoundError"
+
+
+@doh_bp.route("/status")
+def status():
+    return "Enabled" if config["ENABLED"] else "Disabled"
+
+
+@doh_bp.route("/disable")
+def disable():
+    config["ENABLED"] = False
+    commit_config()
+    return "Disabled"
+
+
+@doh_bp.route("/enable")
+def enable():
+    config["ENABLED"] = True
+    commit_config()
+    return "Enabled"
+
+
+@doh_bp.route("/skip/<date>")
+def skip(date):
+    try:
+        datetime.strptime(date, "%Y%m%d")
+    except ValueError:
+        return "Failed to parse date"
+
+    config["SKIP_DAYS"].append(date)
+    commit_config()
+    return f"Will skip on {date}"
+
+
+def commit_config():
+    with open(CONFIG_FILE, "w") as fh:
+        json.dump(config, fh)
+
+
+app.register_blueprint(doh_bp)
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=int(os.environ.get("DOH_FLASK_PORT", 8050)))
