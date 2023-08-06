6e2006e3fd9925ec68978b967f310ca737ff349e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:34 +0000

    Add package ecoforesttest/0.0.2/ecoforesttest-0.0.2.tar.gz

---
+++ packages/ecoforesttest/ecoforesttest-0.0.2.tar.gz/ecoforesttest-0.0.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="ecoforesttest",
+    version="0.0.2",
+    author="",
+    author_email="",
+    description="",
+    long_description="an interpretable data-driven macroeconomic forecasting method based on N-BEATS neural network",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
