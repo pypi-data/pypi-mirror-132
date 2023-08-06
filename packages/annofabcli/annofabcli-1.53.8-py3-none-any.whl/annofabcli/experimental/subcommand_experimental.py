1d090fcb332d385197345e628ba774efe1faa227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:29 +0000

    Add package annofabcli/1.53.8/annofabcli-1.53.8-py3-none-any.whl

---
+++ packages/annofabcli/annofabcli-1.53.8-py3-none-any.whl/annofabcli/experimental/subcommand_experimental.py
@@ -0,0 +1,33 @@
+import argparse
+from typing import Optional
+
+import annofabcli
+import annofabcli.common.cli
+from annofabcli.experimental import (
+    dashboard,
+    list_labor_worktime,
+    list_labor_worktime_from_csv,
+    list_out_of_range_annotation_for_movie,
+)
+
+
+def parse_args(parser: argparse.ArgumentParser):
+    subparsers = parser.add_subparsers(dest="subcommand_name")
+
+    # サブコマンドの定義
+    dashboard.add_parser(subparsers)
+    list_labor_worktime.add_parser(subparsers)
+    list_labor_worktime_from_csv.add_parser(subparsers)
+    list_out_of_range_annotation_for_movie.add_parser(subparsers)
+
+
+def add_parser(subparsers: Optional[argparse._SubParsersAction] = None):
+    subcommand_name = "experimental"
+    subcommand_help = "アルファ版のサブコマンド"
+    description = "アルファ版のサブコマンド。予告なしに削除されたり、コマンドライン引数が変わったりします。"
+
+    parser = annofabcli.common.cli.add_parser(
+        subparsers, subcommand_name, subcommand_help, description, is_subcommand=False
+    )
+    parse_args(parser)
+    return parser
