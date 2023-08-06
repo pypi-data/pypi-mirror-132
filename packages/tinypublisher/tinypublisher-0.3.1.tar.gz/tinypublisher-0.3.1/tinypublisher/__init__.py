8277ff8a3f582462bcf800e5dfab3ecca8bf0fee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:08 +0000

    Add package tinypublisher/0.3.1/tinypublisher-0.3.1.tar.gz

---
+++ packages/tinypublisher/tinypublisher-0.3.1.tar.gz/tinypublisher-0.3.1/tinypublisher/__init__.py
@@ -0,0 +1,12 @@
+import logging
+
+__version__ = "0.3.1"
+__appname__ = 'tinypublisher'
+
+# Logger
+logger = logging.getLogger(__appname__)
+logger.setLevel(logging.INFO)
+_handler = logging.StreamHandler()
+_formatter = logging.Formatter('%(name)s.%(levelname)s: %(message)s')
+_handler.setFormatter(_formatter)
+logger.addHandler(_handler)
