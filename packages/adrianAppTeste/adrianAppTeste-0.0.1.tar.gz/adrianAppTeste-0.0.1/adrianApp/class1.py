a6af6828cb84d037d23a474f43c2fa29f19b0846
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:43:55 +0000

    Add package adrianAppTeste/0.0.1/adrianAppTeste-0.0.1.tar.gz

---
+++ packages/adrianAppTeste/adrianAppTeste-0.0.1.tar.gz/adrianAppTeste-0.0.1/adrianApp/class1.py
@@ -0,0 +1,5 @@
+class Class1:
+    def hello_world(self):
+        print('hello_world')
+    def new_method(self):
+        print('new method')
---
+++ packages/adrianAppTeste/adrianAppTeste-0.0.1.tar.gz/adrianAppTeste-0.0.1/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name='adrianAppTeste',
+    version='0.0.1',
+    author='Adrian Grosch',
+    author_email='adriangrosch15@gmail.com',
+    description='Teste',
+    long_description='Teste',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    include_package_data=True,
+)
