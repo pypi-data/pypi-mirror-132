226ee11c2348d3d13677ea955d358ef16fa9dfd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:19 +0000

    Add package Juneja/1.0/Juneja-1.0-py3-none-any.whl

---
+++ packages/Juneja/Juneja-1.0-py3-none-any.whl/Juneja/__init__.py
@@ -0,0 +1,67 @@
+import math
+import random
+
+def sum(a,b):
+    x = a+b
+    print('''Hi, This is "Ansh Juneja" From Python Community.''')
+    return x
+
+def sub(a,b):
+    x = a-b
+    print('''Hi, This is "Ansh Juneja" From Python Community.''')
+    return x
+
+def product(a,b):
+    x = a*b
+    print('''Hi, This is "Ansh Juneja" From Python Community.''')
+    return x
+
+def division(a,b):
+    x = a/b
+    print('''Hi, This is "Ansh Juneja" From Python Community.''')
+    return x
+
+def square(a):
+    x = a**2
+    print('''Hi, This is "Ansh Juneja" From Python Community.''')
+    return x
+
+def power(a,b):
+    x = math.pow(a,b)
+    print('''Hi, This is "Ansh Juneja" From Python Community.''')
+    return x
+
+def factorial(n):
+    x = math.factorial(n)
+    print('''Hi, This is "Ansh Juneja" From Python Community.''')
+    return x
+
+def Simple_Interest(a,b,c):
+    si = (a*b*c)/100
+    print('''Hi, This is "Ansh Juneja" From Python Community.''')
+    return si
+
+def fabs(n):
+    Absolute_Value = math.fabs(n)
+    print('''Hi, This is "Ansh Juneja" From Python Community.''')
+    return Absolute_Value
+
+def sqroot(n):
+    square_root = math.sqrt(n)
+    print('''Hi, This is "Ansh Juneja" From Python Community.''')
+    return square_root
+
+def randint(a,b):
+    randint = random.randint(a,b)
+    print('''Hi, This is "Ansh Juneja" From Python Community.''')
+    return randint
+
+def randrange(a,b):
+    randrange = random.randrange(a,b)
+    print('''Hi, This is "Ansh Juneja" From Python Community.''')
+    return randrange
+
+def remainder(a,b):
+    Remainder = math.fmod(a,b)
+    print('''Hi, This is "Ansh Juneja" From Python Community.''')
+    return Remainder
