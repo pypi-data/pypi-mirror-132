dc705a853535936bd0de106f8e52901724c17fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:18 +0000

    Add package headpose/1.1.6/headpose-1.1.6.tar.gz

---
+++ packages/headpose/headpose-1.1.6.tar.gz/headpose-1.1.6/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import re
+
+with open('README.md') as f:
+    readme = f.read()
+
+# extract version
+with open('headpose/__init__.py') as file:
+    for line in file.readlines():
+        m = re.match("__version__ *= *['\"](.*)['\"]", line)
+        if m:
+            version = m.group(1)
+
+setup(name='headpose',
+      version=version,
+      description='estimate the pose of the head based on an image.',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      url='https://github.com/pfriedrich-hub/headpose/headpose.git',
+      author='Ole Bialas',
+      author_email='bialas@cbs.mpg.de',
+      license='MIT',
+      python_requires='>=3.6',
+      install_requires=['opencv-python',
+                        'numpy',
+                        'requests'],
+      packages=find_packages(),
+      package_data={'headpose': ['haarcascade_frontalface_default.xml']},
+      include_package_data=True,
+      zip_safe=False)
+
+# add pypi.org/classifiers
