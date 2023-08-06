08c257109ef91d9b8304003d11f57118608b42e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:34 +0000

    Add package pedurma/0.1.69/pedurma-0.1.69-py3-none-any.whl

---
+++ packages/pedurma/pedurma-0.1.69-py3-none-any.whl/pedurma/__init__.py
@@ -0,0 +1,13 @@
+from .docx_serializer import *
+from .notes import *
+from .pagination_update import *
+from .pecha import *
+from .preprocess import *
+from .preview import *
+from .preview_note_layer import *
+from .reconstruction import *
+from .save_text import *
+from .texts import *
+from .utils import *
+
+__version__ = "0.1.69"
