a04ca86db0dd67c1193862fb68102ce1e2f33ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:15 +0000

    Add package ladybug-charts/1.9.0/ladybug-charts-1.9.0.tar.gz

---
+++ packages/ladybug-charts/ladybug-charts-1.9.0.tar.gz/ladybug-charts-1.9.0/ladybug_charts/_extend_data.py
@@ -0,0 +1,14 @@
+"""Add capability to generate a Figures from ladybug data objects. """
+
+from ladybug.datacollection import HourlyContinuousCollection, \
+    HourlyDiscontinuousCollection, MonthlyCollection, DailyCollection
+from ladybug_charts.to_figure import heat_map, monthly_bar_chart, daily_bar_chart, \
+    hourly_line_chart, per_hour_line_chart
+
+
+HourlyContinuousCollection.heat_map = heat_map
+HourlyDiscontinuousCollection.heat_map = heat_map
+MonthlyCollection.bar_chart = monthly_bar_chart
+DailyCollection.bar_chart = daily_bar_chart
+HourlyContinuousCollection.line_chart = hourly_line_chart
+HourlyContinuousCollection.per_hour_line_chart = per_hour_line_chart
---
+++ packages/ladybug-charts/ladybug-charts-1.9.0.tar.gz/ladybug-charts-1.9.0/tests/to_figure_test.py
@@ -0,0 +1,71 @@
+from plotly.graph_objects import Figure
+from ladybug_comfort.degreetime import heating_degree_time, cooling_degree_time
+from ladybug.datacollection import HourlyContinuousCollection
+from ladybug.datatype.temperaturetime import HeatingDegreeTime, CoolingDegreeTime
+from ladybug_charts.to_figure import bar_chart, wind_rose
+from ladybug.color import Color
+
+
+def test_hourly_continuous_to_heatmap(epw):
+    fig = epw.dry_bulb_temperature.heat_map()
+    assert isinstance(fig, Figure)
+
+
+def test_hourly_discontinuous_to_heatmap(epw):
+    fig = epw.dry_bulb_temperature.filter_by_conditional_statement('a>25').heat_map()
+    assert isinstance(fig, Figure)
+
+
+def test_hourly_to_per_hour_chart(epw):
+    fig = epw.dry_bulb_temperature.per_hour_line_chart()
+    assert isinstance(fig, Figure)
+
+
+def test_hourly_to_line_chart(epw):
+    fig = epw.dry_bulb_temperature.line_chart()
+    assert isinstance(fig, Figure)
+
+
+def test_monthly_to_bar_chart(epw):
+    fig = epw.dry_bulb_temperature.average_monthly().bar_chart()
+    assert isinstance(fig, Figure)
+
+
+def test_daily_to_bar_chart(epw):
+    fig = epw.dry_bulb_temperature.average_daily().bar_chart()
+    assert isinstance(fig, Figure)
+
+
+def test_bar_chart_multiple_monthly_data(epw):
+    dbt = epw.dry_bulb_temperature
+
+    _heat_base_ = 18
+    _cool_base_ = 23
+
+    hourly_heat = HourlyContinuousCollection.compute_function_aligned(
+        heating_degree_time, [dbt, _heat_base_],
+        HeatingDegreeTime(), 'degC-hours')
+    hourly_heat.convert_to_unit('degC-days')
+
+    hourly_cool = HourlyContinuousCollection.compute_function_aligned(
+        cooling_degree_time, [dbt, _cool_base_],
+        CoolingDegreeTime(), 'degC-hours')
+    hourly_cool.convert_to_unit('degC-days')
+
+    fig = bar_chart([hourly_heat.total_monthly(),
+                    hourly_cool.total_monthly()],
+                    chart_title='Degree-days',
+                    colors=[Color(255, 0, 0), Color(0, 0, 255)], stack=True)
+    assert isinstance(fig, Figure)
+
+
+def test_bar_chart_multiple_daily_data(epw):
+    dbt = epw.dry_bulb_temperature.average_daily()
+    rh = epw.relative_humidity.average_daily()
+    fig = bar_chart([dbt, rh])
+    assert isinstance(fig, Figure)
+
+
+def test_wind_rose(epw):
+    fig = wind_rose(epw.wind_speed, epw.wind_direction)
+    assert isinstance(fig, Figure)
