bbb32d127241cc1dafe62b0970457c0f0a748a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:26 +0000

    Add package funcgui/0.0.6/funcgui-0.0.6.tar.gz

---
+++ packages/funcgui/funcgui-0.0.6.tar.gz/funcgui-0.0.6/funcgui/tk.py
@@ -0,0 +1,29 @@
+# coding:utf-8
+import tkinter
+import os
+
+
+class python():
+    def __init__(self, name):
+        self.win = tkinter.Tk()
+        self.name = name
+        return('换行请用. ^ 表示。For line breaks, please use ".^" express.')
+
+    def b(self):
+        while True:
+            t1 = tkinter.StringVar()
+            tkinter.Entry(self.win, textvariable=t1, width=100).pack()
+            tkinter.Button(self.win, text='确定',
+                           command=lambda: self.a(t1)).pack()
+            self.win.mainloop()
+
+    def a(self, t1):
+        t = t1.get()
+        x = '\n'
+        for i in range(len(t)):
+            if t[i] == '.' and t[i+1] == '^':
+                t = t[:i]+x+t[i+1:]
+        with open(self.name, 'w')as p:
+            p.write(t)
+
+        os.system('python index.py')
