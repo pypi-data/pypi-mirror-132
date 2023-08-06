7aeb2beb53cdcf92f683a404968a9b076911842a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:38 +0000

    Add package k-SchoolMeal/1.0.3.Alpha/k-SchoolMeal-1.0.3.Alpha.tar.gz

---
+++ packages/k-SchoolMeal/k-SchoolMeal-1.0.3.Alpha.tar.gz/k-SchoolMeal-1.0.3.Alpha/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='k-SchoolMeal',
+    version='1.0.3 Alpha',
+
+    description='school meal data',
+    long_description = open('README.md').read(),
+    long_description_content_type = 'text/markdown',
+
+
+    author='EGGnmad',
+    author_email='viewnono1219@gmail.com',
+
+    url='https://github.com/EGGnmad/K-SchoolMeal',
+    download_url='https://github.com/EGGnmad/K-SchoolMeal',
+    install_requires = ['aiohttp', 'python-dotenv'],
+
+    packages=find_packages(),
+    keywords=['korea', 'school meal'],
+    python_requires = '>=3.7',
+)
