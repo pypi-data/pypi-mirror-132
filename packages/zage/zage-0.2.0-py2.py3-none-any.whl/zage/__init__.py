4abd10bf6cf0cbf29827880a12cd814bbad2f893
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:43 +0000

    Add package zage/0.2.0/zage-0.2.0-py2.py3-none-any.whl

---
+++ packages/zage/zage-0.2.0-py2.py3-none-any.whl/zage/__init__.py
@@ -0,0 +1,7 @@
+from zage.payments import *  # noqa
+
+# configuration variables
+public_key: str = None
+secret_key: str = None
+base_url = "https://api.zage.dev/v0/"
+sandbox_url = "https://api.sandbox.zage.dev/v0/"
