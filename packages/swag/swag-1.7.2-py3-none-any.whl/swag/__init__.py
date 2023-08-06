4d6ad08d20dc9ee4f9dbd6b7d7da2eddf0b0a390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:34 +0000

    Add package swag/1.7.2/swag-1.7.2-py3-none-any.whl

---
+++ packages/swag/swag-1.7.2-py3-none-any.whl/swag/__init__.py
@@ -0,0 +1,22 @@
+from swag.swaggy import *
+
+__all__ = [
+    "swag",
+    "black",
+    "yellow",
+    "green",
+    "red",
+    "blue",
+    "purple",
+    "white",
+    "cyan",
+    "reset",
+    "clear",
+    "colors",
+    "BOLD",
+    "UNDERLINE",
+    "BACKGROUND",
+    "INTENSE",
+    "INTENSE_BOLD",
+    "INTENSE_BACKGROUND",
+]
