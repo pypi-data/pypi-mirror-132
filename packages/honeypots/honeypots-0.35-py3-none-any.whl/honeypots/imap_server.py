479dcd6c1b89e75fb8f7f6a72b991087dbea61fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:21 +0000

    Add package honeypots/0.35/honeypots-0.35-py3-none-any.whl

---
+++ packages/honeypots/honeypots-0.35-py3-none-any.whl/honeypots/imap_server.py
@@ -0,0 +1,154 @@
+"""
+//  -------------------------------------------------------------
+//  author        Giga
+//  project       qeeqbox/honeypots
+//  email         gigaqeeq@gmail.com
+//  description   app.py (CLI)
+//  licensee      AGPL-3.0
+//  -------------------------------------------------------------
+//  contributors list qeeqbox/honeypots/graphs/contributors
+//  -------------------------------------------------------------
+"""
+
+from twisted.mail.imap4 import IMAP4Server
+from twisted.internet.protocol import Factory
+from twisted.internet import reactor
+from random import choice
+from twisted import cred
+from imaplib import IMAP4
+from subprocess import Popen
+from os import path
+from honeypots.helper import close_port_wrapper, get_free_port, kill_server_wrapper, server_arguments, setup_logger, disable_logger, set_local_vars, check_if_server_is_running
+from uuid import uuid4
+
+
+class QIMAPServer():
+    def __init__(self, ip=None, port=None, username=None, password=None, mocking=False, config=''):
+        self.auto_disabled = None
+        self.mocking = mocking or ''
+        self.random_servers = [b'OK Microsoft Exchange Server 2003 IMAP4rev1 server version 6.5.6944.0 DC9 ready']
+        self.process = None
+        self.uuid = 'honeypotslogger' + '_' + __class__.__name__ + '_' + str(uuid4())[:8]
+        self.ip = None
+        self.port = None
+        self.username = None
+        self.password = None
+        self.config = config
+        if config:
+            self.logs = setup_logger(self.uuid, config)
+            set_local_vars(self, config)
+        else:
+            self.logs = setup_logger(self.uuid, None)
+        self.ip = ip or self.ip or '0.0.0.0'
+        self.port = port or self.port or 143
+        self.username = username or self.username or 'test'
+        self.password = password or self.password or 'test'
+
+    def imap_server_main(self):
+
+        _q_s = self
+
+        class CustomIMAP4Server(IMAP4Server):
+
+            def check_bytes(self, string):
+                if isinstance(string, bytes):
+                    return string.decode()
+                else:
+                    return str(string)
+
+            def connectionMade(self):
+                _q_s.logs.info(["servers", {'server': 'imap_server', 'action': 'connection', 'ip': self.transport.getPeer().host, 'port': self.transport.getPeer().port}])
+
+                if isinstance(_q_s.mocking, bool):
+                    if _q_s.mocking == True:
+                        self.sendPositiveResponse(message=choice(_q_s.random_servers))
+                elif isinstance(_q_s.mocking, str):
+                    self.sendPositiveResponse(message=choice(_q_s.random_servers))
+                else:
+                    self.sendPositiveResponse(message=b'Welcome')
+
+            def authenticateLogin(self, user, passwd):
+                user = self.check_bytes(user)
+                passwd = self.check_bytes(user)
+
+                if user == _q_s.username and passwd == _q_s.password:
+                    _q_s.logs.info(["servers", {'server': 'imap_server', 'action': 'login', 'status': 'success', 'ip': self.transport.getPeer().host, 'port': self.transport.getPeer().port, 'username': _q_s.username, 'password': _q_s.password}])
+                else:
+                    _q_s.logs.info(["servers", {'server': 'imap_server', 'action': 'login', 'status': 'failed', 'ip': self.transport.getPeer().host, 'port': self.transport.getPeer().port, 'username': user, 'password': passwd}])
+
+                raise cred.error.UnauthorizedLogin()
+
+            def lineReceived(self, line):
+                try:
+                    _line = line.split(b" ")[1]
+                    if _line.lower().startswith(b"login") or _line.lower().startswith(b"capability"):
+                        IMAP4Server.lineReceived(self, line)
+                except BaseException:
+                    pass
+
+        class CustomIMAPFactory(Factory):
+            protocol = CustomIMAP4Server
+            portal = None
+
+            def buildProtocol(self, address):
+                p = self.protocol()
+                p.portal = self.portal
+                p.factory = self
+                return p
+
+        factory = CustomIMAPFactory()
+        reactor.listenTCP(port=self.port, factory=factory, interface=self.ip)
+        reactor.run()
+
+    def run_server(self, process=False, auto=False):
+        status = 'error'
+        run = False
+        if process:
+            if auto and not self.auto_disabled:
+                port = get_free_port()
+                if port > 0:
+                    self.port = port
+                    run = True
+            elif self.close_port() and self.kill_server():
+                run = True
+
+            if run:
+                self.process = Popen(['python3', path.realpath(__file__), '--custom', '--ip', str(self.ip), '--port', str(self.port), '--username', str(self.username), '--password', str(self.password), '--mocking', str(self.mocking), '--config', str(self.config), '--uuid', str(self.uuid)])
+                if self.process.poll() is None and check_if_server_is_running(self.uuid):
+                    status = 'success'
+
+            self.logs.info(["servers", {'server': 'imap_server', 'action': 'process', 'status': status, 'ip': self.ip, 'port': self.port, 'username': self.username, 'password': self.password}])
+
+            if status == 'success':
+                return True
+            else:
+                self.kill_server()
+                return False
+        else:
+            self.imap_server_main()
+
+    def test_server(self, ip=None, port=None, username=None, password=None):
+        try:
+            _ip = ip or self.ip
+            _port = port or self.port
+            _username = username or self.username
+            _password = password or self.password
+            imap_test = IMAP4(_ip, _port)
+            imap_test.login(_username, _password)
+        except BaseException:
+            pass
+
+    def close_port(self):
+        ret = close_port_wrapper('imap_server', self.ip, self.port, self.logs)
+        return ret
+
+    def kill_server(self):
+        ret = kill_server_wrapper('imap_server', self.uuid, self.process)
+        return ret
+
+
+if __name__ == '__main__':
+    parsed = server_arguments()
+    if parsed.docker or parsed.aws or parsed.custom:
+        qimapserver = QIMAPServer(ip=parsed.ip, port=parsed.port, username=parsed.username, password=parsed.password, mocking=parsed.mocking, config=parsed.config)
+        qimapserver.run_server()
