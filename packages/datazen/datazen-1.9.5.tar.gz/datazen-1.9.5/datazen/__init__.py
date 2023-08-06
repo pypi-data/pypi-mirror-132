dd171d0a654657ce184f2c3fafb1a48d34e0cc08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:18 +0000

    Add package datazen/1.9.5/datazen-1.9.5.tar.gz

---
+++ packages/datazen/datazen-1.9.5.tar.gz/datazen-1.9.5/datazen/__init__.py
@@ -0,0 +1,28 @@
+# =====================================
+# generator=datazen
+# version=1.9.5
+# hash=9f9deac2b468030dc4a11c70ba3af25b
+# =====================================
+
+"""
+datazen - Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "Compile and render schema-validated configuration data."
+PKG_NAME = "datazen"
+VERSION = "1.9.5"
+
+DEFAULT_TYPE = "yaml"
+DEFAULT_MANIFEST = f"manifest.{DEFAULT_TYPE}"
+DEFAULT_DIR = f"{PKG_NAME}-out"
+CACHE_SUFFIX = "_cache"
+GLOBAL_KEY = "global"
+
+
+def to_private(input_str: str) -> str:
+    """Convert some input to a 'private' String."""
+
+    return f"__{input_str}__"
+
+
+ROOT_NAMESPACE = to_private("root")
