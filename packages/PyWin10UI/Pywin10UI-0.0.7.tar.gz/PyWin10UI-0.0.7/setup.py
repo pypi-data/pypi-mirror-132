0cdb8e66e2c6c173a3943ace77b45d307031f752
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:08 +0000

    Add package PyWin10UI/0.0.7/Pywin10UI-0.0.7.tar.gz

---
+++ packages/PyWin10UI/Pywin10UI-0.0.7.tar.gz/PyWin10UI-0.0.7/setup.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+from setuptools import setup
+
+setup(name='PyWin10UI',
+      version='0.0.7',
+      description='2021/12/30',
+      url='https://github.com/bytfr/pywin10ui',
+      author='bytfr 啃竹子',
+      author_email='2678509244@qq.com',
+      license='MIT',
+      packages=['pywin10ui'])
