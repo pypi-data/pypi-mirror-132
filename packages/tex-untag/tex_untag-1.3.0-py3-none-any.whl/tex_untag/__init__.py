d49ffcdbf3917aab9b688fb0dc383a41438c6120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:00 +0000

    Add package tex-untag/1.3.0/tex_untag-1.3.0-py3-none-any.whl

---
+++ packages/tex-untag/tex_untag-1.3.0-py3-none-any.whl/tex_untag/__init__.py
@@ -0,0 +1,14 @@
+"""A small script for removing markup tags from sets of TeX files.
+
+This module includes two main public functions for removing markup tags from
+TeX documents:
+    untag_file -- removes tags from a file or list of files
+    untag_folder -- recursively removes tags from all files in a directory
+
+The removed tags are assumed to use the standard "\tag{...}" format, and the
+functions that accept a tag string as an argument expect the exact text
+falling between the "\" and the "{".
+"""
+
+from ._version import __author__, __version__, _author_email, _copyright_year
+from .tex_untag import *
