b6bef1321b2fde5d5455f17e0a2766b4f733f325
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:33 +0000

    Add package dbt-cloud-cli/0.2.1/dbt-cloud-cli-0.2.1.tar.gz

---
+++ packages/dbt-cloud-cli/dbt-cloud-cli-0.2.1.tar.gz/dbt-cloud-cli-0.2.1/dbt_cloud/account.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+
+class DbtCloudAPI(BaseModel):
+    api_token: str
+
+
+class DbtCloudAccount(DbtCloudAPI):
+    account_id: int
+
+    def get_api_url(self, api_version: str = "v2") -> str:
+        return f"https://cloud.getdbt.com/api/{api_version}/accounts/{self.account_id}"
