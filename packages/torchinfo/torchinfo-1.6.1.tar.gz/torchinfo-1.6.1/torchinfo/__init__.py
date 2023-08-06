9c311197d025bf209c3deb74d4b415c396ee2c01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:54 +0000

    Add package torchinfo/1.6.1/torchinfo-1.6.1.tar.gz

---
+++ packages/torchinfo/torchinfo-1.6.1.tar.gz/torchinfo-1.6.1/torchinfo/__init__.py
@@ -0,0 +1,7 @@
+""" torchinfo """
+from .enums import ColumnSettings, RowSettings, Verbosity
+from .model_statistics import ModelStatistics
+from .torchinfo import summary
+
+__all__ = ("summary", "ColumnSettings", "ModelStatistics", "RowSettings", "Verbosity")
+__version__ = "1.6.1"
