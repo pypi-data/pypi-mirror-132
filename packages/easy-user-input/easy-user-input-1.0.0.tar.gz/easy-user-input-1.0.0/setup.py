c0223407a0f447e1607f5f96df367908f5388d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:52 +0000

    Add package easy-user-input/1.0.0/easy-user-input-1.0.0.tar.gz

---
+++ packages/easy-user-input/easy-user-input-1.0.0.tar.gz/easy-user-input-1.0.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['easy_user_input']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'easy-user-input',
+    'version': '1.0.0',
+    'description': 'Misc python3 methods for collecting input from users',
+    'long_description': '# easy-user-input\nMiscellaneous python3 methods for collecting input from users\n\nCreated for use in a different project.\n',
+    'author': 'generic-user1',
+    'author_email': '89677116+generic-user1@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
