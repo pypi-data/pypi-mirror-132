fa9aa677779d5a9157f94309b24a0a0aecfd431a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:43 +0000

    Add package crawlio/2.3.0/crawlio-2.3.0-py3-none-any.whl

---
+++ packages/crawlio/crawlio-2.3.0-py3-none-any.whl/crawlio/__init__.py
@@ -0,0 +1,9 @@
+"""
+Author: Maximilian Wolf (max@w0lf.me)
+License: GPLv3
+"""
+
+
+__version__ = '2.3.0'
+
+from .crawlio import Crawler, Selector
---
+++ packages/crawlio/crawlio-2.3.0-py3-none-any.whl/crawlio/web/app.py
@@ -0,0 +1,34 @@
+from typing import Dict, List
+
+import uvicorn as uvicorn
+from fastapi import FastAPI, Body
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+from pydantic.networks import AnyHttpUrl
+
+from crawlio import Crawler, Selector
+
+
+app = FastAPI()
+
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+
+@app.get("/")
+async def index():
+    return FileResponse('index.html')
+
+
+@app.post('/api/crawl')
+async def crawl(
+        url: AnyHttpUrl = Body(...),
+        selectors: List[Selector] = Body(None, description='List of `crawlio.Selector` instances'),
+        delay: float = Body(.3, description='Max. download delay')
+):
+    crawler = Crawler(url, selectors, delay)
+    pages = await crawler.run()
+    return pages
+
+
+if __name__ == '__main__':
+    uvicorn.run(app)
