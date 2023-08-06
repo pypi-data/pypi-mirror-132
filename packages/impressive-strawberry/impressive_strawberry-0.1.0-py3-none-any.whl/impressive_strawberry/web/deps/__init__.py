3fbc7fda77bca8750a4ce3ce37531809007314fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:16 +0000

    Add package impressive-strawberry/0.1.0/impressive_strawberry-0.1.0-py3-none-any.whl

---
+++ packages/impressive-strawberry/impressive_strawberry-0.1.0-py3-none-any.whl/impressive_strawberry/web/deps/__init__.py
@@ -0,0 +1,19 @@
+"""
+This package contains all usable dependencies of :mod:`impressive_strawberry`.
+"""
+
+from .achievement import *
+from .application import *
+from .database import *
+from .group import *
+from .unlock import *
+from .user import *
+
+__all__ = (
+    "achievement",
+    "application",
+    "database",
+    "group",
+    "user",
+    "unlock",
+)
