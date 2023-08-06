d413832d4da0d46282c485f4cf6d529f2efc2100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:41 +0000

    Add package goolog/0.0.2/goolog-0.0.2.tar.gz

---
+++ packages/goolog/goolog-0.0.2.tar.gz/goolog-0.0.2/README.md
@@ -0,0 +1,2 @@
+# goolog
+This is local colorful log printer. It prints colorful message for different log level.
---
+++ packages/goolog/goolog-0.0.2.tar.gz/goolog-0.0.2/setup.py
@@ -0,0 +1,39 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+dev_requires = [
+    'build==0.5.1',
+    'twine>=3.4.2,<4',
+    'pip-tools>=5,<6',
+    'pytest>=6.2.4,<6.3'
+]
+
+setuptools.setup(
+    name="goolog",
+    version="0.0.2",
+    author="YiXiaoCuoHuaiFenZi",
+    author_email="249664317@qq.com",
+    description="colorful printer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/YiXiaoCuoHuaiFenZi/goolog",
+    project_urls={
+        "Bug Tracker": "https://github.com/YiXiaoCuoHuaiFenZi/goolog/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[],
+    setup_requires=[],
+    tests_require=[],
+    extras_require={
+        "dev": dev_requires
+    },
+)
