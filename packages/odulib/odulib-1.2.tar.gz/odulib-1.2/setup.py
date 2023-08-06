df04534fdd5627c8830767ccbd7889b544d3e36a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:07 +0000

    Add package odulib/1.2/odulib-1.2.tar.gz

---
+++ packages/odulib/odulib-1.2.tar.gz/odulib-1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+long_description= '''Python module for ODU tasks'''
+setup(name='odulib',
+      version='1.2',
+      url='https://github.com/deaflurryth/odulib',
+      license='MIT',
+      author='deaflurryth',
+      author_email='deaflurryth@icloud.com',
+      description='Add static script_dir() method to Path',
+      zip_safe=False)
