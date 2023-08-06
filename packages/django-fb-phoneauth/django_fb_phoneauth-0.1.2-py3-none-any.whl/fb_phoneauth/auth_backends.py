ad76e9d0e238c57c4d85ba11a48da26550fbce19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:16 +0000

    Add package django-fb-phoneauth/0.1.2/django_fb_phoneauth-0.1.2-py3-none-any.whl

---
+++ packages/django-fb-phoneauth/django_fb_phoneauth-0.1.2-py3-none-any.whl/fb_phoneauth/auth_backends.py
@@ -0,0 +1,91 @@
+import logging
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+from django.core.exceptions import ImproperlyConfigured
+from django.conf import settings
+import firebase_admin
+from firebase_admin.auth import (
+    verify_id_token,
+    get_user,
+)
+
+logger = logging.getLogger(__name__)
+
+FIREBASE_SKIP_VERIFY_TOKEN = getattr(
+    settings, 'FIREBASE_SKIP_VERIFY_TOKEN', False)
+
+if FIREBASE_SKIP_VERIFY_TOKEN:
+    fireBaseApp = firebase_admin.initialize_app()
+
+User = get_user_model()
+PHONENUMBER_FIELD = getattr(settings, 'FB_PHONENUMBER_FIELD', 'phone_number')
+
+
+class FbPhoneAuthBackend(ModelBackend):
+    """
+    Custom auth backend that uses an phone number and token got from firebase
+    For this to work, the User model must have an 'phone_number' field
+    """
+
+    def _authenticate(self, request, phone_number=None, token=None, *args, **kwargs):
+
+        logger.debug(
+            (F'Phone authenticate :{phone_number}')
+        )
+        if phone_number is None:
+            if 'username' not in kwargs or kwargs['username'] is None:
+                return None
+            clean_phone_number = kwargs['username']
+        else:
+            clean_phone_number = phone_number
+
+        # Check if we're dealing with an international formatted number
+        if '+' not in clean_phone_number:
+            logger.debug(
+                (F'A non international number {clean_phone_number}')
+            )
+            return None
+        if not FIREBASE_SKIP_VERIFY_TOKEN:
+            decoded_token = verify_id_token(
+                token, app=fireBaseApp, check_revoked=True)
+
+            uid = decoded_token['uid']
+
+            user = get_user(uid, app=fireBaseApp)
+            user_phone_number = user.phone_number  # NOTE: This is FireBase user Obj
+
+            if phone_number != user_phone_number:
+                logger.warning(
+                    (f'phone_number ({phone_number}) from client does not match with'
+                     f' phone_number ({user_phone_number}) from firebase user obj')
+                )
+                return None
+
+        filter_kwargs = {PHONENUMBER_FIELD: phone_number}
+        matching_users = User.objects.filter(**filter_kwargs)
+
+        if len(matching_users) > 1:
+            # This is the problem scenario where we have multiple users
+            logger.error(
+                (f'Multiple users with the given phone_number ({phone_number})')
+            )
+            raise User.MultipleObjectsReturned(
+                "There are multiple users with the given phone number")
+        elif len(matching_users) < 1:
+            logger.info(
+                (f'Create a new user with the given phone_number ({phone_number})')
+            )
+            db_user = User.objects.create(**filter_kwargs)
+        else:
+            db_user = matching_users[0]
+
+        if self.user_can_authenticate(db_user):
+            return db_user
+        else:
+            logger.warning(
+                (f'Inactive user with the phone_number ({phone_number}) is trying to login')
+            )
+        return None
+
+    def authenticate(self, *args, **kwargs):
+        return self._authenticate(*args, **kwargs)
---
+++ packages/django-fb-phoneauth/django_fb_phoneauth-0.1.2-py3-none-any.whl/fb_phoneauth/tests.py
@@ -0,0 +1,72 @@
+import os
+import time
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.chrome import options
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from django.conf import settings
+
+
+class AccountTestCase(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        HEADLESS = os.getenv('HEADLESS', 'True') == 'True'
+        SELENIUM_EXPLICIT_WAIT = int(os.getenv('SELENIUM_EXPLICIT_WAIT', '60'))
+        options = Options()
+        if HEADLESS:
+            options.add_argument("--window-size=1920,1080")
+            options.add_argument("--start-maximized")
+            options.add_argument("--headless")
+        cls.selenium = WebDriver(options=options)
+        cls.selenium.implicitly_wait(10)
+        cls.wait = WebDriverWait(cls.selenium, SELENIUM_EXPLICIT_WAIT)
+        cls.old_settings = settings
+        settings.FIREBASE_TESTING_MODE = True
+        settings.FIREBASE_SKIP_VERIFY_TOKEN = True
+        super().setUpClass()
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.selenium.quit()
+        settings.FIREBASE_TESTING_MODE = cls.old_settings.FIREBASE_TESTING_MODE
+        settings.FIREBASE_SKIP_VERIFY_TOKEN = cls.old_settings.FIREBASE_SKIP_VERIFY_TOKEN
+        super().tearDownClass()
+
+    def test_login(self):
+        num_code = (
+            ('9999999999', '999999'),
+            ('9999999997', '999997'),
+        )
+        for (number, code) in num_code:
+            self.login_test(number, code)
+
+    def login_test(self, number, code):
+        self.selenium.get('%s%s' % (self.live_server_url,
+                          '/phone_login/?next=/phone_login/__test__/'))
+        self.wait.until(EC.visibility_of_element_located(
+            (By.XPATH, "//div[@class='iti__selected-flag']")))
+        time.sleep(2)
+        phone_number = self.selenium.find_element_by_id('id_phone_number')
+        otp = self.selenium.find_element_by_id('verification-code')
+        signinbutton = self.selenium.find_element_by_id('sign-in-button')
+        verbutton = self.selenium.find_element_by_id('verify-code-button')
+        self.slow_send_keys(phone_number, number)
+        signinbutton.click()
+        time.sleep(1)
+        self.slow_send_keys(otp, code)
+        verbutton.click()
+        time.sleep(2)
+        timeout = 20
+        WebDriverWait(self.selenium, timeout).until(
+            lambda driver: driver.find_element_by_tag_name('body').text == 'Success')
+
+    def slow_send_keys(self, obj, string):
+        for chr in string:
+            obj.send_keys(chr)
+            time.sleep(0.1)
