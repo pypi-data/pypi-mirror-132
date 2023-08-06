4901d5a83770c393c96267961c955e5b34d42ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:37 +0000

    Add package example-pkg-naturalett/1.2/example-pkg-naturalett-1.2.tar.gz

---
+++ packages/example-pkg-naturalett/example-pkg-naturalett-1.2.tar.gz/example-pkg-naturalett-1.2/setup.py
@@ -0,0 +1,13 @@
+import setuptools, os
+
+setuptools.setup(
+    name="example-pkg-naturalett",  # Replace with your own username
+    version=os.environ.get("BUILD_VERSION", None),
+    author="Foo Bar",
+    author_email="lidor.ettinger@example.com",
+    description="A small example package",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.6',
+)
