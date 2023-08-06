ee98fd61ec2b257df449707903da3c9103e2418c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:22 +0000

    Add package types-chardet/4.0.2/types-chardet-4.0.2.tar.gz

---
+++ packages/types-chardet/types-chardet-4.0.2.tar.gz/types-chardet-4.0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+name = "types-chardet"
+description = "Typing stubs for chardet"
+long_description = '''
+## Typing stubs for chardet
+
+This is a PEP 561 type stub package for the `chardet` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `chardet`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/chardet. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `22bf9e8a787b38b1a0e193edd5b47119e43286ca`.
+'''.lstrip()
+
+setup(name=name,
+      version="4.0.2",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      install_requires=[],
+      packages=['chardet-stubs'],
+      package_data={'chardet-stubs': ['__init__.pyi', 'enums.pyi', 'langbulgarianmodel.pyi', 'langcyrillicmodel.pyi', 'langgreekmodel.pyi', 'langhebrewmodel.pyi', 'langhungarianmodel.pyi', 'langthaimodel.pyi', 'langturkishmodel.pyi', 'universaldetector.pyi', 'version.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Typing :: Typed",
+      ]
+)
