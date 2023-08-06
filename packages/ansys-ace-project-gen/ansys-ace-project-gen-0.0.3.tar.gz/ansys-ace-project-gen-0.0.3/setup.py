fcdb95d64bc9fd10aafb06a5cf14ada9e6ae1cf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:48 +0000

    Add package ansys-ace-project-gen/0.0.3/ansys-ace-project-gen-0.0.3.tar.gz

---
+++ packages/ansys-ace-project-gen/ansys-ace-project-gen-0.0.3.tar.gz/ansys-ace-project-gen-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+CLASSIFIERS = [
+    "Development Status :: 1 - Planning",
+    "Intended Audience :: Developers",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3",
+]
+
+setup(name='ansys-ace-project-gen',
+      version='0.0.3',
+      url='',
+      license='GNU-GPL',
+      author='Babacar FALL',
+      author_email='babacar.fall@ansys.com',
+      entry_points={"console_scripts": ["ansys-ace-project-gen = createaceproject.main:cli"]},
+      description='Ansys ACE Project Generator',
+      keywords=['python', 'ansys', 'ace'],
+      packages=find_packages(),
+      install_requires=['easygui'],
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
+      zip_safe=False,
+      classifiers=CLASSIFIERS,
+ )
