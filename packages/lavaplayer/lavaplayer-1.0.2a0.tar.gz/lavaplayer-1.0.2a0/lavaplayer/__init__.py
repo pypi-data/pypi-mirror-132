814a35f2c425891f6ee6372d7e1a25414abea311
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:02 +0000

    Add package lavaplayer/1.0.2a0/lavaplayer-1.0.2a0.tar.gz

---
+++ packages/lavaplayer/lavaplayer-1.0.2a0.tar.gz/lavaplayer-1.0.2a0/lavaplayer/__init__.py
@@ -0,0 +1,16 @@
+"""
+Lavalink connecter
+~~~~~~~~~~~~~~~
+:copyright: (c) 2021 HazemMeqdad
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'lavaplayer'
+__author__ = 'HazemMeqdad'
+__license__ = 'MIT'
+
+
+from .client import LavalinkClient
+from .objects import *
+from .api import Api
+from .exceptions import *
