2f482fecb39616f5c891a374b4af9247cb7a9851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:35 +0000

    Add package lrng/1.0.1/lrng-1.0.1-py3-none-any.whl

---
+++ packages/lrng/lrng-1.0.1-py3-none-any.whl/lrng/__init__.py
@@ -0,0 +1,10 @@
+from .lrng import LabeledRange, LabeledRanges
+from .numba import coalesce, label_range, relevant_labels
+name = 'lrng'
+
+major = 1
+minor = 0
+patch = 1
+
+version = '{}.{}.{}'.format(major, minor, patch)
+description = 'Labeled Ranges'
