680119add6d02630c4c61d4bb5ec673b1d8f9850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:29 +0000

    Add package python-clap/0.5.0/python_clap-0.5.0-py3-none-any.whl

---
+++ packages/python-clap/python_clap-0.5.0-py3-none-any.whl/clap/__init__.py
@@ -0,0 +1,31 @@
+"""(C)ommand (L)ine (A)rgument (P)arsing.
+
+A thin wrapper around the standard Python argparse library.
+"""
+
+import logging as _logging
+
+from ._core import (
+    Config,
+    MainType,
+    NewCommand,
+    Parser,
+    main_factory,
+    new_command_factory,
+)
+
+
+__all__ = [
+    "Config",
+    "MainType",
+    "NewCommand",
+    "Parser",
+    "main_factory",
+    "new_command_factory",
+]
+
+__author__ = "Bryan M Bugyi"
+__email__ = "bryanbugyi34@gmail.com"
+__version__ = "0.5.0"
+
+_logging.getLogger(__name__).addHandler(_logging.NullHandler())
