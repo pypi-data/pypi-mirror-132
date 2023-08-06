bd49f0a6a916d94f694a25a6d837f96760d9d1df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:09 +0000

    Add package django-admin-inline-paginator/0.3.0/django-admin-inline-paginator-0.3.0.tar.gz

---
+++ packages/django-admin-inline-paginator/django-admin-inline-paginator-0.3.0.tar.gz/django-admin-inline-paginator-0.3.0/django_admin_inline_paginator/tests/admin_unit_test.py
@@ -0,0 +1,45 @@
+import unittest
+from django.contrib.admin.views.main import ChangeList
+
+from django_admin_inline_paginator.admin import InlineChangeList, PaginationFormSetBase
+
+
+class TestInlineChangeList(unittest.TestCase):
+
+    def test_default_values(self):
+        self.assertEqual(InlineChangeList.can_show_all, True)
+        self.assertEqual(InlineChangeList.multi_page, True)
+        self.assertEqual(InlineChangeList.get_query_string, ChangeList.__dict__['get_query_string'])
+
+    def test_init_values(self):
+        pass
+    #     cl = InlineChangeList(request, page_num, paginator)
+    #     cl.page_num = page_num
+    #     cl.paginator = paginator
+    #     cl.result_count = paginator.count
+
+    #     cl.show_all = 'all' in request.GET
+    #     cl.params = dict(request.GET.items())
+
+
+class TestPaginationFormSetBase(unittest.TestCase):
+
+    def test_default_values(self):
+        self.assertEqual(PaginationFormSetBase.queryset, None)
+        self.assertEqual(PaginationFormSetBase.request, None)
+        self.assertEqual(PaginationFormSetBase.per_page, 20)
+
+    def test_get_page_num(self):
+        pass
+
+    def test_get_page(self):
+        pass
+
+    def test_mount_paginator(self):
+        pass
+
+    def test_mount_queryset(self):
+        pass
+
+    def test_init(self):
+        pass
