d60448c5bd29ad840b95511416ddba6b6a6d7bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:32 +0000

    Add package timeranges/0.7.1/timeranges-0.7.1.tar.gz

---
+++ packages/timeranges/timeranges-0.7.1.tar.gz/timeranges-0.7.1/src/timeranges/__init__.py
@@ -0,0 +1,8 @@
+"""Time ranges."""
+
+__version__ = "0.7.1"
+
+from ._timeranges import TimeRange, TimeRanges, WeekRange
+
+# TODO Maybe generate it programatically?
+__all__ = ["TimeRange", "TimeRanges", "WeekRange"]
