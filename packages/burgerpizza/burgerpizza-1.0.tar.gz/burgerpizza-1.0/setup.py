f8fc34bf0e6d865e5a737d7e520b641a4a88c032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:11 +0000

    Add package burgerpizza/1.0/burgerpizza-1.0.tar.gz

---
+++ packages/burgerpizza/burgerpizza-1.0.tar.gz/burgerpizza-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='burgerpizza',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='license.txt',
+    author='Ibrahim biryani zaheer',
+    author_email='zaheeribrahim346@gmail.com',
+    description='this is a file that icreated'
+)
