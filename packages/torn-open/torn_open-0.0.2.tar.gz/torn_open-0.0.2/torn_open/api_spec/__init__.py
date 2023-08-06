7e6125383b6afe9ae72c1aee89d84e7cac4d0019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:04 +0000

    Add package torn-open/0.0.2/torn_open-0.0.2.tar.gz

---
+++ packages/torn-open/torn_open-0.0.2.tar.gz/torn_open-0.0.2/torn_open/api_spec/__init__.py
@@ -0,0 +1,10 @@
+from torn_open.api_spec.decorators import tags, summary
+from torn_open.api_spec.plugin import TornOpenPlugin
+from torn_open.api_spec.core import TornOpenAPISpec
+
+__all__ = [
+    "tags",
+    "summary",
+    "TornOpenAPISpec",
+    "TornOpenPlugin",
+]
