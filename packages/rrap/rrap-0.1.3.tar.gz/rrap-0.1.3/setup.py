9737891517960c1349ab3fd001d845bf20a9bdee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:17 +0000

    Add package rrap/0.1.3/rrap-0.1.3.tar.gz

---
+++ packages/rrap/rrap-0.1.3.tar.gz/rrap-0.1.3/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['rrap']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['joblib>=0.16.0,<0.17.0',
+ 'matplotlib>=3.0.3,<4.0.0',
+ 'numpy>=1.19.1,<2.0.0',
+ 'scikit-learn>=0.23.1,<0.24.0',
+ 'scipy>=1.5.2,<2.0.0',
+ 'sklearn>=0.0,<0.1',
+ 'threadpoolctl>=2.1.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'rrap',
+    'version': '0.1.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.3,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
