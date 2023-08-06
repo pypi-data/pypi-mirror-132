b4c7c5b7687af322c14f00932548827b7a2c7fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:45 +0000

    Add package wudaoai/1.0.7/wudaoai-1.0.7.tar.gz

---
+++ packages/wudaoai/wudaoai-1.0.7.tar.gz/wudaoai-1.0.7/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    # 包信息
+    name="wudaoai",
+    version="1.0.7",
+    author="yufeng",
+    author_email="zhuyufeng@wudaoai.com",
+    license='MIT',
+    description="超大规模中文预训练模型",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wjx2zuoshi/wudaoAI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests',
+    ],
+    # 包搜集
+
+)
