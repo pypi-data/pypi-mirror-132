3ce4028efdf5d63cd2b7f12aaa8184a4ea4d7653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:25 +0000

    Add package e2j2/0.7.1/e2j2-0.7.1-py3-none-any.whl

---
+++ packages/e2j2/e2j2-0.7.1-py3-none-any.whl/e2j2/tags/json_tag.py
@@ -0,0 +1,16 @@
+import json
+import re
+from json.decoder import JSONDecodeError
+
+from e2j2.exceptions import E2j2Exception
+
+
+def parse(json_string):
+    try:
+        # handle hashrocket styled json
+        if re.search(r'"\s*=>\s*["{[]', json_string):
+            return json.loads(re.sub(r'"\s*=>\s*', '":', json_string))
+        else:
+            return json.loads(json_string)
+    except JSONDecodeError:
+        raise E2j2Exception("invalid JSON")
