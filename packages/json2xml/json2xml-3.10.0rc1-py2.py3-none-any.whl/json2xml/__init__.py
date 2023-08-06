34c92c222da20dc28e2ff0e3a25525e863dfbcf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:11:10 +0000

    Add package json2xml/3.10.0rc1/json2xml-3.10.0rc1-py2.py3-none-any.whl

---
+++ packages/json2xml/json2xml-3.10.0rc1-py2.py3-none-any.whl/json2xml/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for json2xml."""
+
+__author__ = """Vinit Kumar"""
+__email__ = "mail@vinitkumar.me"
+__version__ = "3.10.0rc1"
+
+
+# from .utils import readfromurl, readfromstring, readfromjson
