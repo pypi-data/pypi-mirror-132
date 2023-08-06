f2deab03e9c0cd939dd83d91c19f76de8cef11d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:03 +0000

    Add package json-cpp/1.0.2/json_cpp-1.0.2.tar.gz

---
+++ packages/json-cpp/json_cpp-1.0.2.tar.gz/json_cpp-1.0.2/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='json_cpp',description='a better json library',author='german espinosa',author_email='germanespinosa@gmail.com',packages=['json-cpp'],install_requires=['requests'],license='MIT',version='1.0.2',zip_safe=False)
