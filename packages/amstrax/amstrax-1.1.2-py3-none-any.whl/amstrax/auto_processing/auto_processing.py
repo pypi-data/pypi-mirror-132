fbd10bad242dfc2012845c0dc68849886c043564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:34 +0000

    Add package amstrax/1.1.2/amstrax-1.1.2-py3-none-any.whl

---
+++ packages/amstrax/amstrax-1.1.2-py3-none-any.whl/amstrax/auto_processing/auto_processing.py
@@ -0,0 +1,59 @@
+import argparse
+import time
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Autoprocess xams(l) data',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '--target',
+        default='raw_records_v1724',
+        help="Name of context to use")
+    parser.add_argument(
+        '--timeout',
+        default=60,
+        type=int,
+        help="Sleep this many seconds in between")
+    parser.add_argument(
+        '--max_jobs',
+        default=None,
+        type=int,
+        help="Max number of jobs to submit, if you exceed this number, break submitting new jobs")
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    version = '2.1.0'
+    print('Starting autoprocess version %s...' % version)
+
+    # Later import to prevent slow --help
+    import amstrax
+    from amstrax.auto_processing import submit_stbc
+
+    # settings
+    nap_time = int(args.timeout)
+    max_jobs = int(args.max_jobs) if args.max_jobs is not None else None
+    runs_col = amstrax.get_mongo_collection()
+
+    while 1:
+        # Update task list
+        run_docs_to_do = list(runs_col.find({'processing_status': 'pending'}))
+        if len(run_docs_to_do) > 0:
+            print('I found %d runs to process, time to get to work!' % len(run_docs_to_do))
+            print('These runs I will do:')
+            print([run_doc['number'] for run_doc in run_docs_to_do])
+
+        for run_doc in run_docs_to_do[:max_jobs]:
+            run_name = f'{int(run_doc["number"]):06}'
+            submit_stbc.submit_job(run_name, target='raw_records_aqmon', script='process_run')
+            runs_col.find_one_and_update({'number': run_name},
+                                         {'$set': {'processing_status': 'submitted_job'
+                                                  }})
+            time.sleep(2)
+        if max_jobs is not None and len(run_docs_to_do) > max_jobs:
+            print(f'Got {len(run_docs_to_do)} which is larger than {max_jobs}, I quit!')
+            break
+        print("Waiting %d seconds before rechecking, press Ctrl+C to quit..." % nap_time)
+        time.sleep(nap_time)
---
+++ packages/amstrax/amstrax-1.1.2-py3-none-any.whl/amstrax/common.py
@@ -0,0 +1,60 @@
+import inspect
+import io
+import os
+import tarfile
+
+import numpy as np
+import strax
+import straxen
+
+export, __all__ = strax.exporter()
+__all__ += ['amstrax_dir', 'to_pe']
+
+amstrax_dir = os.path.dirname(os.path.abspath(
+    inspect.getfile(inspect.currentframe())))
+
+# Current values 
+n_tpc_pmts = 8
+to_pe = 1
+
+
+def open_test_data(file_name
+                   ):
+    """Downloads amstrax test data to strax_test_data in the current directory"""
+    with open(file_name, mode='rb') as f:
+        result = f.read()
+    f = io.BytesIO(result)
+    tf = tarfile.open(fileobj=f)
+    tf.extractall()
+
+
+first_sr1_run = '1'
+
+cache_dict = dict()
+
+
+# Placeholder for resource management system in the future?
+@export
+def get_resource(x, fmt='text'):
+    return straxen.get_resource(x, fmt=fmt)
+
+
+@export
+def get_elife(run_id):
+    """Return electron lifetime for run_id in ns"""
+    return 642e3
+
+
+@export
+def select_channels(arr, channel_list):
+    """Select only the values in arr that have arr['channel'] in channel_list
+    """
+    sel = np.sum([arr['channel'] == channel for channel in channel_list], axis=0) > 0
+    return arr[sel]
+
+
+@export
+def print_versions(*args, **kwargs):
+    """Use straxen print_versions (see help(straxen.print_versions))"""
+    kwargs.setdefault('modules', ('amstrax', 'strax', 'straxen'))
+    return straxen.print_versions(*args, **kwargs)
