dfe08c85d4865611677d61ab01a08f1cf82c6187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:21 +0000

    Add package metastore/1.0.0.dev16/metastore-1.0.0.dev16-py3-none-any.whl

---
+++ packages/metastore/metastore-1.0.0.dev16-py3-none-any.whl/metastore/package.py
@@ -0,0 +1,35 @@
+'''
+Package description information.
+'''
+
+#: Package display name.
+__title__ = 'Metastore'
+
+#: Package version.
+__version__ = '1.0.0.dev16'
+
+#: Package description.
+__description__ = 'Metastore Python SDK. Feature store and data catalog for machine learning.'
+
+#: Package author name.
+__author__ = 'Metastore Developers'
+
+#: Package author email.
+__email__ = 'metastoredevelopers@gmail.com'
+
+#: Package license name.
+__license__ = 'BSD-3-Clause'
+
+#: Package copyright.
+__copyright__ = 'Copyright (c) 2022, Metastore Developers. All rights reserved.'
+
+
+__all__ = [
+    '__title__',
+    '__version__',
+    '__description__',
+    '__author__',
+    '__email__',
+    '__license__',
+    '__copyright__'
+]
