927c4734bb865e6babead1eb68676e1ecfc3daa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:16 +0000

    Add package storage3/0.1.1/storage3-0.1.1.tar.gz

---
+++ packages/storage3/storage3-0.1.1.tar.gz/storage3-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['storage3', 'storage3.lib']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.19,<0.22']
+
+setup_kwargs = {
+    'name': 'storage3',
+    'version': '0.1.1',
+    'description': 'Supabase Storage client for Python.',
+    'long_description': '# Storage-py\n\nPython Client library to interact with Supabase Storage.\n',
+    'author': 'Joel Lee',
+    'author_email': 'joel@joellee.org',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/supabase-community/storage-py',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
