59fb6e666e00ba492dcb65a3beb1f0c13e50e7a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:24 +0000

    Add package py-code-meli/0.1.3/py_code_meli-0.1.3-py3-none-any.whl

---
+++ packages/py-code-meli/py_code_meli-0.1.3-py3-none-any.whl/py_code_meli/validator.py
@@ -0,0 +1,26 @@
+
+
+def validate_meli_code(value: str) -> bool:
+    """
+    To see how the algorithem works, see http://www.aliarash.com/article/codemeli/codemeli.htm
+    
+    """
+    if not len(value) == 10:
+        # raise ValueError("کد ملی باید ۱۰ رقم باشد.")
+        return False
+
+    res = 0
+    for i, num in enumerate(value[:-1]):
+        res = res + (int(num) * (10 - i))
+
+    remain = res % 11
+    if remain < 2:
+        if not remain == int(value[-1]):
+            # raise ValueError("کد ملی درست نیست")
+            return False
+    else:
+        if not (11 - remain) == int(value[-1]):
+            # raise ValueError("کد ملی درست نیست")
+            return False
+
+    return True
