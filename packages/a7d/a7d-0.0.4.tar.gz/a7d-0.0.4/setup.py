da81d6aaba80b581028af746b59d1a12df1e5f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:28 +0000

    Add package a7d/0.0.4/a7d-0.0.4.tar.gz

---
+++ packages/a7d/a7d-0.0.4.tar.gz/a7d-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("../readme.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="a7d",
+    version="0.0.4",
+    author="Uladzislau Khamkou",
+    description="cool plaintext archiver",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'License :: Public Domain',
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+    py_modules=["a7d"],
+    package_dir={"": "."},
+    install_requires=[],
+)
