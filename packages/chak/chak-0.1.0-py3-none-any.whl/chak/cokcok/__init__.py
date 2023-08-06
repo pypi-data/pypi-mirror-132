5153635e62c56783ff5c8bf22b08437cdc93bf8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:22 +0000

    Add package chak/0.1.0/chak-0.1.0-py3-none-any.whl

---
+++ packages/chak/chak-0.1.0-py3-none-any.whl/chak/cokcok/__init__.py
@@ -0,0 +1,10 @@
+class chacha:
+    def __init__(self) -> None:
+        print("process")
+
+    def goh(self, adadd) -> None:
+        # random number
+        import random
+
+        random_number = random.randint(1, 100)
+        print(random_number, "    ", adadd)
