932b3c6f944dac97844eb67997f022928c19d14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:06 +0000

    Add package FastAPI-REST-JSONAPI/0.4/FastAPI-REST-JSONAPI-0.4.tar.gz

---
+++ packages/FastAPI-REST-JSONAPI/FastAPI-REST-JSONAPI-0.4.tar.gz/FastAPI-REST-JSONAPI-0.4/fastapi_rest_jsonapi/data_layer.py
@@ -0,0 +1,26 @@
+from abc import ABCMeta, abstractmethod
+from fastapi_rest_jsonapi.field import Field
+
+from fastapi_rest_jsonapi.sort import Sort
+
+
+class DataLayer(metaclass=ABCMeta):
+    @abstractmethod
+    def get(self, sorts: list[Sort], fields: list[Field]) -> list:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_one(self, id: int) -> object:
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete_one(self, id: int) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def update_one(self, id: int, **kwargs) -> object:
+        raise NotImplementedError
+
+    @abstractmethod
+    def create_one(self, **kwargs) -> object:
+        raise NotImplementedError
