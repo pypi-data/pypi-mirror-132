50cf3877f0e7ce0ab27e91b4f3319b42d0b6a49b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:53 +0000

    Add package django-typomatic/1.6.0/django_typomatic-1.6.0-py3-none-any.whl

---
+++ packages/django-typomatic/django_typomatic-1.6.0-py3-none-any.whl/django_typomatic/test__init__.py
@@ -0,0 +1,104 @@
+import io
+import pytest
+from rest_framework import serializers
+from django.db import models
+from unittest.mock import patch, mock_open, MagicMock
+from . import ts_interface, generate_ts, get_ts
+
+
+@ts_interface(context='internal')
+class FooSerializer(serializers.Serializer):
+    some_field = serializers.ListField(child=serializers.IntegerField())
+    another_field = serializers.CharField()
+    null_field = serializers.CharField(allow_null=True)
+
+
+@ts_interface(context='internal')
+class BarSerializer(serializers.Serializer):
+    foo = FooSerializer()
+    foos = FooSerializer(many=True)
+    bar_field = serializers.CharField()
+
+
+@ts_interface(context='external')
+class OtherSerializer(serializers.Serializer):
+    field = serializers.IntegerField()
+
+
+@ts_interface(context='choice')
+class OtherSerializer(serializers.Serializer):
+    field = serializers.IntegerField()
+
+
+class ActionType(models.TextChoices):
+    ACTION1 = "Action1", ("Action1")
+    ACTION2 = "Action2", ("Action2")
+    ACTION3 = "Action3", ("Action3")
+
+
+@ts_interface('choices')
+class ActionSerializer(serializers.Serializer):
+    action = serializers.ChoiceField(choices=ActionType.choices)
+
+
+def test_get_ts():
+    expected = """export interface FooSerializer {
+    some_field: number[];
+    another_field: string;
+    null_field: string | null;
+}
+
+export interface BarSerializer {
+    foo: FooSerializer;
+    foos: FooSerializer[];
+    bar_field: string;
+}
+
+"""
+    interfaces = get_ts('internal')
+    assert interfaces == expected
+
+
+def test_exclude_serializer():
+    expected = """export interface Foo {
+    some_field: number[];
+    another_field: string;
+    null_field: string | null;
+}
+
+export interface Bar {
+    foo: Foo;
+    foos: Foo[];
+    bar_field: string;
+}
+
+"""
+    interfaces = get_ts('internal', trim_serializer_output=True)
+    assert interfaces == expected
+
+def test_camlize():
+    expected = """export interface FooSerializer {
+    someField: number[];
+    anotherField: string;
+    nullField: string | null;
+}
+
+export interface BarSerializer {
+    foo: FooSerializer;
+    foos: FooSerializer[];
+    barField: string;
+}
+
+"""
+    interfaces = get_ts('internal', camelize=True)
+    assert interfaces == expected
+
+
+def test_choices():
+    expected = """export interface ActionSerializer {
+    action: Action1 | Action2 | Action3;
+}
+
+"""
+    interfaces = get_ts('choices')
+    assert interfaces == expected
