1e611746a88739dbc6e9e6f001183e484a5c4808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:00 +0000

    Add package cv2PySide6/1.0.2/cv2PySide6-1.0.2.tar.gz

---
+++ packages/cv2PySide6/cv2PySide6-1.0.2.tar.gz/cv2PySide6-1.0.2/cv2PySide6/__init__.py
@@ -0,0 +1,24 @@
+from .version import __version__ # noqa
+
+
+from .utilwidgets import ClickableSlider
+from .labels import ScalableQLabel, NDArrayLabel, array2qimage, qimage2array
+from .videoplayer import (QVideoFrame2Array, NDArrayVideoWidget,
+    NDArrayVideoPlayerWidget)
+from .util import get_data_path
+
+
+__all__ = [
+    "ClickableSlider",
+
+    "ScalableQLabel",
+    "NDArrayLabel",
+    "array2qimage",
+    "qimage2array",
+
+    "QVideoFrame2Array",
+    "NDArrayVideoWidget",
+    "NDArrayVideoPlayerWidget",
+
+    "get_data_path",
+]
