1bfabf428d47f1580b8296405ff08738283bbf64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:09:32 +0000

    Add package ipynb-conversor/0.0.1/ipynb_conversor-0.0.1.tar.gz

---
+++ packages/ipynb-conversor/ipynb_conversor-0.0.1.tar.gz/ipynb_conversor-0.0.1/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = ''.join(["\n",fh.read()])
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A package for getting python code from jupyter notebook.'
+
+# Setting up
+setup(
+    name="ipynb_conversor",
+    version=VERSION,
+    author="Vitia Fritelle (Vitor Guilherme Coutinho de Barros Junior)",
+    author_email="<vitorjunior448@gmail.com>",
+    description=DESCRIPTION,
+    license='MIT',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    url='https://github.com/vitia-fritelle/ipynb_converter',
+    packages=find_packages(),
+    install_requires=['typing'],
+    keywords=['python', 'jupyter notebook', 'converter'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'Framework :: Jupyter',
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
