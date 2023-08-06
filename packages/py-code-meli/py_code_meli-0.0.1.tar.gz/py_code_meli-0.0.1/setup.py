d454ce1db86d3eeeb94226b0922cb9b523ab97dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:16 +0000

    Add package py-code-meli/0.0.1/py_code_meli-0.0.1.tar.gz

---
+++ packages/py-code-meli/py_code_meli-0.0.1.tar.gz/py_code_meli-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['py_code_meli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pydantic>=1.8.2,<2.0.0', 'pytest>=6.2.5,<7.0.0']
+
+setup_kwargs = {
+    'name': 'py-code-meli',
+    'version': '0.0.1',
+    'description': 'This python package gives you a simple method that you can validate code meli (Iranian National Code) with it.',
+    'long_description': None,
+    'author': 'ali',
+    'author_email': 'lmntrix.afr@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
