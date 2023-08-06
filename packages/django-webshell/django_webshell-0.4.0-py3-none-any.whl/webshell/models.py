4129fc6bab1f193139a65372cb1d2b507954c039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:00 +0000

    Add package django-webshell/0.4.0/django_webshell-0.4.0-py3-none-any.whl

---
+++ packages/django-webshell/django_webshell-0.4.0-py3-none-any.whl/webshell/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+try:
+    from django.utils.translation import gettext_lazy as _
+except ImportError:
+    from django.utils.translation import ugettext_lazy as _
+
+
+class Script(models.Model):
+    name = models.CharField(_('Name'), max_length=100)
+    source = models.TextField(_('Source'))
+
+    class Meta:
+        verbose_name = _('Script')
+        verbose_name_plural = _('Scripts')
+
+    def __unicode__(self):
+        return self.name
