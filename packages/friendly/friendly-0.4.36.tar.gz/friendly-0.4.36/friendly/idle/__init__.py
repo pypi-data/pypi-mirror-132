9c224063329ca71cfd3a2ff3dbaac1273db2e8fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:55 +0000

    Add package friendly/0.4.36/friendly-0.4.36.tar.gz

---
+++ packages/friendly/friendly-0.4.36.tar.gz/friendly-0.4.36/friendly/idle/__init__.py
@@ -0,0 +1,7 @@
+from .main import *  # noqa
+
+__all__ = list(helpers)  # noqa
+__all__.append("install")  # noqa
+__all__.append("run")  # noqa
+__all__.append("start_console")  # noqa
+__all__.append("Friendly")  # noqa
