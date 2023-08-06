a0ac37ad3a472eca139f3f560899f21ea84e2f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:19 +0000

    Add package cli-toolkit/1.2.1/cli-toolkit-1.2.1.tar.gz

---
+++ packages/cli-toolkit/cli-toolkit-1.2.1.tar.gz/cli-toolkit-1.2.1/cli_toolkit/exceptions.py
@@ -0,0 +1,15 @@
+"""
+Exceptions raised by cli-toolkit
+"""
+
+
+class ConfigurationError(Exception):
+    """
+    Errors raised by configuration processing
+    """
+
+
+class ScriptError(Exception):
+    """
+    Errors raise during script processing
+    """
