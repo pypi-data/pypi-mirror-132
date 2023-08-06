463542ee3c697da1caf13e55cf62d4cfd02fb889
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:04 +0000

    Add package ntai/1.0.0/ntai-1.0.0-py3-none-any.whl

---
+++ packages/ntai/ntai-1.0.0-py3-none-any.whl/ntai/__init__.py
@@ -0,0 +1,14 @@
+from .codex import Codex, NPCodex
+from .bedtools import bedtools
+from .fetch import fetch_files
+from .labeler import Labeler
+# from .ranges import LabeledRange, LabeledRanges
+
+name = 'ntai'
+
+major = 1
+minor = 0
+patch = 0
+version = '{}.{}.{}'.format(major, minor, patch)
+
+description = 'Nucleotide A.I.'
