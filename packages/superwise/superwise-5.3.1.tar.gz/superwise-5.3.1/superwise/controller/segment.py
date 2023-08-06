16852583b28448eb9689a539999970b1c9967c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:56 +0000

    Add package superwise/5.3.1/superwise-5.3.1.tar.gz

---
+++ packages/superwise/superwise-5.3.1.tar.gz/superwise-5.3.1/superwise/controller/segment.py
@@ -0,0 +1,23 @@
+""" This module implement tasks functionality  """
+from superwise.controller.base import BaseController
+
+
+class SegmentController(BaseController):
+    """Class SegmentController - responsible for segment functionality"""
+
+    def __init__(self, client, sw):
+        """
+        ### Description:
+
+        Constructor of SegmentController
+
+        ### Args:
+
+        `client`: superwise client object
+
+        `sw`: superwise  object
+
+        """
+        super().__init__(client, sw)
+        self.path = "admin/v1/segments"
+        self.model_name = "Segment"
