9f4970606c32f91cdfa1296a4b8703491a5e9257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:54 +0000

    Add package arimionim-math/0.0.1/arimionim_math-0.0.1.tar.gz

---
+++ packages/arimionim-math/arimionim_math-0.0.1.tar.gz/arimionim_math-0.0.1/arimionim_math/__init__.py
@@ -0,0 +1,3 @@
+__version__ = "0.0.1"
+__author__ = 'Arslan Aknazarov'
+__credits__ = 'ITMO ML Lab'
---
+++ packages/arimionim-math/arimionim_math-0.0.1.tar.gz/arimionim_math-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='arimionim_math',
+    version='0.0.1',
+    description='Arimionim\'s example Python package',
+    url='https://github.com/abc/xyz',
+    author='Arslan Aknazarov',
+    author_email='abc@xyz.ru',
+    license='BSD 2-clause',
+    packages=['arimionim_math'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
+
