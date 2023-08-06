5a0b1434d6a52e400408f40365253aae30938bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:01 +0000

    Add package ntubSystem/1.0b5/ntubSystem-1.0b5.tar.gz

---
+++ packages/ntubSystem/ntubSystem-1.0b5.tar.gz/ntubSystem-1.0b5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup,find_packages
+__version__ = "1.0b5"
+with open("README.md",encoding="utf-8") as FileObj:
+    long_desc = FileObj.read()
+setup(
+    name="ntubSystem",
+    version=__version__,
+    author="TecHaonical",
+    description="NTUB 選課系統",
+    keywords="ntub",
+    license='MIT',
+    url="https://github.com/321qwedsa000/ntubSystem",
+    packages=find_packages(),
+    platforms=["any"],
+    install_requires=["requests","bs4","prettytable","lxml"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10"
+    ],
+    python_requires='>=3.6'
+)
