9819840f276acb98bc529cf60eacde1728bc9a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:19 +0000

    Add package DWonder/0.1/DWonder-0.1.tar.gz

---
+++ packages/DWonder/DWonder-0.1.tar.gz/DWonder-0.1/setup.py
@@ -0,0 +1,29 @@
+
+from setuptools import setup, find_packages
+ 
+setup(
+    name="DWonder",
+    version="0.1",
+    license="MIT Licence",
+ 
+    url="https://github.com/yuanlong-o/Deep_widefield_cal_inferece",
+    author="YuanlongZhang&GuoxunZhang",
+    author_email="zhanggx19@mails.tsinghua.edu.cn",
+ 
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=['opencv-python==4.1.2.30',
+    'tifffile  ==2021.3.5',
+    'scikit-image==0.17.2',
+    'scikit-learn==0.24.1',
+    'scipy==1.5.2',
+    'numpy==1.19.2',
+    'torch==1.7.1',
+    'torchvision==0.8.2',
+    'sklearn==0.0',]
+)
+
+# python setup.py sdist
+# pip install twine
+# twine upload dist/*
