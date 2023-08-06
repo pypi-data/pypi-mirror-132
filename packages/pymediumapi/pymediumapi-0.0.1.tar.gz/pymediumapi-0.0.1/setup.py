9f3d2e8a2757f19c55388753d970cf3e9806fdd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:15 +0000

    Add package pymediumapi/0.0.1/pymediumapi-0.0.1.tar.gz

---
+++ packages/pymediumapi/pymediumapi-0.0.1.tar.gz/pymediumapi-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pymediumapi",
+    version="0.0.1",
+    author="Andrea Grillo",
+    author_email="andrea.grillo96@live.com",
+    description="A python package to interact with Medium API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/andregri/pymediumapi",
+    project_urls={
+        "Bug Tracker": "https://github.com/andregri/pymediumapi/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "."},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.6",
+)
