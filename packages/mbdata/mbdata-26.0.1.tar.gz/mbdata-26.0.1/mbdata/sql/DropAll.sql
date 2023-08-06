2d9562c8beed1b50c6540af226aa6e89afca975d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:21 +0000

    Add package mbdata/26.0.1/mbdata-26.0.1.tar.gz

---
+++ packages/mbdata/mbdata-26.0.1.tar.gz/mbdata-26.0.1/mbdata/sql/DropAll.sql
@@ -0,0 +1,15 @@
+\unset ON_ERROR_STOP
+
+-- This script is a really quick and dirty way to tear down a database.
+-- It's really just a debug / development tool.
+
+\i admin/sql/DropReplicationTriggers.sql
+\i admin/sql/DropTriggers.sql
+\i admin/sql/DropSlaveOnlyTriggers.sql
+\i admin/sql/DropFunctions.sql
+\i admin/sql/DropViews.sql
+\i admin/sql/DropFKConstraints.sql
+\i admin/sql/DropIndexes.sql
+\i admin/sql/DropPrimaryKeys.sql
+\i admin/sql/DropTables.sql
+\i admin/sql/DropCollations.sql
