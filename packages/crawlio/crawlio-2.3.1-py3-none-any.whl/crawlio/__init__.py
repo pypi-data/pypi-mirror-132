9c9fb5c4e3b0323f06ee51d9706f3ccad369458c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:47 +0000

    Add package crawlio/2.3.1/crawlio-2.3.1-py3-none-any.whl

---
+++ packages/crawlio/crawlio-2.3.1-py3-none-any.whl/crawlio/__init__.py
@@ -0,0 +1,9 @@
+"""
+Author: Maximilian Wolf (max@w0lf.me)
+License: GPLv3
+"""
+
+
+__version__ = '2.3.1'
+
+from .crawlio import Crawler, Selector
---
+++ packages/crawlio/crawlio-2.3.1-py3-none-any.whl/crawlio/cmd.py
@@ -0,0 +1,35 @@
+import asyncio
+
+import click as click
+
+from crawlio import Crawler
+
+
+@click.group(invoke_without_command=True)
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument('url')
+@click.option('-e', '--export', type=bool, default=False)
+def run(url: str, export: bool = False):
+    crawler = Crawler(url)
+    output = asyncio.run(crawler.run())
+    if not export:
+        print('-----------\nStats:')
+        for key, value in output['info'].items():
+            print(key, ': ', value)
+        print('-----------\nResults:')
+        for page in output['data']:
+            print('\n')
+            for key, value in page.items():
+                print(key, '\t', value)
+    else:
+        # TODO: Implement export functionality
+        pass
+
+
+if __name__ == '__main__':
+    cli()
+
