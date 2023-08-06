f4bf0b041fd9276ab794d7a1223f186785e719b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:45 +0000

    Add package marklibrary/0.0.1/marklibrary-0.0.1.tar.gz

---
+++ packages/marklibrary/marklibrary-0.0.1.tar.gz/marklibrary-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="marklibrary",
+    packages=['marklibrary'],
+    version="0.0.1",
+    author="Kozlov Mark",
+    author_email="mark.k.2012@yandex.ru",
+    url="https://github.com/SMALA-comand/Optimization_algo",
+)
