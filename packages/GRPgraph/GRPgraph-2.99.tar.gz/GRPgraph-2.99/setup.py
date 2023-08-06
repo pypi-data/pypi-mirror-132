cdf0caf6b4bec7effaeb1f1f735b811425f08d50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:11 +0000

    Add package GRPgraph/2.99/GRPgraph-2.99.tar.gz

---
+++ packages/GRPgraph/GRPgraph-2.99.tar.gz/GRPgraph-2.99/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup  
+requirements = ["Pygame==2.1.0"]
+setup(name='GRPgraph',
+       version='2.99',
+      description='small and compact graphick distributions',
+      packages=['GRPgraph'],       
+      author_email='pvana621@gmail.com', 
+      install_requires=requirements,      
+      zip_safe=False)
