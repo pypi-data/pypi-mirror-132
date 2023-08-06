979c2cfc7f65d50a711bc3d04323446e89b54dd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:24 +0000

    Add package numcalc/1.0/numcalc-1.0.tar.gz

---
+++ packages/numcalc/numcalc-1.0.tar.gz/numcalc-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="numcalc",
+    version='1.0',
+    description='Python Distribution Utilities',
+    author='Arinjoy Nath',
+    author_email='arinjoy762@gmail.com',
+    url='',
+    packages=[''],
+)
