1efa59f46449eea809d4da651192ee69b1220b32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:43 +0000

    Add package yapic-io/0.2.7/yapic_io-0.2.7.tar.gz

---
+++ packages/yapic-io/yapic_io-0.2.7.tar.gz/yapic_io-0.2.7/setup.py
@@ -0,0 +1,31 @@
+import os
+
+from setuptools import setup
+
+reqs = ['numpy>=1.13.1',
+        'munkres>=1.0.8',
+        'scikit_image>=0.12.3',
+        'tifffile',
+        'h5py',
+        'matplotlib',
+        'sparse>=0.12.0']
+
+
+def readme():
+    README_rst = os.path.join(os.path.dirname(__file__), 'README.md')
+    with open(README_rst) as f:
+        return f.read()
+
+
+setup(name='yapic_io',
+      version='0.2.7',
+      description='io data handling module for various image sources as interface for pixel classification tools',
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      author='Manuel Schoelling, Christoph Moehl',
+      author_email='manuel.schoelling@dzne.de, christoph.moehl@dzne.de',
+      packages=['yapic_io'],
+      zip_safe=False,
+      install_requires=reqs,
+      test_suite='nose.collector',
+      tests_require=['coverage', 'nose-timer', 'nose'])
