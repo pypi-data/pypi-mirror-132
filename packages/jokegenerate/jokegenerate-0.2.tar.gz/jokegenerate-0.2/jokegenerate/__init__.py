0d802af65df6f1bd2380c13fbc96ab0a5d45f1d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:16 +0000

    Add package jokegenerate/0.2/jokegenerate-0.2.tar.gz

---
+++ packages/jokegenerate/jokegenerate-0.2.tar.gz/jokegenerate-0.2/jokegenerate/__init__.py
@@ -0,0 +1,31 @@
+import random
+print("Hello from Saksham Jain")
+class Joke:
+    def getJoke():
+          jokelist=["Is it true that cannibals don't eat clowns because they taste funny",
+
+                    "What kind of pig can you ignore at a party? A wild bore.",
+
+                    "Why do Marxists like fruit infusions? Because all proper tea is theft!",
+
+                    "What was born to succeed? A budgie with a blunt beak.",
+
+                    "What lies on the bottom of the ocean and shakes? A nervous wreck.",
+
+                    "What's brown and sticky? A stick",
+
+                    "Q. Where do you keep a baby ape? A In an apricot",
+
+                    "Close your eyes. Dark isn't it?",
+
+                    "What do you call an aarvark that has been beaten up? A vark.",
+
+                    "- Can you swim?- Some times.- What do you mean by some times? - Only when I'm in the water.",
+
+                    "Why do bees hum ? Because they don't know the words",
+
+                    "What's orange and sounds like a parrot? A carrot!",
+
+                    "Why do elephants have big ears?Noddy wouldn't pay the ransom"
+                   ]
+          print(random.choice(jokelist))
---
+++ packages/jokegenerate/jokegenerate-0.2.tar.gz/jokegenerate-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="jokegenerate",
+version="0.2",
+description="This is module is made by Saksham Jain",
+long_description = "This is a very very long description",
+author="Saksham Jain",
+packages=['jokegenerate'],
+install_requires=[])
