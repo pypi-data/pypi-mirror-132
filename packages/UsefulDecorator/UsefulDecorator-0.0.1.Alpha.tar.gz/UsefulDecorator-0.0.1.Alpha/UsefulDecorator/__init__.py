5aefa9a13981512b29083734d50338f46ad5a56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:09 +0000

    Add package UsefulDecorator/0.0.1.Alpha/UsefulDecorator-0.0.1.Alpha.tar.gz

---
+++ packages/UsefulDecorator/UsefulDecorator-0.0.1.Alpha.tar.gz/UsefulDecorator-0.0.1.Alpha/UsefulDecorator/__init__.py
@@ -0,0 +1,1 @@
+from .NotNone import NotNone
---
+++ packages/UsefulDecorator/UsefulDecorator-0.0.1.Alpha.tar.gz/UsefulDecorator-0.0.1.Alpha/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='UsefulDecorator',
+    version='0.0.1 Alpha',
+    description='useful decorator',
+
+    author='EGGnmad',
+    author_email='viewnono1219@gmail.com',
+    keywords='decorator',
+
+    packages=find_packages()
+)
