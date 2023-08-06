843c3ffae77582f03af6596fbb60503a159612ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:28 +0000

    Add package friendly-data-registry/20211227/friendly_data_registry-20211227.tar.gz

---
+++ packages/friendly-data-registry/friendly_data_registry-20211227.tar.gz/friendly_data_registry-20211227/setup.py
@@ -0,0 +1,23 @@
+"""Frictionless Energy Data Registry
+
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name="friendly_data_registry",
+    version="20211227",
+    description="Schema registry for friendly_data",
+    url="https://github.com/sentinel-energy/friendly_data_registry",
+    packages=find_packages(exclude=["tests"]),
+    install_requires=["pyyaml"],
+    package_data={
+        "friendly_data_registry": [
+            "cols/*.json",
+            "cols/*.yaml",
+            "idxcols/*.json",
+            "idxcols/*.yaml",
+        ]
+    },
+    include_package_data=True,
+)
