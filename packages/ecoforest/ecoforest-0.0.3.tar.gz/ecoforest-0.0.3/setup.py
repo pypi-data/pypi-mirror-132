9718eadb7c3e06a0161c030947ec57bad1e9ac4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:44 +0000

    Add package ecoforest/0.0.3/ecoforest-0.0.3.tar.gz

---
+++ packages/ecoforest/ecoforest-0.0.3.tar.gz/ecoforest-0.0.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+setuptools.setup(
+    name="ecoforest",
+    version="0.0.3",
+    author="",
+    author_email="",
+    description="",
+    long_description="an interpretable data-driven macroeconomic forecasting method based on N-BEATS neural network",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'hyperopt==0.2.5',
+        'matplotlib==3.3.3',
+        'numpy==1.18.5',
+        'pandas==1.1.5',
+        'requests==2.25.1',
+        'scipy==1.4.1',
+        'statsmodels==0.12.2',
+        'torch==1.8.1',
+        'tqdm==4.54.1',
+    ],
+)
