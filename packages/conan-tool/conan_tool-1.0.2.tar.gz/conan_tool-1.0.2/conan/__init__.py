5f5b3a913a77f9ff661cb075367742e6c5b91d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:32 +0000

    Add package conan-tool/1.0.2/conan_tool-1.0.2.tar.gz

---
+++ packages/conan-tool/conan_tool-1.0.2.tar.gz/conan_tool-1.0.2/conan/__init__.py
@@ -0,0 +1,4 @@
+
+from .conan import *
+
+__all__ = ["lp_wrapper", "cp_wrapper", "break_point"]
---
+++ packages/conan-tool/conan_tool-1.0.2.tar.gz/conan_tool-1.0.2/setup.py
@@ -0,0 +1,51 @@
+import os
+import re
+
+from setuptools import find_packages, setup
+
+VERSION_RE = re.compile(r"""__version__ = ['"]([0-9.]+)['"]""")
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*args):
+    """Reads complete file contents."""
+    return open(os.path.join(HERE, *args)).read()
+
+
+def get_version():
+    """Reads the version from this module."""
+    init = read("constant.py")
+    return VERSION_RE.search(init).group(1)
+
+
+def get_requirements():
+    """Reads the requirements file."""
+    requirements = read("requirements.txt")
+    return list(requirements.strip().splitlines())
+
+
+setup(
+    name="conan_tool",
+    packages=find_packages(),
+    url='https://github.com/vlean/conan',
+    version=get_version(),
+    author="vlean",
+    author_email='vlean@qq.com',
+    maintainer="vlean",
+    description="conan: cpu & memory profiler tool",
+    long_description=read("README.md"),
+    keywords=["conan", "profiler"],
+    install_requires=get_requirements(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Security",
+        "Topic :: Security :: Cryptography",
+    ],
+)
