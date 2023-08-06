80d97c48d8d763c10c8ba8205157a37083e082c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:07 +0000

    Add package GRPgraph/2.915/GRPgraph-2.915.tar.gz

---
+++ packages/GRPgraph/GRPgraph-2.915.tar.gz/GRPgraph-2.915/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup  
+setup(name='GRPgraph',
+       version='2.915',
+      description='small and compact graphick distributions',
+      packages=['GRPgraph'],       
+      author_email='pvana621@gmail.com',       
+      zip_safe=False)
