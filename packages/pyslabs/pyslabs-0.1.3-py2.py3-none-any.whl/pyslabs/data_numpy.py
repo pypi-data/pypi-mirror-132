d15496a904b9d390b3cfce9cb9f819ce35612654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:34 +0000

    Add package pyslabs/0.1.3/pyslabs-0.1.3-py2.py3-none-any.whl

---
+++ packages/pyslabs/pyslabs-0.1.3-py2.py3-none-any.whl/pyslabs/data_numpy.py
@@ -0,0 +1,41 @@
+import numpy as np
+from io import BytesIO
+
+def dump(slab, file):
+    return np.save(file, slab)
+
+
+def concat(arrays, axis=0):
+    return np.concatenate(arrays, axis=axis)
+
+
+def stack(arrays):
+    return np.stack(arrays)
+
+
+def load(file):
+    bytes = BytesIO()
+    bytes.write(file.read())
+    bytes.seek(0)
+    return np.load(bytes)
+
+
+def shape(slab):
+    return slab.shape
+
+
+def ndim(slab):
+    return slab.ndim
+
+
+def length(slab, axis):
+    _s = shape(slab)
+    return _s[axis]
+
+
+def squeeze(slab):
+    return np.squeeze(slab, axis=0)
+
+
+def get_slice(slab, key):
+    return slab.__getitem__(tuple(key))
