4696fc93e15b0f839813a9e3e513ecdb4e5fc83c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:54 +0000

    Add package tsc-draw/0.3/tsc-draw-0.3.tar.gz

---
+++ packages/tsc-draw/tsc-draw-0.3.tar.gz/tsc-draw-0.3/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os
+
+if os.path.exists('readme.md'):
+    long_description = open('readme.md', 'r', encoding='utf8').read()
+else:
+    long_description = '代码: https://github.com/aitsc/tsc-draw'
+
+setup(
+    name='tsc-draw',
+    version='0.3',
+    description="绘图",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='tanshicheng',
+    license='GPLv3',
+    url='https://github.com/aitsc/tsc-draw',
+    keywords='tools',
+    packages=find_packages(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries',
+    ],
+    install_requires=[
+        'matplotlib>=3.1.3,<=3.2.3',
+        'numpy>=1.18.1',
+        'scipy>=1.4.1',
+        'statsmodels>=0.12.2',
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/tsc-draw/tsc-draw-0.3.tar.gz/tsc-draw-0.3/tsc_draw/__init__.py
@@ -0,0 +1,7 @@
+'''
+### 暂时无用的用于超算的文件
+- info.py
+- query.py
+- srun.sh
+'''
+from .draw import Draw
