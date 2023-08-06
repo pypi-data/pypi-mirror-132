a2de243bb71f952884c9892d211874694f6485d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:36 +0000

    Add package aa-buybackprogram/0.1.6/aa-buybackprogram-0.1.6.tar.gz

---
+++ packages/aa-buybackprogram/aa-buybackprogram-0.1.6.tar.gz/aa-buybackprogram-0.1.6/buybackprogram/urls.py
@@ -0,0 +1,56 @@
+from django.conf.urls import url
+from django.urls import path
+
+from buybackprogram.views import calculate, common, programs, stats
+
+app_name = "buybackprogram"
+
+urlpatterns = [
+    path("", common.index, name="index"),
+    path("setup", programs.setup, name="setup"),
+    path("program_add", programs.program_add, name="program_add"),
+    path("location_add", programs.location_add, name="location_add"),
+    url(
+        r"^program/(?P<program_pk>[0-9]+)/edit_item$",
+        programs.program_edit_item,
+        name="program_edit_item",
+    ),
+    url(
+        r"^program/(?P<program_pk>[0-9]+)/edit",
+        programs.program_edit,
+        name="program_edit",
+    ),
+    url(
+        r"^program/(?P<program_pk>[0-9]+)/remove$",
+        programs.program_remove,
+        name="program_remove",
+    ),
+    url(
+        r"^program/(?P<program_pk>[0-9]+)/program_item/(?P<item_pk>[0-9]+)/remove$",
+        programs.program_item_remove,
+        name="program_item_remove",
+    ),
+    url(
+        r"^program/(?P<program_pk>[0-9]+)/calculate$",
+        calculate.program_calculate,
+        name="program_calculate",
+    ),
+    path("my_stats", stats.my_stats, name="my_stats"),
+    path(
+        "my_stats/<str:contract_title>/details/",
+        stats.contract_details,
+        name="contract_details",
+    ),
+    path("program_stats", stats.program_stats, name="program_stats"),
+    path("program_stats_all", stats.program_stats_all, name="program_stats_all"),
+    url(
+        r"^item_autocomplete/$",
+        common.item_autocomplete,
+        name="item_autocomplete",
+    ),
+    url(
+        r"^solarsystem_autocomplete/$",
+        common.solarsystem_autocomplete,
+        name="solarsystem_autocomplete",
+    ),
+]
