2e8e53e3644b07b904e7ecd24781f2013356247e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:08:49 +0000

    Add package ControlManual/0.0.3.7/ControlManual-0.0.3.7-py3-none-any.whl

---
+++ packages/ControlManual/ControlManual-0.0.3.7-py3-none-any.whl/ControlManual/src/info.py
@@ -0,0 +1,10 @@
+__title__ = "ControlManual"
+__authors__ = __author__ = "ZeroIntensity and 2231puppy"
+__license__ = "MIT"
+__version__ = "0.0.3.7"
+
+if __name__ == "__main__":
+    print(f"""Title: {__title__}
+Authors: {__authors__}
+License: {__license__}
+Version: {__version__}""")
